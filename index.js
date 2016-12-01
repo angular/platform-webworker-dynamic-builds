c8a77bf0328ec5adf2f1c7193f4103aca3f18d57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 00:01:11 +0000

    c066281 docs(changelog): add changelog for 2.3.0-rc.0

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import { Version } from '@angular/core';
 /**
- * @module
- * @description
- * Entry point for all public APIs of the platform-browser-dynamic package.
+ * @stable
  */
+export var VERSION = new Version('2.3.0-rc.0-c066281');
 export * from './src/platform-webworker-dynamic';
 //# sourceMappingURL=index.js.map
