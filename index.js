6ae2048cab4d18bfd63dd7de137e9ee089cf034a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:44:59 +0000

    dfd8140 Fix format

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-rc.0-75d1617');
+export var VERSION = new Version('2.3.0-rc.0-dfd8140');
 export * from './src/platform-webworker-dynamic';
 //# sourceMappingURL=index.js.map
