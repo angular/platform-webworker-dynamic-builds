16fdd5c9f8618d23309cc55c58391f7fca20ab29
GitHub <noreply@github.com>
2016-12-08 23:20:45 +0000

    4398056 fix(compiler): fix transpiled ES5 code (#13322)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-1b54788
+ * @license Angular v2.3.0-4398056
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.3.0-1b54788');
+    var VERSION = new _angular_core.Version('2.3.0-4398056');
 
     /**
      * @experimental API related to bootstrapping are still under review.
