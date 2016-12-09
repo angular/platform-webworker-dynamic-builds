3378158cea18254d04e8b6bfc8c199b673d253c1
Victor Berchet <victor@suumit.com>
2016-12-09 18:58:35 +0000

    9a8423d fix(selector): SelectorMatcher match elements with :not selector (#12977)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-f0b0762
+ * @license Angular v2.3.0-9a8423d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.3.0-f0b0762');
+    var VERSION = new _angular_core.Version('2.3.0-9a8423d');
 
     /**
      * @experimental API related to bootstrapping are still under review.
