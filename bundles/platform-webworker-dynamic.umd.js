5fd43bc5a1f5916e7f32f27bff01e2705d8e13c8
GitHub <noreply@github.com>
2016-12-09 18:57:26 +0000

    b5c4bf1 refactor(router): misc refactoring (#13330)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-56c361f
+ * @license Angular v2.3.0-b5c4bf1
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.3.0-56c361f');
+    var VERSION = new _angular_core.Version('2.3.0-b5c4bf1');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-56c361f');
+export var VERSION = new Version('2.3.0-b5c4bf1');
 //# sourceMappingURL=version.js.map
