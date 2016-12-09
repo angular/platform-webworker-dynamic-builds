00afbd34d5160f93599746556eb095b21ebc69a7
Victor Berchet <victor@suumit.com>
2016-12-09 02:56:35 +0000

    72361fb feat(platform browser): introduce Meta service (#12322)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-5c6ec20');
+export var VERSION = new Version('2.3.0-72361fb');
 //# sourceMappingURL=version.js.map
