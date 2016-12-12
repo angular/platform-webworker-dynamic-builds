b97b694bd6a32e2711a05f8b5c5d29f5211c93fb
Victor Berchet <victor@suumit.com>
2016-12-12 19:30:11 +0000

    e9f307f fix(forms): fix Validators.min/maxLength with FormArray (#13095)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-2e500cc
+ * @license Angular v2.3.0-e9f307f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.3.0-2e500cc');
+    var VERSION = new _angular_core.Version('2.3.0-e9f307f');
 
     /**
      * @experimental API related to bootstrapping are still under review.
