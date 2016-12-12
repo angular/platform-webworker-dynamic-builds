f89d6afd4c96539798b942929eb7ea1dc2b0bf9f
Victor Berchet <victor@suumit.com>
2016-12-12 19:30:39 +0000

    4c35be3 feat(forms): add novalidate by default (#13092)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-e9f307f');
+export var VERSION = new Version('2.3.0-4c35be3');
 //# sourceMappingURL=version.js.map
