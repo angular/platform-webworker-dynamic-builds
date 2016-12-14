2dbf46c52b265ed6d3350e73baa4a0e64b20e2c1
Victor Berchet <victor@suumit.com>
2016-12-14 01:57:34 +0000

    a0a0504 refactor: format & lint

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-d62d893
+ * @license Angular v2.3.0-a0a0504
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.3.0-d62d893');
+    var VERSION = new _angular_core.Version('2.3.0-a0a0504');
 
     /**
      * @experimental API related to bootstrapping are still under review.
