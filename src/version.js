e009f7a03dc4689aa7ed558b055ac0e4ae0d469c
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 20:19:10 +0000

    14e785f fix(build): use bash string comparison operator (#13502)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.0-33910dd');
+export var VERSION = new Version('4.0.0-beta.0-14e785f');
 //# sourceMappingURL=version.js.map
