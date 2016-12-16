aaa00ffd4e4f41d55c20349f9ba632627f643fd3
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 01:20:33 +0000

    55dfa1b test(forms): refactor integration tests to improve speed (#13500)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-0fe3cd9
+ * @license Angular v4.0.0-beta.0-55dfa1b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-0fe3cd9');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-55dfa1b');
 
     /**
      * @experimental API related to bootstrapping are still under review.
