20dfc818decc865e5ddbccd6fd80253684c1a8b2
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 23:27:07 +0000

    c6e8939 fix(upgrade): fix `registerForNg1Tests` (#13522)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-55dfa1b
+ * @license Angular v4.0.0-beta.0-c6e8939
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-55dfa1b');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-c6e8939');
 
     /**
      * @experimental API related to bootstrapping are still under review.
