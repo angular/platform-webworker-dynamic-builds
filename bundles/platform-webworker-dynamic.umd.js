263b421d46bd8d332cfc69f9ecdfd7204e4f85ee
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 19:29:48 +0000

    1ee574c docs(changelog): add changelog for 2.4.0

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.0-6efdf84
+ * @license Angular v4.0.0-beta.0-1ee574c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.4.0-6efdf84');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-1ee574c');
 
     /**
      * @experimental API related to bootstrapping are still under review.
