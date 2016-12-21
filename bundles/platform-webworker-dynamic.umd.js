60be62289087fa3a43d3ac037c03a1428e92106b
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:04:15 +0000

    c0f750a fix(compiler): ignore @import in comments (#13368)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-bcd37f5
+ * @license Angular v4.0.0-beta.0-c0f750a
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-bcd37f5');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-c0f750a');
 
     /**
      * @experimental API related to bootstrapping are still under review.
