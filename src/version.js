488f2ab369d964752de354570b0bb3e43b629780
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:12:43 +0000

    9b8488f build: fix publish-build-artifacts branch detection (#13599)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.0-1ee574c');
+export var VERSION = new Version('4.0.0-beta.0-9b8488f');
 //# sourceMappingURL=version.js.map
