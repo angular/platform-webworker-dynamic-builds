7756c0483e76935df0a9f4946aace321c5b1e047
GitHub <noreply@github.com>
2016-12-21 02:02:59 +0000

    e69c1fb refactor(platform-browser): resolver merge conflict for tslint (#13601)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-fcd116f
+ * @license Angular v4.0.0-beta.0-e69c1fb
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-fcd116f');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-e69c1fb');
 
     /**
      * @experimental API related to bootstrapping are still under review.
