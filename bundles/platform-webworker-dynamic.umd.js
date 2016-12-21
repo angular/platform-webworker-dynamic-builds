3dfee1d4731c00e62361525a873f354c740e996c
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:31:28 +0000

    9da4c25 feat(upgrade): support the `$doCheck()` lifecycle hook in `UpgradeComponent` (#13015)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-383adc9
+ * @license Angular v4.0.0-beta.0-9da4c25
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-383adc9');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-9da4c25');
 
     /**
      * @experimental API related to bootstrapping are still under review.
