97c9a8bf094afa255a8414adc4eb179a1a209b8c
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:03:34 +0000

    bcd37f5 Include bower instructions in DEVELOPER.md (#13591)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-e69c1fb
+ * @license Angular v4.0.0-beta.0-bcd37f5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.0-e69c1fb');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-bcd37f5');
 
     /**
      * @experimental API related to bootstrapping are still under review.
