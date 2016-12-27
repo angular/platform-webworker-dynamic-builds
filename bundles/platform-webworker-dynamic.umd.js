5d6265ac4ecadde956ad1cb4d0ff105fbb36dece
Hans <hans@hansl.ca>
2016-12-27 22:22:48 +0000

    69fa3bb feat(router): add an extra argument to CanDeactivate interface (#13560)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-445ed43
+ * @license Angular v4.0.0-beta.1-69fa3bb
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.1-445ed43');
+    var VERSION = new _angular_core.Version('4.0.0-beta.1-69fa3bb');
 
     /**
      * @experimental API related to bootstrapping are still under review.
