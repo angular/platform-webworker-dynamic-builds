19d521c5a7b9e9ca0c5c864a3b8df96cf54f0337
Hans <hans@hansl.ca>
2016-12-28 01:11:44 +0000

    7690d02 fix(compiler): don’t throw when using `ANALYZE_FOR_ENTRY_COMPONENTS` with user classes (#13679)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.1-7c21064');
+export var VERSION = new Version('4.0.0-beta.1-7690d02');
 //# sourceMappingURL=version.js.map
