43c7a78a7ec1992b678b3a093ec4c51a176cc8b9
Hans <hans@hansl.ca>
2016-12-29 17:52:57 +0000

    1e6440e docs(Http): fix and extend samples for testing/MockBackend (#13689)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-6b02b80
+ * @license Angular v4.0.0-beta.1-1e6440e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.1-6b02b80');
+    var VERSION = new _angular_core.Version('4.0.0-beta.1-1e6440e');
 
     /**
      * @experimental API related to bootstrapping are still under review.
