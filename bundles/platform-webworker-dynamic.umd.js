3c2e2682169a5ae430fb7d58813ae3bd663940a4
Igor Minar <iminar@google.com>
2017-01-05 19:46:52 +0000

    889b48d fix(core): animations should blend in all previously transitioned styles into next animation if interrupted (#13148)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-1bd04e9
+ * @license Angular v4.0.0-beta.1-889b48d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.1-1bd04e9');
+    var VERSION = new _angular_core.Version('4.0.0-beta.1-889b48d');
 
     /**
      * @experimental API related to bootstrapping are still under review.
