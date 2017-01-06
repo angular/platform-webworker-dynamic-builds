56bf21722335b24a2bbb1f892e90600286f1559d
Igor Minar <igor@angularjs.org>
2017-01-06 21:15:33 +0000

    02dd90f docs(changelog): cherry-pick 2.4.2 release notes into the master branch

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-1c85e99
+ * @license Angular v4.0.0-beta.2-02dd90f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.2-1c85e99');
+    var VERSION = new _angular_core.Version('4.0.0-beta.2-02dd90f');
 
     /**
      * @experimental API related to bootstrapping are still under review.
