5a0c9f2d90df469a5acd2ffa270aec044f5d935a
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 23:48:12 +0000

    f14d549 feat(Router): call resolver when upstream params change (#12942)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.2-5d9cbd7');
+export var VERSION = new Version('4.0.0-beta.2-f14d549');
 //# sourceMappingURL=version.js.map
