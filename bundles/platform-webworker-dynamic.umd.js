a5649a23d5006b353a11d09cd6cec0324a706862
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 19:50:46 +0000

    8578682 feat(NgComponentOutlet): add NgComponentOutlet directive

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-02dd90f
+ * @license Angular v4.0.0-beta.2-8578682
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.2-02dd90f');
+    var VERSION = new _angular_core.Version('4.0.0-beta.2-8578682');
 
     /**
      * @experimental API related to bootstrapping are still under review.
