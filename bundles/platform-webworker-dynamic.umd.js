773e2ae4ded8ec64e7a1ea859c52ed69e83ee69a
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 23:56:43 +0000

    aeed737 fix(compiler-cli): avoid handling functions in loadChildren as lazy load routes paths

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-d43e5dd
+ * @license Angular v4.0.0-beta.2-aeed737
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.4.2-d43e5dd');
+    var VERSION = new _angular_core.Version('4.0.0-beta.2-aeed737');
 
     /**
      * @experimental API related to bootstrapping are still under review.
