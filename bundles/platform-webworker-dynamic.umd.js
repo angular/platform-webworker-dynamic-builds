f36a8e50d27c603e2f7db028abd0982ae95034f0
Alex Eagle <alexeagle@google.com>
2017-01-11 01:14:15 +0000

    ed32340 chore(owners): configure pullapprove.com

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-d4d3782
+ * @license Angular v4.0.0-beta.2-ed32340
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.2-d4d3782');
+    var VERSION = new _angular_core.Version('4.0.0-beta.2-ed32340');
 
     /**
      * @experimental API related to bootstrapping are still under review.
