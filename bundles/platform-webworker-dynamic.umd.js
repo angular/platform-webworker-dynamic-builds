aaa087f128fd9a7c1114b0031254c43974500a87
Miško Hevery <misko@hevery.com>
2017-01-12 18:10:32 +0000

    dc63cef docs(http): Spelling Fix #13867

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-2c294d5
+ * @license Angular v4.0.0-beta.3-dc63cef
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.3-2c294d5');
+    var VERSION = new _angular_core.Version('4.0.0-beta.3-dc63cef');
 
     /**
      * @experimental API related to bootstrapping are still under review.
