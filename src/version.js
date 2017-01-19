d5c7c22a5ad9a6a6dd44881be677e932c25365e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-19 20:22:01 +0000

    b049217 chore(docs): add missing comments (#14003)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.3-1200cf2');
+export var VERSION = new Version('4.0.0-beta.4-b049217');
 //# sourceMappingURL=version.js.map
