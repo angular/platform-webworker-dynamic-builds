b71a959714f701ac4fefa5b5291c9ed4a824ec13
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-20 22:41:08 +0000

    0589f93 Fixed documentation reference to canActivate in canDeactivate (#14018)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-2f87eb5
+ * @license Angular v4.0.0-beta.4-0589f93
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.4-2f87eb5');
+    var VERSION = new _angular_core.Version('4.0.0-beta.4-0589f93');
 
     /**
      * @experimental API related to bootstrapping are still under review.
