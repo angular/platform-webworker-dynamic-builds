17c425c08957d9259c7ba9165fe4bd153171f3c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 18:38:14 +0000

    1f54040 docs(common): fix a typo on the DatePipe API docs (#14060)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.4-6541737');
+export var VERSION = new Version('4.0.0-beta.4-1f54040');
 //# sourceMappingURL=version.js.map
