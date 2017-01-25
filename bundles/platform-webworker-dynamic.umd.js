a0c022ca0910895f9fcb0256f210ea1f85acd524
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 21:34:55 +0000

    d2859cd style(compiler): run format

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-4931a61
+ * @license Angular v4.0.0-beta.4-d2859cd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.4-4931a61');
+    var VERSION = new _angular_core.Version('4.0.0-beta.4-d2859cd');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.4-4931a61');
+export var VERSION = new Version('4.0.0-beta.4-d2859cd');
 //# sourceMappingURL=version.js.map
