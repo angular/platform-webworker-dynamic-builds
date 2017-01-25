6a0f190f980cc06f4f539a59ea73740e120fbcfc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 18:45:33 +0000

    a733444 docs(compiler): add comment to warn about regexp changes (#14106)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-b2f9d56
+ * @license Angular v4.0.0-beta.4-a733444
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.4-b2f9d56');
+    var VERSION = new _angular_core.Version('4.0.0-beta.4-a733444');
 
     /**
      * @experimental API related to bootstrapping are still under review.
