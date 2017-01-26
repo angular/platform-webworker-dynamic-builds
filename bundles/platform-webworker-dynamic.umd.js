68ace571a70eb667a91d3a7e11f576e4e59c9995
Victor Berchet <victor@suumit.com>
2017-01-26 01:57:40 +0000

    3ef73c2 feat(NgComponentOutlet): add NgModule support to NgComponentOutlet directive (#14088)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-4106d18
+ * @license Angular v4.0.0-beta.5-3ef73c2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-4106d18');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-3ef73c2');
 
     /**
      * @experimental API related to bootstrapping are still under review.
