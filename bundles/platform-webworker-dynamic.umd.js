f0e6df7199ee9b5ff637df7630455e0c6314e9fb
Miško Hevery <misko@hevery.com>
2017-01-27 20:57:32 +0000

    d339d8b refactor(abstract): Use abstract keyword where possible to decrease file size. (#14112)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-4d5a4d8
+ * @license Angular v4.0.0-beta.5-d339d8b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-4d5a4d8');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-d339d8b');
 
     /**
      * @experimental API related to bootstrapping are still under review.
