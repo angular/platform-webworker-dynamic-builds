8dcbd92f3c862c61084a017dd6380a13c7973041
Igor Minar <igor@angularjs.org>
2017-01-27 08:05:54 +0000

    b141a22 ci(aio): run docs-tests in travis (#14097)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.5-1e729d7');
+export var VERSION = new Version('4.0.0-beta.5-b141a22');
 //# sourceMappingURL=version.js.map
