96b16e35f6254a1bbb2e4ecd335745a1798436b7
Miško Hevery <misko@hevery.com>
2017-01-28 00:35:17 +0000

    3368f29 ci: update typings test to use standard typings (#14156)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-8960d49
+ * @license Angular v4.0.0-beta.5-3368f29
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-8960d49');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-3368f29');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.5-8960d49');
+export var VERSION = new Version('4.0.0-beta.5-3368f29');
 //# sourceMappingURL=version.js.map
