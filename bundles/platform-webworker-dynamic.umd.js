317ca9f175d1135a9af838f5a6c0d8e98cee919b
Igor Minar <iminar@google.com>
2017-01-28 02:41:09 +0000

    24ea3f0 test(compiler): don't print warnings from closure compiler (#14159)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3368f29
+ * @license Angular v4.0.0-beta.5-24ea3f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-3368f29');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-24ea3f0');
 
     /**
      * @experimental API related to bootstrapping are still under review.
