6251076bdd1133e179a3cf922a5a45dd5e1eea30
Miško Hevery <misko@hevery.com>
2017-01-30 23:14:45 +0000

    104c157 build: fix red travis: fetch more github history (#14193)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-bc1320d
+ * @license Angular v4.0.0-beta.5-104c157
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-bc1320d');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-104c157');
 
     /**
      * @experimental API related to bootstrapping are still under review.
