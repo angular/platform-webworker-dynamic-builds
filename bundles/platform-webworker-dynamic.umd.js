c311a33466941436380353ac030f4c9009aed08a
Miško Hevery <misko@hevery.com>
2017-02-01 01:42:40 +0000

    e0e5e78 fix: lint errors to make circle-ci green

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-8270bec
+ * @license Angular v4.0.0-beta.5-e0e5e78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-8270bec');
+    var VERSION = new _angular_core.Version('4.0.0-beta.5-e0e5e78');
 
     /**
      * @experimental API related to bootstrapping are still under review.
