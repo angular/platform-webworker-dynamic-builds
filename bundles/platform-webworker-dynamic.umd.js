43843fe3ae45a6af2fa2d459eeaf2ed810f4d8c3
Misko Hevery <misko@hevery.com>
2017-02-03 05:41:31 +0000

    559cf9d docs(changelog): add changelog for 4.0.0-beta.6

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-69a4bb0
+ * @license Angular v4.0.0-beta.6-559cf9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.5-69a4bb0');
+    var VERSION = new _angular_core.Version('4.0.0-beta.6-559cf9d');
 
     /**
      * @experimental API related to bootstrapping are still under review.
