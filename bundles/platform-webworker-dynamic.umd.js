fe622a507256fe1f9c0c7dcff171b9243bc617bf
Miško Hevery <misko@hevery.com>
2017-02-07 19:56:14 +0000

    a378aab docs(compiler): incorrect method reference (#14314)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-7036e04
+ * @license Angular v4.0.0-beta.6-a378aab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.6-7036e04');
+    var VERSION = new _angular_core.Version('4.0.0-beta.6-a378aab');
 
     /**
      * @experimental API related to bootstrapping are still under review.
