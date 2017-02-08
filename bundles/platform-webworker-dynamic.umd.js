a177157e0f945d8d414a608d229a42b3f27037b2
Miško Hevery <misko@hevery.com>
2017-02-08 23:10:14 +0000

    24af51a fix(core): view engine - fix perf regressions (#14345)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-7a4c255
+ * @license Angular v4.0.0-beta.6-24af51a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.6-7a4c255');
+    var VERSION = new _angular_core.Version('4.0.0-beta.6-24af51a');
 
     /**
      * @experimental API related to bootstrapping are still under review.
