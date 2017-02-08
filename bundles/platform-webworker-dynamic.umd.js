505f9e534bd64392d7495fae77d26ba28f1b6071
Miško Hevery <misko@hevery.com>
2017-02-08 01:45:50 +0000

    c33fda2 perf: Don’t subclass Error; resulting in smaller binary (#14160)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-2e14130
+ * @license Angular v4.0.0-beta.6-c33fda2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.6-2e14130');
+    var VERSION = new _angular_core.Version('4.0.0-beta.6-c33fda2');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- index.js
+++ index.js
@@ -11,4 +11,5 @@
  * Entry point for all public APIs of the platform-browser-dynamic package.
  */
 export * from './src/platform-webworker-dynamic';
+// This file only reexports content of the `src` folder. Keep it that way.
 //# sourceMappingURL=index.js.map
