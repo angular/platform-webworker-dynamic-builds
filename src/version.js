4b982370044c915f77cad3e84536c57e6e460b7b
Miško Hevery <misko@hevery.com>
2017-02-09 17:28:11 +0000

    d1feb47 build(integration): fix tests (#14371)

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.6-24af51a');
+export var VERSION = new Version('4.0.0-beta.6-d1feb47');
 //# sourceMappingURL=version.js.map
