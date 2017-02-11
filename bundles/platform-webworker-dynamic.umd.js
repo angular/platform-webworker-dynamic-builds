abc09360e7f45e03f461de20775596692aa36049
Misko Hevery <misko@hevery.com>
2017-02-11 05:25:31 +0000

    2769370 ci: fix validate-commit-messages

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-d3f174a
+ * @license Angular v4.0.0-beta.7-2769370
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-d3f174a');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-2769370');
 
     /**
      * @experimental API related to bootstrapping are still under review.
