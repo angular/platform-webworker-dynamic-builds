61b8cae24ff64c1dd44490337fd22cb3e2c5d094
GitHub <noreply@github.com>
2017-02-15 17:16:17 +0000

    731c069 Update .pullapprove.yml

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.7-59b2b65');
+export var VERSION = new Version('4.0.0-beta.7-731c069');
 //# sourceMappingURL=version.js.map
