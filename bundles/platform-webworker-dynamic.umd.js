e34e9c8f99f89973eb7d6daee760b7c49189a9aa
Igor Minar <iminar@google.com>
2017-02-15 05:22:56 +0000

    bb9c7ae feat: RendererV2 integration (#14469)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b4d444a
+ * @license Angular v4.0.0-beta.7-bb9c7ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-b4d444a');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-bb9c7ae');
 
     /**
      * @experimental API related to bootstrapping are still under review.
