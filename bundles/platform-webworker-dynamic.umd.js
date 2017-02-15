f0af5a41934e350c63fc394e18980a836a241bf4
Igor Minar <iminar@google.com>
2017-02-15 17:22:30 +0000

    0e2fd9d build: update .pullapprove (#14506)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-4e7752a
+ * @license Angular v4.0.0-beta.7-0e2fd9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-4e7752a');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-0e2fd9d');
 
     /**
      * @experimental API related to bootstrapping are still under review.
