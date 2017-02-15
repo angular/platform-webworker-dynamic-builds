9d150af06edb5f6e13fd1fe8e2878f1803408509
Igor Minar <iminar@google.com>
2017-02-15 16:59:15 +0000

    4e7752a feat(compiler): integrate compiler with view engine (#14487)

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.7-e9ba7aa');
+export var VERSION = new Version('4.0.0-beta.7-4e7752a');
 //# sourceMappingURL=version.js.map
