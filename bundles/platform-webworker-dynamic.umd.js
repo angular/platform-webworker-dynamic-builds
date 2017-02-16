06ec6bdfecda2b62190c92c2342550c86e29807d
Igor Minar <iminar@google.com>
2017-02-16 19:53:04 +0000

    b36f60c docs(router): fix broken link (#14431)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-57461e9
+ * @license Angular v4.0.0-beta.7-b36f60c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-57461e9');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-b36f60c');
 
     /**
      * @experimental API related to bootstrapping are still under review.
