8551ced059222da424e2d1e4272d7d398393e764
Igor Minar <iminar@google.com>
2017-02-16 20:05:07 +0000

    3dbd9a0 fix(aio): fix staging build travis config (#14539)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b36f60c
+ * @license Angular v4.0.0-beta.7-3dbd9a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-b36f60c');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-3dbd9a0');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.7-b36f60c');
+export var VERSION = new Version('4.0.0-beta.7-3dbd9a0');
 //# sourceMappingURL=version.js.map
