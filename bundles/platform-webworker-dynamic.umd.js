cb352b68aec9c62004cfaf054da4af1888208c2a
Igor Minar <iminar@google.com>
2017-02-16 18:47:15 +0000

    57461e9 docs(router): fix guards API docs (#14528)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-612f120
+ * @license Angular v4.0.0-beta.7-57461e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-612f120');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-57461e9');
 
     /**
      * @experimental API related to bootstrapping are still under review.
