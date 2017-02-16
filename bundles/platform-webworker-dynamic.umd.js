16a297b055b340e0f9ae74d9d64a7e002aa91439
GitHub <noreply@github.com>
2017-02-16 23:46:49 +0000

    8c20aaa fix(aio): use cleanUrls option rather than bruteforce rewriting rule (#14547)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-88eb3b2
+ * @license Angular v4.0.0-beta.7-8c20aaa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-88eb3b2');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-8c20aaa');
 
     /**
      * @experimental API related to bootstrapping are still under review.
