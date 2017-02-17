c4c0e34c6372c10803e8c0d58632bd81c87b030a
Igor Minar <iminar@google.com>
2017-02-17 01:32:07 +0000

    74ce121 feat(aio): temporarily include ‘content’ folder among production assets (#14548)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-b1a79fd
+ * @license Angular v4.0.0-beta.7-74ce121
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.7-b1a79fd');
+    var VERSION = new _angular_core.Version('4.0.0-beta.7-74ce121');
 
     /**
      * @experimental API related to bootstrapping are still under review.
