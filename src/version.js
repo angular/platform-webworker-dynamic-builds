5a1ba121ecbf6d87a3cdf07070151adaf61b6fe6
Igor Minar <iminar@google.com>
2017-02-21 17:33:40 +0000

    bb0460b refactor(core): don’t use switch fall through but rather multiple `if`s

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.8-2ddd1c3');
+export var VERSION = new Version('4.0.0-beta.8-bb0460b');
 //# sourceMappingURL=version.js.map
