2aa3261e606fec62aa22150542fc0769057036ce
Victor Berchet <victor@suumit.com>
2017-02-21 00:37:29 +0000

    601fd3e fix(platform-webworker): integrate review feedback

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-1bdf706
+ * @license Angular v4.0.0-beta.8-601fd3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.8-1bdf706');
+    var VERSION = new _angular_core.Version('4.0.0-beta.8-601fd3e');
 
     /**
      * @experimental API related to bootstrapping are still under review.
--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('4.0.0-beta.8-1bdf706');
+export var VERSION = new Version('4.0.0-beta.8-601fd3e');
 //# sourceMappingURL=version.js.map
