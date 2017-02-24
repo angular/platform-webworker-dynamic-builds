e02420303896e3a57b788be1b9fde2b7830f2a02
GitHub <noreply@github.com>
2017-02-24 21:26:28 +0000

    3c9a46c fix(core): fix directive instantiation (#14715)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-beta.8-436a179
+   * @license Angular v4.0.0-beta.8-3c9a46c
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-beta.8-436a179');
+  var VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
 
   /**
    * @experimental API related to bootstrapping are still under review.
