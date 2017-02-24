3521915befb6f76afa5bae7a7889ca4b13135e3f
Igor Minar <iminar@google.com>
2017-02-24 05:36:36 +0000

    01907ba build(platform-server): update parse5 to v3 (#14538)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-39f56fa
+ * @license Angular v4.0.0-beta.8-01907ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-beta.8-39f56fa');
+const VERSION = new Version('4.0.0-beta.8-01907ba');
 
 /**
  * @experimental API related to bootstrapping are still under review.
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-beta.8-39f56fa
+   * @license Angular v4.0.0-beta.8-01907ba
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-beta.8-39f56fa');
+  var VERSION = new _core.Version('4.0.0-beta.8-01907ba');
 
   /**
    * @experimental API related to bootstrapping are still under review.
