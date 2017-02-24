9f495a03d658f02ccd2c2b2753c046a351cdc002
Igor Minar <iminar@google.com>
2017-02-24 17:58:36 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-32c2fd5
+ * @license Angular v4.0.0-beta.8-93ddd38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-beta.8-32c2fd5');
+const VERSION = new Version('4.0.0-beta.8-93ddd38');
 
 /**
  * @experimental API related to bootstrapping are still under review.
