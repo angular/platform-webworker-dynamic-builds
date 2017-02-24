90abbd435194f5e70335e4ea89199b2d43307edf
Igor Minar <iminar@google.com>
2017-02-24 08:54:05 +0000

    32c2fd5 refactor(animations): change NoOp -> Noop (#14690)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-beta.8-1f3198c
+   * @license Angular v4.0.0-beta.8-32c2fd5
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-beta.8-1f3198c');
+  var VERSION = new _core.Version('4.0.0-beta.8-32c2fd5');
 
   /**
    * @experimental API related to bootstrapping are still under review.
