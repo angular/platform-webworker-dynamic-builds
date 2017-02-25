ad82d5c390bdd5dd13d4bfa3fe30ba241921ec9c
GitHub <noreply@github.com>
2017-02-25 02:44:23 +0000

    f32f4de docs: more changelog fixes

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-26ed262
+ * @license Angular v4.0.0-rc.1-f32f4de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-26ed262');
+const VERSION = new Version('4.0.0-rc.1-f32f4de');
 
 /**
  * @experimental API related to bootstrapping are still under review.
