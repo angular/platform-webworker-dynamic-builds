9581658545bd8d9d5ae404552bffd1d95a04e0b3
Igor Minar <igor@angularjs.org>
2017-02-25 02:44:11 +0000

    26ed262 docs: fix typo in the changelog

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-c7d1b36
+ * @license Angular v4.0.0-rc.1-26ed262
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-c7d1b36');
+const VERSION = new Version('4.0.0-rc.1-26ed262');
 
 /**
  * @experimental API related to bootstrapping are still under review.
