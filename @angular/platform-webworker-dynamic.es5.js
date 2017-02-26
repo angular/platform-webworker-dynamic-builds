d500b16051052b2986dca0a8bafc870314f99b13
GitHub <noreply@github.com>
2017-02-26 04:39:43 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,9 +1,9 @@
 /**
- * @license Angular v4.0.0-rc.1-c868457
+ * @license Angular v4.0.0-rc.1-213e210
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import{ɵPLATFORM_WORKER_UI_ID}from'@angular/common';import{ResourceLoader,platformCoreDynamic}from'@angular/compiler';import{PLATFORM_ID,COMPILER_OPTIONS,createPlatformFactory,Version}from'@angular/core';import{ɵResourceLoaderImpl}from'@angular/platform-browser-dynamic';/**
  * @stable
- */var VERSION=new Version('4.0.0-rc.1-c868457');/**
+ */var VERSION=new Version('4.0.0-rc.1-213e210');/**
  * @experimental API related to bootstrapping are still under review.
  */var platformWorkerAppDynamic=createPlatformFactory(platformCoreDynamic,'workerAppDynamic',[{provide:COMPILER_OPTIONS,useValue:{providers:[{provide:ResourceLoader,useClass:ɵResourceLoaderImpl}]},multi:true},{provide:PLATFORM_ID,useValue:ɵPLATFORM_WORKER_UI_ID}]);export{platformWorkerAppDynamic,VERSION};
