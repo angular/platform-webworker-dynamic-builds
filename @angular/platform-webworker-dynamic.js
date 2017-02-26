b2e5a88f26094522b2d8dcc3d8971b0a7b99b7c7
GitHub <noreply@github.com>
2017-02-26 04:37:01 +0000

    c868457 docs: improve Flat ES Modules docs

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-f32f4de
+ * @license Angular v4.0.0-rc.1-c868457
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-f32f4de');
+const VERSION = new Version('4.0.0-rc.1-c868457');
 
 /**
  * @experimental API related to bootstrapping are still under review.
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-f32f4de
+   * @license Angular v4.0.0-rc.1-c868457
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
+  var VERSION = new _core.Version('4.0.0-rc.1-c868457');
 
   /**
    * @experimental API related to bootstrapping are still under review.
