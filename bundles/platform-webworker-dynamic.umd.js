f2005525fa9c67f811b6b592794a0f90858acb95
Igor Minar <iminar@google.com>
2017-02-27 19:47:31 +0000

    77682a3 fix(compiler): quote `animation` in `RendererTypeV2` and skip if empty (#14773)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-213e210
+   * @license Angular v4.0.0-rc.1-77682a3
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-213e210');
+  var VERSION = new _core.Version('4.0.0-rc.1-77682a3');
 
   /**
    * @experimental API related to bootstrapping are still under review.
