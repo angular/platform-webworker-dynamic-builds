5036f2e0329ede8921d054dcd29db85dfa197feb
Igor Minar <iminar@google.com>
2017-02-27 19:48:44 +0000

    f356041 docs: update version in changelog to reflect RC.1 (not 0) (#14740)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-77682a3
+   * @license Angular v4.0.0-rc.1-f356041
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-77682a3');
+  var VERSION = new _core.Version('4.0.0-rc.1-f356041');
 
   /**
    * @experimental API related to bootstrapping are still under review.
