3f1aad4415a309271f002433ca0ecce83064382a
Igor Minar <iminar@google.com>
2017-02-28 00:38:52 +0000

    e58cb7b docs: fix animations module instructions in the changelog (#14729)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-5caab71
+ * @license Angular v4.0.0-rc.1-e58cb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-5caab71');
+const VERSION = new Version('4.0.0-rc.1-e58cb7b');
 
 /**
  * @experimental API related to bootstrapping are still under review.
