060b41bf711304e641c4f54bf446e10fb22107c1
Igor Minar <iminar@google.com>
2017-02-28 07:29:09 +0000

    126fda2 perf: delete pre-view-engine core, compiler, platform-browser, etc code (#14788)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-e58cb7b
+   * @license Angular v4.0.0-rc.1-126fda2
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-e58cb7b');
+  var VERSION = new _core.Version('4.0.0-rc.1-126fda2');
 
   /**
    * @experimental API related to bootstrapping are still under review.
