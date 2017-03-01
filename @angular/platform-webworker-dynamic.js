88dc769169adce1e0848c4c4ffeb553719c07b41
Igor Minar <iminar@google.com>
2017-03-01 21:44:34 +0000

    7a66a41 test(language-service): test `@angular/language-service` can be loaded by tsserver.js (#14721)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-79fc1e3
+ * @license Angular v4.0.0-rc.1-7a66a41
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-79fc1e3');
+const VERSION = new Version('4.0.0-rc.1-7a66a41');
 
 /**
  * @experimental API related to bootstrapping are still under review.
