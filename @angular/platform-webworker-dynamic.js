06d47b6cae3f28a6d129b8b0e49dcfd01439b4f9
Igor Minar <iminar@google.com>
2017-03-01 16:23:41 +0000

    bc9e1de docs: changelog - fix else example (#14766)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-fc9e6b2
+ * @license Angular v4.0.0-rc.1-bc9e1de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-fc9e6b2');
+const VERSION = new Version('4.0.0-rc.1-bc9e1de');
 
 /**
  * @experimental API related to bootstrapping are still under review.
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-fc9e6b2
+   * @license Angular v4.0.0-rc.1-bc9e1de
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-fc9e6b2');
+  var VERSION = new _core.Version('4.0.0-rc.1-bc9e1de');
 
   /**
    * @experimental API related to bootstrapping are still under review.
