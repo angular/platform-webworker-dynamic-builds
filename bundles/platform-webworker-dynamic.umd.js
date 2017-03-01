09c53baac1c8f762a952fda43171b2a5dd2e8dbe
Igor Minar <iminar@google.com>
2017-03-01 19:37:33 +0000

    47bdc2b fix(platform-server): don't setup Testability and TestabilityRegistry on the server (#14510)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-49b462e
+   * @license Angular v4.0.0-rc.1-47bdc2b
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-49b462e');
+  var VERSION = new _core.Version('4.0.0-rc.1-47bdc2b');
 
   /**
    * @experimental API related to bootstrapping are still under review.
