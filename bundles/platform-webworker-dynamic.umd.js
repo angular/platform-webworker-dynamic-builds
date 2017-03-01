9e6b4161bf59baaab9fdb17c7414665d76c33ace
Igor Minar <iminar@google.com>
2017-03-01 20:37:47 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-9402df9
+   * @license Angular v4.0.0-rc.1-79fc1e3
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-9402df9');
+  var VERSION = new _core.Version('4.0.0-rc.1-79fc1e3');
 
   /**
    * @experimental API related to bootstrapping are still under review.
