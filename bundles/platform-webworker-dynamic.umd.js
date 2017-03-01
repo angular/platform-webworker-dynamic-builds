505c165e0227ec638c81a260b766ac51815eb237
Igor Minar <iminar@google.com>
2017-03-01 02:46:15 +0000

    5ba55b0 fix(compiler): fix identifier names of `EMPTY_MAP` / `EMPTY_ARRAY` (#14806)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-126fda2
+   * @license Angular v4.0.0-rc.1-5ba55b0
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-126fda2');
+  var VERSION = new _core.Version('4.0.0-rc.1-5ba55b0');
 
   /**
    * @experimental API related to bootstrapping are still under review.
