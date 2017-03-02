94fdb1da798eddcd4c42ace7a3d63976ab62ce24
Igor Minar <iminar@google.com>
2017-03-02 00:19:18 +0000

    3168ef7 build: fix secondary entry point es5 output and core Rx references (#14820)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,9 +1,25 @@
 /**
- * @license Angular v4.0.0-rc.1-b6e6fc1
+ * @license Angular v4.0.0-rc.1-3168ef7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
- */import{ɵPLATFORM_WORKER_UI_ID}from'@angular/common';import{ResourceLoader,platformCoreDynamic}from'@angular/compiler';import{PLATFORM_ID,COMPILER_OPTIONS,createPlatformFactory,Version}from'@angular/core';import{ɵResourceLoaderImpl}from'@angular/platform-browser-dynamic';/**
+ */
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader, platformCoreDynamic } from '@angular/compiler';
+import { PLATFORM_ID, COMPILER_OPTIONS, createPlatformFactory, Version } from '@angular/core';
+import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+
+/**
  * @stable
- */var VERSION=new Version('4.0.0-rc.1-b6e6fc1');/**
+ */
+var VERSION = new Version('4.0.0-rc.1-3168ef7');
+
+/**
  * @experimental API related to bootstrapping are still under review.
- */var platformWorkerAppDynamic=createPlatformFactory(platformCoreDynamic,'workerAppDynamic',[{provide:COMPILER_OPTIONS,useValue:{providers:[{provide:ResourceLoader,useClass:ɵResourceLoaderImpl}]},multi:true},{provide:PLATFORM_ID,useValue:ɵPLATFORM_WORKER_UI_ID}]);export{platformWorkerAppDynamic,VERSION};
+ */
+var platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'workerAppDynamic', [{
+  provide: COMPILER_OPTIONS,
+  useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl }] },
+  multi: true
+}, { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }]);
+
+export { platformWorkerAppDynamic, VERSION };
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-b6e6fc1
+   * @license Angular v4.0.0-rc.1-3168ef7
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-b6e6fc1');
+  var VERSION = new _core.Version('4.0.0-rc.1-3168ef7');
 
   /**
    * @experimental API related to bootstrapping are still under review.
