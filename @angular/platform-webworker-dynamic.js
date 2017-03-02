2d23aafb42833a6b52b88831675f69a5419ffb9a
Igor Minar <iminar@google.com>
2017-03-02 01:35:11 +0000

    9560ad8 fix(animations): make animations work in AOT (#14775)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-3168ef7
+ * @license Angular v4.0.0-rc.1-9560ad8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-3168ef7');
+const VERSION = new Version('4.0.0-rc.1-9560ad8');
 
 /**
  * @experimental API related to bootstrapping are still under review.
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.1-3168ef7
+   * @license Angular v4.0.0-rc.1-9560ad8
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.1-3168ef7');
+  var VERSION = new _core.Version('4.0.0-rc.1-9560ad8');
 
   /**
    * @experimental API related to bootstrapping are still under review.
