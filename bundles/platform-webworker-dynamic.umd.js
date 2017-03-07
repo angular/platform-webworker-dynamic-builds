f15da23f67fbe9576ba1ce04ec6b71143fed798a
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 17:54:17 +0000

    07122f0 fix(upgrade): populate upgraded component's view before creating the controller (#14289)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.2-ebd4463
+   * @license Angular v4.0.0-rc.2-07122f0
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.2-ebd4463');
+  var VERSION = new _core.Version('4.0.0-rc.2-07122f0');
 
   /**
    * @experimental API related to bootstrapping are still under review.
