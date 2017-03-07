e750f0deb00cf637149afa88bbaf2bd519258c7c
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 19:41:36 +0000

    1cff125 refactor: remove old compiler options (#14891)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.2-07122f0
+   * @license Angular v4.0.0-rc.2-1cff125
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.2-07122f0');
+  var VERSION = new _core.Version('4.0.0-rc.2-1cff125');
 
   /**
    * @experimental API related to bootstrapping are still under review.
