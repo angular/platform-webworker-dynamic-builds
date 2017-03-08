596c766936dbcb764910dc52a2f49db3e16d6898
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 02:44:15 +0000

    174d4c8 ci(aio): do not deploy PR if preconditions not met

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-ad3b44a
+ * @license Angular v4.0.0-rc.2-174d4c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.2-ad3b44a');
+const VERSION = new Version('4.0.0-rc.2-174d4c8');
 
 /**
  * @experimental API related to bootstrapping are still under review.
