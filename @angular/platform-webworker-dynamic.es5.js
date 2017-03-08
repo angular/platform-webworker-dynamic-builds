115a5963bd05145145285408d58b8affc73138c8
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 21:29:30 +0000

    5ad5301 feat(aio): add API list page (#14899)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-174d4c8
+ * @license Angular v4.0.0-rc.2-5ad5301
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.2-174d4c8');
+var VERSION = new Version('4.0.0-rc.2-5ad5301');
 
 /**
  * @experimental API related to bootstrapping are still under review.
--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -24,11 +24,11 @@
    * @stable
    */
   /**
-   * @license Angular v4.0.0-rc.2-174d4c8
+   * @license Angular v4.0.0-rc.2-5ad5301
    * (c) 2010-2017 Google, Inc. https://angular.io/
    * License: MIT
    */
-  var VERSION = new _core.Version('4.0.0-rc.2-174d4c8');
+  var VERSION = new _core.Version('4.0.0-rc.2-5ad5301');
 
   /**
    * @experimental API related to bootstrapping are still under review.
