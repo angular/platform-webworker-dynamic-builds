82259a675a43b391807173644c00fecc379f7e78
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:20:21 +0000

    ab40fcb docs(aio): fix relative links

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6497633
+ * @license Angular v4.0.0-rc.3-ab40fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-6497633');
+const VERSION = new Version('4.0.0-rc.3-ab40fcb');
 
 /**
  * @experimental API related to bootstrapping are still under review.
