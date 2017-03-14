e47ddfc9470f81e44f9baf7afccd789d67c76937
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:03:37 +0000

    eedca09 build(aio): generate the api-list.json file from the API docs

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ab40fcb
+ * @license Angular v4.0.0-rc.3-eedca09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-ab40fcb');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-eedca09');
     /**
      * @experimental API related to bootstrapping are still under review.
      */
