ab605d45b6ee50534896ce67b08cfb2ba4a36bd6
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:25:44 +0000

    0aad270 refactor(platform-browser): move platform-browser/animations to animations/browser (#15130)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-221899a
+ * @license Angular v4.0.0-rc.3-0aad270
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-221899a');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-0aad270');
     /**
      * @experimental API related to bootstrapping are still under review.
      */
