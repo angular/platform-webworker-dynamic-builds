3ff731863c6e00075091d2d8117ebe8bd23a1a52
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:52:17 +0000

    2c5a671 fix: don't instantiate providers with ngOnDestroy eagerly. (#15070)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-0aad270
+ * @license Angular v4.0.0-rc.3-2c5a671
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-0aad270');
+var VERSION = new Version('4.0.0-rc.3-2c5a671');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
