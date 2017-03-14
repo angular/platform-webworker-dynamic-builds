298ef94e3f280d906627ebd1c607c90769bc8b27
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:52 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-d4205bb');
+var VERSION = new Version('4.0.0-rc.3-2f250c1');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-d4205bb');
+const VERSION = new Version('4.0.0-rc.3-2f250c1');
 
 /**
  * @experimental API related to bootstrapping are still under review.
