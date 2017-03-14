c819eed2a3d9b66bc99d021bdad955b4d8852c3c
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:45 +0000

    778f7d6 fix(forms): remove equalsTo validator (#15050)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2c5a671
+ * @license Angular v4.0.0-rc.3-778f7d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-2c5a671');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-778f7d6');
     /**
      * @experimental API related to bootstrapping are still under review.
      */
