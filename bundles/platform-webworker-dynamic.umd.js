0b3a55d717c3103725f3bf075748385284b5469c
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 19:09:38 +0000

    221899a ci: fix incorrect github credetials (#15146)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-1c1085b
+ * @license Angular v4.0.0-rc.3-221899a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-1c1085b');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-221899a');
     /**
      * @experimental API related to bootstrapping are still under review.
      */
