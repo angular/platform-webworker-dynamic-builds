7b56c98ce34058d1cc4b9368172357aa168d15af
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 16:43:30 +0000

    5c0ea20 build(tsc-wrapped): use tsickleCompilerHost for initial file load

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-4f7d62a
+ * @license Angular v4.0.0-rc.3-5c0ea20
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-4f7d62a');
+var VERSION = new Version('4.0.0-rc.3-5c0ea20');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
