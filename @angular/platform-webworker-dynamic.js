a9b054107456ef09d8bdbd0ccd1c18c2d0ec2aa6
Igor Minar <igor@angularjs.org>
2017-03-15 04:26:09 +0000

    ec548ad feat(aio): hide search results when click outside search elements

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-423bfb0
+ * @license Angular v4.0.0-rc.3-ec548ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-423bfb0');
+const VERSION = new Version('4.0.0-rc.3-ec548ad');
 
 /**
  * @experimental API related to bootstrapping are still under review.
