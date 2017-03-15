ef359a866e8fa3d393607692228bbe3bde35db65
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:29:12 +0000

    959a03a fix(compiler): fix utf8encode, move to sharted utils, add tests (#15076)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3b1956b
+ * @license Angular v4.0.0-rc.3-959a03a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-3b1956b');
+const VERSION = new Version('4.0.0-rc.3-959a03a');
 
 /**
  * @experimental API related to bootstrapping are still under review.
