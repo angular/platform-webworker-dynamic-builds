f062be1b70d8029c2394d5f2eda10ada83027874
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 18:58:11 +0000

    a3e32fb feat(upgrade): use `ComponentFactory.inputs/outputs/ngContentSelectors`

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-36ce0af
+ * @license Angular v4.0.0-rc.3-a3e32fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-36ce0af');
+var VERSION = new Version('4.0.0-rc.3-a3e32fb');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
