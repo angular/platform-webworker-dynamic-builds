7c12819cc4dddf38cac78138c831ea7e01b88f47
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:30:57 +0000

    3c15916 fix(compiler): Improve error message for missing annotations (#14724)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-959a03a
+ * @license Angular v4.0.0-rc.3-3c15916
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-959a03a');
+var VERSION = new Version('4.0.0-rc.3-3c15916');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
