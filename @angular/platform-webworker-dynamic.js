63d67e641e79eb5ecc0800625cf3b3dd8ada2453
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:36 +0000

    013d806 fix(platform-server): handle styles with extra ':'s correctly (#15189)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-dd36d41
+ * @license Angular v4.0.0-rc.3-013d806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-dd36d41');
+const VERSION = new Version('4.0.0-rc.3-013d806');
 
 /**
  * @experimental API related to bootstrapping are still under review.
