f8c4c5074abb213dcf0f026620690a17e4a5411f
GitHub <noreply@github.com>
2017-03-17 17:02:11 +0000

    7fb4528 fix(compiler-cli): update the tsc-wrapped dependency version (#15226)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-b7212f5
+ * @license Angular v4.0.0-rc.4-7fb4528
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.4-b7212f5');
+const VERSION = new Version('4.0.0-rc.4-7fb4528');
 
 /**
  * @experimental API related to bootstrapping are still under review.
