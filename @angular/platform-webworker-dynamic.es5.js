3538b5748374f06a46d7bc13dbb6e40c69c59640
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 03:39:34 +0000

    b7212f5 docs: add changelog for 4.0.0-rc.4

---
+++ @angular/platform-webworker-dynamic.es5.js
@@ -0,0 +1,26 @@
+/**
+ * @license Angular v4.0.0-rc.4-b7212f5
+ * (c) 2010-2017 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader, platformCoreDynamic } from '@angular/compiler';
+import { PLATFORM_ID, COMPILER_OPTIONS, createPlatformFactory, Version } from '@angular/core';
+import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+/**
+ * @stable
+ */
+var VERSION = new Version('4.0.0-rc.4-b7212f5');
+/**
+ * @experimental API related to bootstrapping are still under review.
+ */
+var platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl }] },
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
+]);
+export { platformWorkerAppDynamic, VERSION };
+//# sourceMappingURL=platform-webworker-dynamic.es5.js.map
