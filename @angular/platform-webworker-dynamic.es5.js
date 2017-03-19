1fa57db267d16d8986ea137d9517ed33de476335
Miško Hevery <misko@hevery.com>
2017-03-19 18:22:08 +0000

    f634c62 test: add systemjs+umd integration test (#14196)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-80075af
+ * @license Angular v4.0.0-rc.5-f634c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-80075af');
+var VERSION = new Version('4.0.0-rc.5-f634c62');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
