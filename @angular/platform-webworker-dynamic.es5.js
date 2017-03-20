92d5e3b1a52ca600567f378d64b3bc8fd279fb2d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 12:28:32 +0000

    6d4f5ce build(aio): update to dgeni-packages 0.17.0

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d1e82b0
+ * @license Angular v4.0.0-rc.5-6d4f5ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-d1e82b0');
+var VERSION = new Version('4.0.0-rc.5-6d4f5ce');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d1e82b0
+ * @license Angular v4.0.0-rc.5-6d4f5ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11,7 +11,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.5-d1e82b0');
+const VERSION = new Version('4.0.0-rc.5-6d4f5ce');
 
 /**
  * @experimental API related to bootstrapping are still under review.
