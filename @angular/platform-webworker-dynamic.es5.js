8d4d588c55afeb13825826f559d91e810835fbb1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 17:32:54 +0000

    d5a6e69 build: update to angular 4.0.0-rc.4 and service-worker@1.0.0-beta.7

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-60c8368
+ * @license Angular v4.0.0-rc.5-d5a6e69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-60c8368');
+var VERSION = new Version('4.0.0-rc.5-d5a6e69');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
