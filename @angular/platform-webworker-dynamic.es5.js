800138299bebe9bc7c7aedb3112aa67d0efa6fb9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 12:09:36 +0000

    ea979cf build(aio): add version into navigation.json

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-1b9c809
+ * @license Angular v4.0.0-rc.5-ea979cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-1b9c809');
+var VERSION = new Version('4.0.0-rc.5-ea979cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
