c28cdc73fd3594168da007f83ce3f2d8706e1503
Igor Minar <igor@angularjs.org>
2017-03-21 04:36:07 +0000

    b7ba331 build: update to angular 4.0.0-rc.4 and service-worker@1.0.0-beta.7

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,18 +1,37 @@
 /**
- * @license Angular v4.0.0-rc.5-d5a6e69
+ * @license Angular v4.0.0-rc.5-b7ba331
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
 import { ResourceLoader, platformCoreDynamic } from '@angular/compiler';
-import { PLATFORM_ID, COMPILER_OPTIONS, createPlatformFactory, Version } from '@angular/core';
+import { COMPILER_OPTIONS, PLATFORM_ID, Version, createPlatformFactory } from '@angular/core';
 import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 
 /**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the common package.
+ */
+/**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.5-d5a6e69');
+const VERSION = new Version('4.0.0-rc.5-b7ba331');
 
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 /**
  * @experimental API related to bootstrapping are still under review.
  */
@@ -24,6 +43,21 @@ const platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'wor
     },
     { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
 ]);
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the platform-browser-dynamic package.
+ */
+
+// This file only reexports content of the `src` folder. Keep it that way.
 
 export { platformWorkerAppDynamic, VERSION };
 //# sourceMappingURL=platform-webworker-dynamic.js.map
