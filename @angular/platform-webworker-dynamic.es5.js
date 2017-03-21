c228f601fd1a4e29975921d06d37b17827ed8f08
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 17:16:38 +0000

    f40f915 build(aio): upgrade to rxjs@5.2.0

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-11d5309
+ * @license Angular v4.0.0-rc.5-f40f915
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-11d5309');
+var VERSION = new Version('4.0.0-rc.5-f40f915');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
