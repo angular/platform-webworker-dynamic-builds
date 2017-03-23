54ae62f636d2065ef1a4c2ea649f389f12f067ba
Victor Berchet <victor@suumit.com>
2017-03-23 20:56:17 +0000

    0dda01e fix(compiler): correctly handle when `toString` is exported (#15430)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-92084f2
+ * @license Angular v4.0.0-rc.6-0dda01e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-92084f2');
+var VERSION = new Version('4.0.0-rc.6-0dda01e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
