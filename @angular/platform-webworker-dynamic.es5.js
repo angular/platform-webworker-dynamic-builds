fd62621cfbc3f64832f795a6299548279989d6c1
Igor Minar <iminar@google.com>
2017-03-23 00:35:39 +0000

    9319b5f docs(common): update ngFor docs to new `as` syntax (#15166)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-edb2571
+ * @license Angular v4.0.0-rc.5-9319b5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-edb2571');
+var VERSION = new Version('4.0.0-rc.5-9319b5f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
