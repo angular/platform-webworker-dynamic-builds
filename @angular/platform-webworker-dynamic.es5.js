5cbe85c78154e0b89bbe5de5d35f5f1f5c80f9e3
Victor Berchet <victor@suumit.com>
2017-03-23 20:09:58 +0000

    92084f2 fix(platform-browser): setAttribute should work with xmlns namespace (#14874)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-08f2f08
+ * @license Angular v4.0.0-rc.6-92084f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-08f2f08');
+var VERSION = new Version('4.0.0-rc.6-92084f2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
