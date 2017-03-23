9b43dfa1d03cb1636aa9d56043d0da214f6e4c69
Victor Berchet <victor@suumit.com>
2017-03-23 17:44:47 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-73808dd');
+var VERSION = new Version('4.0.0-rc.6-376088d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
