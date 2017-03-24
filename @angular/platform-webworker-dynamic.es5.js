de0adf5c78fca97151aca4dcc79b9450fe4e4fb1
GitHub <noreply@github.com>
2017-03-24 00:37:35 +0000

    db0dca3 docs: add a note about exclusion of symbols prefixed with ɵ from our public api surface (#15440)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-7a715b2');
+var VERSION = new Version('4.0.0-db0dca3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
