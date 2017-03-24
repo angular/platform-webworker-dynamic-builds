610731fff5ef277b0579b0e29d210bd2074c5143
GitHub <noreply@github.com>
2017-03-24 02:41:52 +0000

    b2b1195 docs: update the contributing link one more time :-)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c65b754
+ * @license Angular v4.0.0-b2b1195
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-c65b754');
+const VERSION = new Version('4.0.0-b2b1195');
 
 /**
  * @license
