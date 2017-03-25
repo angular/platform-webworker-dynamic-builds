9871f1f241d4e4839fcb87e0c262ab7a2a3498d9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 21:50:41 +0000

    1616cae build(aio): add renderExamples processor

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-c9e51d9');
+var VERSION = new Version('4.0.0-1616cae');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-c9e51d9');
+const VERSION = new Version('4.0.0-1616cae');
 
 /**
  * @license
