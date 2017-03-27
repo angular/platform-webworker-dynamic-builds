4ddbbab87b143b4d8772d0133f3dea292b20906a
Victor Berchet <victor@suumit.com>
2017-03-27 19:13:38 +0000

    15662ef build(aio): update project config for @angular/cli v1.0.0

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-816b389
+ * @license Angular v4.0.0-15662ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-816b389');
+var VERSION = new Version('4.0.0-15662ef');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
