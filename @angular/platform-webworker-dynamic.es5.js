4c31da1d5d40c5a687d5e66f89b81468e368e18b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:38:05 +0000

    a6fd22c docs(aio): fix typo

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-1616cae');
+var VERSION = new Version('4.0.0-a6fd22c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
