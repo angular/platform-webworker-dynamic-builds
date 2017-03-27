b33a24f69e1c23ddc5147d414b488a6bbc48ccbb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 11:42:27 +0000

    837ed78 feat(aio): add code-example and code-tabs

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-52ea193');
+var VERSION = new Version('4.0.0-837ed78');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-52ea193');
+const VERSION = new Version('4.0.0-837ed78');
 
 /**
  * @license
