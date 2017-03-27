b7848d110d50d2634e660381bad6942f77c47960
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:47:49 +0000

    228238e refactor(aio): move SearchService initialisation into SearchBoxComponent

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a6fd22c
+ * @license Angular v4.0.0-228238e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-a6fd22c');
+var VERSION = new Version('4.0.0-228238e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
