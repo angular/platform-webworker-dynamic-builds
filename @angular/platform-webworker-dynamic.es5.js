db82fa12ba0ca7d9d1c7542478a343198a4b5527
Victor Berchet <victor@suumit.com>
2017-03-27 17:02:35 +0000

    816b389 docs: in doc comments, replace [aA]ngular2 with Angular (#15463)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6605dd1
+ * @license Angular v4.0.0-816b389
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-6605dd1');
+var VERSION = new Version('4.0.0-816b389');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6605dd1
+ * @license Angular v4.0.0-816b389
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-6605dd1');
+const VERSION = new Version('4.0.0-816b389');
 
 /**
  * @license
