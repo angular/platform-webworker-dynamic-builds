cf3dd23958ed177f50697ce0df2c6b464170c83d
Victor Berchet <victor@suumit.com>
2017-03-28 17:09:39 +0000

    8e03f65 refactor(router): improve flatten fn

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b7fa5de
+ * @license Angular v4.0.0-8e03f65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-b7fa5de');
+var VERSION = new Version('4.0.0-8e03f65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
