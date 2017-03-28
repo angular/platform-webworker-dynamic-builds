b58a180d7df2b011983f4c2df955d6b80b906e70
GitHub <noreply@github.com>
2017-03-28 23:07:25 +0000

    93d48f1 fix(core): fix the key/value differ (#15539)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-861953c
+ * @license Angular v4.0.0-93d48f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-861953c');
+var VERSION = new Version('4.0.0-93d48f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
