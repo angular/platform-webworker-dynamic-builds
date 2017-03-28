4453878b0c1863fc25847c0c6d957f75b0d05e66
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 10:08:47 +0000

    9995191 fix(aio): selectedNodes should work for URLs ending with a slash

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9e0d7be
+ * @license Angular v4.0.0-9995191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-9e0d7be');
+var VERSION = new Version('4.0.0-9995191');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
