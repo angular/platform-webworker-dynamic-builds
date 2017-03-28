8383420001e7e09eb2aad2f7ab5a4ec1d5e25173
Victor Berchet <victor@suumit.com>
2017-03-28 21:59:11 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-aa16ccd');
+var VERSION = new Version('4.0.0-861953c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
