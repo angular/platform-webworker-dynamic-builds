2c16d3928bf515a74738e9e1d86b467b6259475b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:29:15 +0000

    2454a3b build(aio): process contributor.json file

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-03fb766
+ * @license Angular v4.1.0-beta.0-2454a3b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-03fb766');
+var VERSION = new Version('4.1.0-beta.0-2454a3b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
