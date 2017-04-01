aca750947fb16b0cc1fd9c2749cb0f9d7568a84e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 11:04:25 +0000

    c8be396 style(aio): fix minor syntax error

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-97deb01
+ * @license Angular v4.1.0-beta.0-c8be396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-97deb01');
+var VERSION = new Version('4.1.0-beta.0-c8be396');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
