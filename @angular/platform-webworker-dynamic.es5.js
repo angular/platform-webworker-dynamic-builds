0c236c99eb38beb7e202e9ae7aa596e3525ad1d2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 21:52:12 +0000

    309bae0 feat(aio): display “code sample missing” message when no code sample

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-ae70293
+ * @license Angular v4.1.0-beta.0-309bae0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-ae70293');
+var VERSION = new Version('4.1.0-beta.0-309bae0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
