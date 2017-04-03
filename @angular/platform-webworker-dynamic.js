f74ae207256962db79172c0a3bf4aa6bc218c649
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 23:04:28 +0000

    25132bf feat(aio): marketing page styles

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0e38bf9
+ * @license Angular v4.1.0-beta.0-25132bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-beta.0-0e38bf9');
+const VERSION = new Version('4.1.0-beta.0-25132bf');
 
 /**
  * @license
