6f983f34995f5c7baa05577c24dca6201b7ce097
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 21:53:10 +0000

    bde9771 fix(language-service): parse extended i18n forms

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-fe0d02f
+ * @license Angular v4.1.0-beta.0-bde9771
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-fe0d02f');
+var VERSION = new Version('4.1.0-beta.0-bde9771');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
