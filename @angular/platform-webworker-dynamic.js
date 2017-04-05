ac4cdd3405d296144a6e2ddcdfeb73fa78df5526
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 01:38:22 +0000

    c933b75 feat(animations): wip: introduce a wave of new animation features

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-64f1bf6
+ * @license Angular v4.1.0-beta.0-c933b75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-beta.0-64f1bf6');
+const VERSION = new Version('4.1.0-beta.0-c933b75');
 
 /**
  * @license
