977efc019161761c84dfce1ff0dfd08607e3ed2b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-10 13:04:26 +0000

    4c566db fix(aio): api search input fix and style

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-bde9771
+ * @license Angular v4.1.0-beta.0-4c566db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-bde9771');
+var VERSION = new Version('4.1.0-beta.0-4c566db');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
