f609293ba0167f06333d047ae40808d490f2c437
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:26:21 +0000

    909264f hr update

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0614754
+ * @license Angular v4.1.0-beta.0-909264f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-0614754');
+var VERSION = new Version('4.1.0-beta.0-909264f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
