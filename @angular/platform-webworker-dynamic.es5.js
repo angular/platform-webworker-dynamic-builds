f4490a6bbede12249ee283e0931304ee9b8fdcae
Hans <hans@hansl.ca>
2017-04-12 21:32:48 +0000

    f09fd6e test(aio): add sidenav tests and refactor related tests

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-909264f
+ * @license Angular v4.1.0-beta.0-f09fd6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-909264f');
+var VERSION = new Version('4.1.0-beta.0-f09fd6e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
