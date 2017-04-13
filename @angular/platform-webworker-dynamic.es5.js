3acc7f24dc09bdf6134e1905aac7fa2cefd33464
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:11:33 +0000

    70b1d6d fix(tsc-wrapped): collect new expressions with no arguments (#15908)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9394835
+ * @license Angular v4.1.0-beta.1-70b1d6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-9394835');
+var VERSION = new Version('4.1.0-beta.1-70b1d6d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
