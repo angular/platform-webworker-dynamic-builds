a7ff67f642b537d47ba921015770128de9652901
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 00:29:28 +0000

    6a2e08d fix(forms): Update types for TypeScript nullability support (#15859)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-fdb3f26
+ * @license Angular v4.1.0-beta.1-6a2e08d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-fdb3f26');
+var VERSION = new Version('4.1.0-beta.1-6a2e08d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
