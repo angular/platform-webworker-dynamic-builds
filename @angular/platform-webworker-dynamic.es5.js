3c2738e2307cf1e6a39e90ab20e4f9936ecd8ed8
GitHub <noreply@github.com>
2017-04-14 01:19:24 +0000

    2e47a0d Revert "fix(forms): Update types for TypeScript nullability support (#15859)" (#15977)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-6a2e08d');
+var VERSION = new Version('4.1.0-beta.1-2e47a0d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
