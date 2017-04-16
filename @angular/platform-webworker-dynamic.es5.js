664d95a20745ff297b93b2dc5edcac94cc9d254c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:49:22 +0000

    11b54f6 feat(aio): edit page link styling and placement

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-4a3c66f');
+var VERSION = new Version('4.1.0-beta.1-11b54f6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
