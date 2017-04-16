7d6779d28970f2cc65ce41e3cde9767e5513b9e5
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-16 20:59:45 +0000

    7c418a7 style(aio): add missing semi-colon

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-58f080a
+ * @license Angular v4.1.0-beta.1-7c418a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-58f080a');
+var VERSION = new Version('4.1.0-beta.1-7c418a7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
