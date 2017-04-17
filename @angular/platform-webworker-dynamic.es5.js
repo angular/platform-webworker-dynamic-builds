1ac20bbb64108966fc5d8c793ee5f968f9eb40e3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 07:09:29 +0000

    e20a722 feat(aio): when select node in sidebar close other expanded nodes

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-abecf3e
+ * @license Angular v4.1.0-beta.1-e20a722
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-abecf3e');
+var VERSION = new Version('4.1.0-beta.1-e20a722');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
