183cd20ba6f73db85a0aef6e6f3bd46eb0186801
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:07:27 +0000

    2ddf3bc fix(language-service): look for type constructors on canonical symbol

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-0a3a9af
+ * @license Angular v4.1.0-beta.1-2ddf3bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-0a3a9af');
+var VERSION = new Version('4.1.0-beta.1-2ddf3bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
