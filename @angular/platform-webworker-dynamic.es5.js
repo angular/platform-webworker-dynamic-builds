513b3a7fbd483afe037354c069bba19b1d955256
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 13:21:02 +0000

    79db6c3 feat(aio): about page style changes

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-76269f4
+ * @license Angular v4.1.0-beta.1-79db6c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-76269f4');
+var VERSION = new Version('4.1.0-beta.1-79db6c3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
