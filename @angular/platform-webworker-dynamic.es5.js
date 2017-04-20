fc65e08bd9241b24b74cda4e24a51bb69153e05f
Miško Hevery <misko@hevery.com>
2017-04-20 04:31:04 +0000

    745731e fix(compiler): make I18NHtmlParser provider AoT-compliant (#15980)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-bfd5f27
+ * @license Angular v4.1.0-beta.1-745731e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-bfd5f27');
+var VERSION = new Version('4.1.0-beta.1-745731e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
