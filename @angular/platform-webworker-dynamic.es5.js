c3171742d91d1f9c48e3f0489a8a72418c50c69f
Miško Hevery <misko@hevery.com>
2017-04-21 20:56:43 +0000

    7a8bd99 docs(aio): correct some minor typos in the form validation cookbook (#16068)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-7520ddc
+ * @license Angular v4.1.0-rc.0-7a8bd99
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-7520ddc');
+var VERSION = new Version('4.1.0-rc.0-7a8bd99');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
