c19e32d5758ad1d8702d6306e0cbad13cbc79d0b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 12:32:09 +0000

    c3fa880 fix(aio): use SVG icons for page load sensitive UI

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-3cad5da
+ * @license Angular v4.1.0-rc.0-c3fa880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-3cad5da');
+var VERSION = new Version('4.1.0-rc.0-c3fa880');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
