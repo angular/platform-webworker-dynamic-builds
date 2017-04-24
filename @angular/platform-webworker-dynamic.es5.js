1723be92373866f4844b7c7e425ef915eab0d936
Miško Hevery <misko@hevery.com>
2017-04-24 17:10:34 +0000

    2991221 docs(aio): remove duplicate `as` (#16265)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-d7719aa');
+var VERSION = new Version('4.1.0-rc.0-2991221');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
