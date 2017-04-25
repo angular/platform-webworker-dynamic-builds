85ee610c3897abfc82a640f04edc8dc82ac1c0cd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:24:45 +0000

    23e6502 fix(aio): copy button placement fix

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ff0f53c
+ * @license Angular v4.1.0-rc.0-23e6502
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-rc.0-ff0f53c');
+const VERSION = new Version('4.1.0-rc.0-23e6502');
 
 /**
  * @license
