199655bf2b6fb343c53a570649c4145951b0ee53
Miško Hevery <misko@hevery.com>
2017-04-28 23:31:33 +0000

    270d694 docs(common): fix API docs for NgComponentOutlet (#16411)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a4de214
+ * @license Angular v4.1.0-270d694
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-a4de214');
+const VERSION = new Version('4.1.0-270d694');
 
 /**
  * @license
