a7862202e4800603dda3a9ab8b0739af95cdba5b
Miško Hevery <misko@hevery.com>
2017-04-30 16:31:00 +0000

    de8de84 test(aio): confirm ga tracks user’s # fragment clicks within a page (#16441)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8d300ff
+ * @license Angular v4.1.0-de8de84
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-8d300ff');
+var VERSION = new Version('4.1.0-de8de84');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
