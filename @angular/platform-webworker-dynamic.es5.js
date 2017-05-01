c79137eb3a1779803cae62b103cbd53690462d22
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:14:27 +0000

    efaf502 feat(aio): tell user when no search results

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-235eb17
+ * @license Angular v4.1.0-efaf502
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-235eb17');
+var VERSION = new Version('4.1.0-efaf502');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
