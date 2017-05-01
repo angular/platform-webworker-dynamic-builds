0074f736a40600376376be27630eb7f0957ed3e4
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:19:04 +0000

    ac220fc docs(aio): fix image size on homepage

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-d32bc5d');
+var VERSION = new Version('4.1.0-ac220fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
