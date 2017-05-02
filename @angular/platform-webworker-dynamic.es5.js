2f9b3e0e8d60c97761e4fc4415bce18fdc23428e
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 18:15:27 +0000

    04dc248 feat(aio): comment cleanup, API table styles

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-e263e19
+ * @license Angular v4.1.0-04dc248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-e263e19');
+var VERSION = new Version('4.1.0-04dc248');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
