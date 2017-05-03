92a5895266d65e00d63e025085871bed66e4cf8c
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:00:02 +0000

    464701a docs(aio): revert removal of tutorial examples

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-5b96fb9');
+const VERSION = new Version('4.1.0-464701a');
 
 /**
  * @license
