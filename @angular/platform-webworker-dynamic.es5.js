d08703ffea91534b9e9603ba76f556976b8aead1
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:47:56 +0000

    c8dc116 docs(aio): fixing new url on glossary date pipe

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9684d78
+ * @license Angular v4.1.0-c8dc116
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-9684d78');
+var VERSION = new Version('4.1.0-c8dc116');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
