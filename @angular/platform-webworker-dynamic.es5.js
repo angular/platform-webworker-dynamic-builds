9d894245f77f80f469914764a5a4a43a4b95f10f
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:48:17 +0000

    8760bf7 build(aio): fix paths to template macros in overview-dump template

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8dc116
+ * @license Angular v4.1.0-8760bf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c8dc116');
+var VERSION = new Version('4.1.0-8760bf7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
