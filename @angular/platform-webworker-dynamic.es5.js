d3ad67b3480aa582b0e3dbf848b9755509c976aa
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:51:01 +0000

    6748aea build(aio): update to dgeni-packages 0.19.0

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b3e63c0
+ * @license Angular v4.1.0-6748aea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-b3e63c0');
+var VERSION = new Version('4.1.0-6748aea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b3e63c0
+ * @license Angular v4.1.0-6748aea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-b3e63c0');
+const VERSION = new Version('4.1.0-6748aea');
 
 /**
  * @license
