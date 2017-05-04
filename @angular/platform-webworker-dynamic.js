e3a6c44e48463c2081bdd1a8d8474713ffee8a37
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 13:05:14 +0000

    8931e71 build(aio): ensure that tutorial index arrives in the tutorial search area

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-895f47a
+ * @license Angular v4.1.0-8931e71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-895f47a');
+const VERSION = new Version('4.1.0-8931e71');
 
 /**
  * @license
