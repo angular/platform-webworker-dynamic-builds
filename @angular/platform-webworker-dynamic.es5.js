977637818854f751b687a850bf0815dbdc33977c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:41:45 +0000

    aec65de refactor(aio): simplify DocViewer

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-919ff12
+ * @license Angular v4.1.0-aec65de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-919ff12');
+var VERSION = new Version('4.1.0-aec65de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
