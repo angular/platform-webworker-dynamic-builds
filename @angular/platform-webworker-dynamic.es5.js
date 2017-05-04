bc94a5317b3e5aa59e3bcdd64bcb080ac45fe807
Martin Probst <martin@probst.io>
2017-05-04 18:12:42 +0000

    478d2ce fix(compiler): avoid a `...null` spread in extraction

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-83d9533');
+var VERSION = new Version('4.1.0-478d2ce');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.1.0-83d9533');
+const VERSION = new Version('4.1.0-478d2ce');
 
 /**
  * @license
