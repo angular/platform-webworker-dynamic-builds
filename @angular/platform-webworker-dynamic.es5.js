8f6de849966a370a33bc17d656a9efb1e70ad12a
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:09:50 +0000

    9449eff docs(aio): improve tutorial next-steps prose/links

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-cd28df6
+ * @license Angular v4.2.0-beta.0-9449eff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-cd28df6');
+var VERSION = new Version('4.2.0-beta.0-9449eff');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
