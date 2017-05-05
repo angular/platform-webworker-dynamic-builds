78ccdb1e60f2d61fc5572a9363bde875f0b63281
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 20:41:21 +0000

    9da6340 fix(http): flatten metadata for @angular/http/testing

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-547c363
+ * @license Angular v4.2.0-beta.0-9da6340
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-547c363');
+var VERSION = new Version('4.2.0-beta.0-9da6340');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
