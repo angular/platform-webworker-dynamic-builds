41b56dc8e102f89b5e2b1d4e58758a24be43d7d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:03:58 +0000

    b7caa3e fix(aio): do not route `eplnkr` URLs to / from ServiceWorker

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-3e33482
+ * @license Angular v4.2.0-beta.0-b7caa3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-3e33482');
+var VERSION = new Version('4.2.0-beta.0-b7caa3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
