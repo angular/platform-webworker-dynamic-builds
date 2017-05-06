86ee9d7985f543e4964868ee6150fd0e293de9ab
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:50:09 +0000

    b9ed97c fix(aio): do not display a Toc initially

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-799be9c
+ * @license Angular v4.2.0-beta.0-b9ed97c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-799be9c');
+var VERSION = new Version('4.2.0-beta.0-b9ed97c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
