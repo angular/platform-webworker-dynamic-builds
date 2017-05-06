69bc5eda7f89a3bac35f69d569e580bc183956c5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:51:33 +0000

    03513e9 fix(aio): constrain header-link styles

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-b9ed97c
+ * @license Angular v4.2.0-beta.0-03513e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-b9ed97c');
+var VERSION = new Version('4.2.0-beta.0-03513e9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
