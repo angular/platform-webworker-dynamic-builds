5a0fb56691ab57e369e80279f90a0a175e4b71aa
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:07:27 +0000

    954c08e fix(aio): remove unwanted code styling from styleguide doc

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a2dcb7b
+ * @license Angular v4.2.0-beta.0-954c08e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-a2dcb7b');
+var VERSION = new Version('4.2.0-beta.0-954c08e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
