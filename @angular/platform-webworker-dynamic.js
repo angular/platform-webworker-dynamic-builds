fe90016b86f15da2da07cb09854fe5e8b9a75a0d
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 17:44:00 +0000

    ce1d7c4 refactor: use view engine also for `NgModuleFactory`s (#16658)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-b9521b5
+ * @license Angular v4.2.0-beta.1-ce1d7c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-beta.1-b9521b5');
+const VERSION = new Version('4.2.0-beta.1-ce1d7c4');
 
 /**
  * @license
