70c28795e4d49f9bad109a3b213f115ea1f5b3e3
Igor Minar <iminar@google.com>
2017-05-12 19:37:09 +0000

    0626464 build: use subshells when changing directories

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-beta.1-21d213d');
+const VERSION = new Version('4.2.0-beta.1-0626464');
 
 /**
  * @license
