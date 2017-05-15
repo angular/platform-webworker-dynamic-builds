fd52f501fdca1e07abd7793e9f61bec5b79eeed5
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:31:12 +0000

    1eba623 fix(compiler-cli): import routing module with forRoot (#16438)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ea2b24f
+ * @license Angular v4.2.0-beta.1-1eba623
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-ea2b24f');
+var VERSION = new Version('4.2.0-beta.1-1eba623');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
