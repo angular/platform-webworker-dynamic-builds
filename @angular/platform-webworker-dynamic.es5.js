5ca81a4c81c9c57542ee868bc53ee86cf2437333
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 17:39:11 +0000

    af99cf2 build(aio): compute `{@link ...} titles more effectively (#16813)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-221f85a
+ * @license Angular v4.2.0-beta.1-af99cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-221f85a');
+var VERSION = new Version('4.2.0-beta.1-af99cf2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
