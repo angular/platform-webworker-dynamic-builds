f91da6c7d97647df72d5c4efba19e329e6d76292
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:09:35 +0000

    0e13a59 fix(aio): add small delay before autoscrolling

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-3361a7b
+ * @license Angular v4.2.0-beta.1-0e13a59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-3361a7b');
+var VERSION = new Version('4.2.0-beta.1-0e13a59');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
