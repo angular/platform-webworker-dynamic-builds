78b3df3bf5251635ded3deb8a54411eb41567d03
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:01:37 +0000

    a42322d feat(aio): code copy button has tooltip and aria-label

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-b836aca
+ * @license Angular v4.2.0-beta.1-a42322d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-b836aca');
+var VERSION = new Version('4.2.0-beta.1-a42322d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
