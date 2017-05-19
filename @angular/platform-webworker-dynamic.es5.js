2d929abf482d3a217d16a129e41d0f7014f90c74
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:00:59 +0000

    b836aca docs(aio): rework of the upgrade guide

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-7d9f96a
+ * @license Angular v4.2.0-beta.1-b836aca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-7d9f96a');
+var VERSION = new Version('4.2.0-beta.1-b836aca');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
