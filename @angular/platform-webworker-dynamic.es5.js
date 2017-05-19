255cdb6ef2a17fbe9aafea7258e93e12e9d578dd
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-19 19:14:03 +0000

    5d4f543 refactor(router): don't run the change detection every time an outlet is activated

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-6cb93c1
+ * @license Angular v4.2.0-beta.1-5d4f543
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-6cb93c1');
+var VERSION = new Version('4.2.0-beta.1-5d4f543');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
