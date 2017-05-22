9070019f599a8dd87f6febb223875965c166baf5
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:39:33 +0000

    85d4c4b feat(core): update zone.js to 0.8.10 and expose the flush method (#16860)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-6e41add
+ * @license Angular v4.2.0-rc.0-85d4c4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-6e41add');
+var VERSION = new Version('4.2.0-rc.0-85d4c4b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
