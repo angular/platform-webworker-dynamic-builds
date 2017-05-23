a919f8d4bac0f37ef42e803c3614e1d15aed91f7
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 17:16:05 +0000

    5af143e refactor(compiler): allow sync AOT compilation (#16832).

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-3b28c75
+ * @license Angular v4.2.0-rc.0-5af143e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-3b28c75');
+var VERSION = new Version('4.2.0-rc.0-5af143e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
