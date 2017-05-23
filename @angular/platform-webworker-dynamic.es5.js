a60619e803c52d697c91409dadb2a9822d9c9005
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:55:35 +0000

    f5b2ce0 docs: update the release schedule (#16943)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-78e3be1
+ * @license Angular v4.2.0-rc.0-f5b2ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-78e3be1');
+var VERSION = new Version('4.2.0-rc.0-f5b2ce0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
