b2c6dac100dbcacae9bcac1383fad5e9366c96eb
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:00:47 +0000

    e10d763 fix(aio): correctly handle re-navigation to the empty path (#16997)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-573b861
+ * @license Angular v4.2.0-rc.0-e10d763
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-573b861');
+var VERSION = new Version('4.2.0-rc.0-e10d763');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
