251673f3b91b83608851f2efa897e9d4cf991862
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 16:32:10 +0000

    7ffb75f refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.0-c0981b8');
+const VERSION = new Version('4.2.0-rc.0-7ffb75f');
 
 /**
  * @license
