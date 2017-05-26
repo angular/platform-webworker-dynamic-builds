64ae869e4de05a6c73918610831fc116a7b03f30
Igor Minar <iminar@google.com>
2017-05-26 08:38:27 +0000

    db3113b build(aio): externalize @angular/cli patches into a js file

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-0c691af
+ * @license Angular v4.2.0-rc.0-db3113b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-0c691af');
+var VERSION = new Version('4.2.0-rc.0-db3113b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
