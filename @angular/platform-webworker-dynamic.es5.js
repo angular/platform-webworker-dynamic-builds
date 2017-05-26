84cf35179071a9c5456b514e418a3e9b2a21ba53
Igor Minar <igor@angularjs.org>
2017-05-26 08:46:04 +0000

    9e7e178 build(aio): add google webmaster tools verification file

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-db3113b
+ * @license Angular v4.2.0-rc.0-9e7e178
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-db3113b');
+var VERSION = new Version('4.2.0-rc.0-9e7e178');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
