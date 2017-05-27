3fb183140546faf3b648feda578e2856ee33fdb3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-27 10:37:06 +0000

    b8b91d3 feat(aio): marketing design refresh

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-0018acf
+ * @license Angular v4.2.0-rc.1-b8b91d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-0018acf');
+var VERSION = new Version('4.2.0-rc.1-b8b91d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
