2cc1ecb618c0a33ea893037903d2f5ac373397fd
GitHub <noreply@github.com>
2017-05-27 19:04:37 +0000

    6220b49 build(aio): upgrade to ngo with top-level NewExpression and CallExpression prefixing (#17063)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-aa683a7
+ * @license Angular v4.2.0-rc.1-6220b49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-aa683a7');
+var VERSION = new Version('4.2.0-rc.1-6220b49');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
