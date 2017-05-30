db4390cb6098e7f61001aaa33c4ffdd3d21e7fc2
Victor Berchet <victor@suumit.com>
2017-05-30 20:38:00 +0000

    8ffa483 fix(compiler): enableLegacyTemplate should not be ignored (#17051)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-ea8a43d
+ * @license Angular v4.2.0-rc.1-8ffa483
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-ea8a43d');
+var VERSION = new Version('4.2.0-rc.1-8ffa483');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
