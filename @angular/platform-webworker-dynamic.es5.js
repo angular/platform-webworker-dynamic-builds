ed1d91a17000ecf17f4e97ad6b7d3f39ba4cc650
Victor Berchet <victor@suumit.com>
2017-05-30 18:03:51 +0000

    18bf772 feat(compiler): emit typescript nodes from an output ast (#16823)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-160221c
+ * @license Angular v4.2.0-rc.1-18bf772
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-160221c');
+var VERSION = new Version('4.2.0-rc.1-18bf772');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
