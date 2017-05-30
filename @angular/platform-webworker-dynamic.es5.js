2b7c450127f2bd5261fd68c2afc9f095c06f23e7
Victor Berchet <victor@suumit.com>
2017-05-30 18:08:53 +0000

    ea8a43d feat(tsc-wrapped): always convert shorthand imports (#16898)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-535d9da
+ * @license Angular v4.2.0-rc.1-ea8a43d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-535d9da');
+var VERSION = new Version('4.2.0-rc.1-ea8a43d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
