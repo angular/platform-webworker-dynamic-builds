d8ba448e454c12bad859d31a2a0549e4947d9a72
GitHub <noreply@github.com>
2017-05-31 11:05:52 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-11505fa');
+var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
