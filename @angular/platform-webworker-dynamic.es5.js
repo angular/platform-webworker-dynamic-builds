281e7b6de1cec605f7d4c49caf8711f2328334c2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-31 06:04:35 +0000

    cb2cb7c build(aio): display "call" and "new" members of interfaces in API docs

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-dfbbbb5
+ * @license Angular v4.2.0-rc.1-cb2cb7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-dfbbbb5');
+var VERSION = new Version('4.2.0-rc.1-cb2cb7c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
