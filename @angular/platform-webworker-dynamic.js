34bbbf7ee0c82af30060454860e96ad04e810427
Igor Minar <iminar@google.com>
2017-05-31 10:41:54 +0000

    11505fa fix(aio): disable search on browsers that don't support web workers

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-cb2cb7c
+ * @license Angular v4.2.0-rc.1-11505fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.1-cb2cb7c');
+const VERSION = new Version('4.2.0-rc.1-11505fa');
 
 /**
  * @license
