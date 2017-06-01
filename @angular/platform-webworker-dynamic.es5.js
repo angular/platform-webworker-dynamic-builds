a79d510d924f837d3ecf2784d419e488e255055c
Victor Berchet <victor@suumit.com>
2017-06-01 16:59:30 +0000

    1338995 fix(aio): fix home learn more button alignment (#17154)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-47e4fca');
+var VERSION = new Version('4.2.0-rc.1-1338995');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
