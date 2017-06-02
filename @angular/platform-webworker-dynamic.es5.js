8747872eb7fdc798cabea3e93effef60c059843e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 17:36:12 +0000

    d5ce086 build(aio): fix up API doc-gen templates

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-82ec02d
+ * @license Angular v4.2.0-rc.2-d5ce086
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-82ec02d');
+var VERSION = new Version('4.2.0-rc.2-d5ce086');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
