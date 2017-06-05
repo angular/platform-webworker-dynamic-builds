3daa33c543cc1bed6bd2213964a942ced6608be4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:38:28 +0000

    47c2a2e ci: switch CircleCI to 2.0

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.2-7c78282');
+const VERSION = new Version('4.2.0-rc.2-47c2a2e');
 
 /**
  * @license
