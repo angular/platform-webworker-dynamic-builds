acf5428374529100b36e5ae627e4f842672f4b2b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:33:04 +0000

    734f30d build(aio): ignore .ngsummary.ts files in examples

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-f2d810f
+ * @license Angular v4.2.0-rc.2-734f30d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-f2d810f');
+var VERSION = new Version('4.2.0-rc.2-734f30d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
