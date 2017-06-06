dd875a9fcf2c7735a04880f17b7eac8be956c343
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:59:07 +0000

    3a121a6 build(aio): migrate contribute page to html

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-c06f4fc');
+var VERSION = new Version('4.2.0-rc.2-3a121a6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
