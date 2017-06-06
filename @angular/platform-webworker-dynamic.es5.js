40a16f5588c74b23800b9601e3fbd92e50c88897
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:58:41 +0000

    c06f4fc docs(aio): remove tutorial from docs landing page

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-95f1ea2
+ * @license Angular v4.2.0-rc.2-c06f4fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-95f1ea2');
+var VERSION = new Version('4.2.0-rc.2-c06f4fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
