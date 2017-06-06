72317868ee3a73fcaf24c2f9a15854aefc189a7d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:45:41 +0000

    bb2fc6b refactor(platform-browser): Remove setGlobalVar from DOM adapter

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bfdd339
+ * @license Angular v4.2.0-rc.2-bb2fc6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-bfdd339');
+var VERSION = new Version('4.2.0-rc.2-bb2fc6b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
