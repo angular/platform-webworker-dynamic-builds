396d0f40b90094d813a5588dbd36b8d7b104c6aa
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 14:02:09 +0000

    d837bfc fix(aio): fix scrolling to an element

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4759975
+ * @license Angular v4.2.0-rc.2-d837bfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4759975');
+var VERSION = new Version('4.2.0-rc.2-d837bfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
