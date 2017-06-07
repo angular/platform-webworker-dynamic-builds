3e204f5464f707332d8f6bd125658b36f8bc28d2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 13:11:57 +0000

    4759975 fix(aio): adjust SideNav top to avoid extra whitespace

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-078a4b0
+ * @license Angular v4.2.0-rc.2-4759975
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-078a4b0');
+var VERSION = new Version('4.2.0-rc.2-4759975');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
