b180552eaf3b0e3a49b1bea8c898356c301661eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:18:40 +0000

    a222c3e fix(forms): fix min and max validator behavior on non-numbers

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-41a765d
+ * @license Angular v4.2.0-rc.2-a222c3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-41a765d');
+var VERSION = new Version('4.2.0-rc.2-a222c3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
