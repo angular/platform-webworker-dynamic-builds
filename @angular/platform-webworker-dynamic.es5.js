840fa020dd0c57ac28f8443cae7a962d584ccefc
GitHub <noreply@github.com>
2017-06-08 22:47:45 +0000

    576d6d8 feat(aio): update the home page and docs landing page content (#17325)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-a222c3e');
+var VERSION = new Version('4.2.0-rc.2-576d6d8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
