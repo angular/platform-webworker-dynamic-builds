e4f49706254654db9334cbec2c28ca3de6edd987
Igor Minar <iminar@google.com>
2017-06-08 19:35:32 +0000

    4d2ee51 fix(aio): remove glossary l-sub-section class (#17305)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-0940e6d
+ * @license Angular v4.2.0-rc.2-4d2ee51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.2-0940e6d');
+const VERSION = new Version('4.2.0-rc.2-4d2ee51');
 
 /**
  * @license
