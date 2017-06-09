f22b643ed170416e9b26abcba880dd0bee003fe5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 20:08:27 +0000

    ce18c68 fix(aio): temporarily remove the focus style of top-bar nav items

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-2d56239
+ * @license Angular v4.2.0-ce18c68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-2d56239');
+var VERSION = new Version('4.2.0-ce18c68');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
