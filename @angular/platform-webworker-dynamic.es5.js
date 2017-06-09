a49e1cf400f092c409a7aac4cad6e3f28ee3d05f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 22:04:31 +0000

    c2d31fb fix(http): move destructuring inside {Request,Response}Options ctor

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-db5e506
+ * @license Angular v4.2.0-c2d31fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-db5e506');
+var VERSION = new Version('4.2.0-c2d31fb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
