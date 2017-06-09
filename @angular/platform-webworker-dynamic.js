158eaf94972fa22a221cf14df9aafa9de7176b13
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 23:16:28 +0000

    90b0713 refactor(compiler): don’t write summaries for jit by default

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-d56b7ed
+ * @license Angular v4.2.0-90b0713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-d56b7ed');
+const VERSION = new Version('4.2.0-90b0713');
 
 /**
  * @license
