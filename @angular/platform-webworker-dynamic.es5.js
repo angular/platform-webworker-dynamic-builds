93019b48c458093233298cf70052acfab398e72e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 16:15:41 +0000

    2d56239 fix(aio): sidebar folder state after select item

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-a0b30e5
+ * @license Angular v4.2.0-2d56239
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-a0b30e5');
+var VERSION = new Version('4.2.0-2d56239');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
