5e957fee604734c1e78926dd71fc14289bd9a0ef
Igor Minar <iminar@google.com>
2017-06-09 21:54:50 +0000

    db5e506 build(aio): update typescript to 2.3.2 (#17382)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-0020dad
+ * @license Angular v4.2.0-db5e506
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-0020dad');
+var VERSION = new Version('4.2.0-db5e506');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
