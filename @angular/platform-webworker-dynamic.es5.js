81048b214cf2708e090c0a6798ba642a5e8f9dca
GitHub <noreply@github.com>
2017-06-10 01:52:21 +0000

    c8c0cee build(bazel): fix the typescript path in the BUILD file (#17316)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-afbc2b0
+ * @license Angular v4.2.1-c8c0cee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-afbc2b0');
+var VERSION = new Version('4.2.1-c8c0cee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
