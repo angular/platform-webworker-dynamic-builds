abc7f8f59eb0b1398a2c5ed52e2de0b4726d02ed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:20:39 +0000

    0268818 build(aio): update Google Analytics property id for the prod environment

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-234268e');
+var VERSION = new Version('4.2.1-0268818');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
