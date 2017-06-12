a7b78ce3581f9dea5a73924450428091af157b49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:19:01 +0000

    234268e ci(aio): deploy commits on the stable branch to production

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7a2d2ef
+ * @license Angular v4.2.1-234268e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-7a2d2ef');
+var VERSION = new Version('4.2.1-234268e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
