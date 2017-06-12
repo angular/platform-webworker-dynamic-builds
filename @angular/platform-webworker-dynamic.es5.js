ef3e767cd7b393439d29992bcf52dbded21f1a5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 22:45:24 +0000

    ed73d4f refactor(compiler): don’t always compile `.ngfactory.ts` files by default

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-451257a
+ * @license Angular v4.2.1-ed73d4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-451257a');
+var VERSION = new Version('4.2.1-ed73d4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
