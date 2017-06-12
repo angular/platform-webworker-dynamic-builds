b4871f790328cc1c588155fa65bc8c809da01061
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:10:40 +0000

    7a2d2ef docs: add changelog for 4.2.2

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-6ca4692
+ * @license Angular v4.2.2-7a2d2ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-6ca4692');
+var VERSION = new Version('4.2.2-7a2d2ef');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
