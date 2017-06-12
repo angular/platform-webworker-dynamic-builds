41b23e8234fec5fc12c3223270804c9d69a0aeff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 22:46:37 +0000

    6ca4692 fix(animations): properly collect :enter nodes in a partially updated collection

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-ed73d4f
+ * @license Angular v4.2.1-6ca4692
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.1-ed73d4f');
+const VERSION = new Version('4.2.1-6ca4692');
 
 /**
  * @license
