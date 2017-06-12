8170e8138baabd19b0165d3f0e60943be017251b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-12 13:32:37 +0000

    3d5f520 test(aio): fix Chrome getting disconnected during unit tests

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-d961911
+ * @license Angular v4.2.1-3d5f520
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.1-d961911');
+const VERSION = new Version('4.2.1-3d5f520');
 
 /**
  * @license
