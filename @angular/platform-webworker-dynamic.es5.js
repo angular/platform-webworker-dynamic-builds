771a93efaac366d6ba49584d187df69af95e0b0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:08:30 +0000

    93d1b4e refactor(compiler): remove duplicate code

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fa81c8e
+ * @license Angular v4.2.2-93d1b4e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fa81c8e');
+var VERSION = new Version('4.2.2-93d1b4e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
