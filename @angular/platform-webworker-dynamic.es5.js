dafe8d05184e8d5b2d311d6bbba13ebe90e742d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 17:47:43 +0000

    98308cd docs(aio): update typescript for `examples/webpack` to same as cli

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-052331f
+ * @license Angular v4.2.2-98308cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-052331f');
+var VERSION = new Version('4.2.2-98308cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
