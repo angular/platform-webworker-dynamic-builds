1b822ffce1c1352ffdd07159574b63efc8053060
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:54:27 +0000

    bffccf4 fix(aio): fix buttons in "Home" and "Features"

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7caa0a8
+ * @license Angular v4.2.2-bffccf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.2-7caa0a8');
+const VERSION = new Version('4.2.2-bffccf4');
 
 /**
  * @license
