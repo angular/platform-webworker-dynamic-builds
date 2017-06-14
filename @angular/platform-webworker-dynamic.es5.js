37a2089f1821ad2b5b71ae9a398c091430e083d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 14:36:08 +0000

    607fb1f fix(aio): always cover the whole footer with its background

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-38fc2a0
+ * @license Angular v4.2.2-607fb1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-38fc2a0');
+var VERSION = new Version('4.2.2-607fb1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
