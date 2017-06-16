96787c4ff39fdef8c98f8c00f16b6cf3f8c75946
Hans Larsen <hans@hansl.ca>
2017-06-16 17:20:33 +0000

    1ddbddb docs: add changelog for 4.2.3

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-232bd93');
+var VERSION = new Version('4.2.2-1ddbddb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
