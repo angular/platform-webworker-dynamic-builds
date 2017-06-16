6c29ddcf7ecea60132a3ecdb491b9d0012b7b288
Hans Larsen <hans@hansl.ca>
2017-06-16 19:34:46 +0000

    71a8ef5 fix(aio): improve no-javascript screen

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-623b9c6
+ * @license Angular v4.2.3-71a8ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.3-623b9c6');
+const VERSION = new Version('4.2.3-71a8ef5');
 
 /**
  * @license
