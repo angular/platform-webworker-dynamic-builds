57fcbd76050536b2d1777f3c1fa17f36bd3c6af6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:11:05 +0000

    ce00fa3 build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-e096a85
+ * @license Angular v4.2.2-ce00fa3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.2-e096a85');
+const VERSION = new Version('4.2.2-ce00fa3');
 
 /**
  * @license
