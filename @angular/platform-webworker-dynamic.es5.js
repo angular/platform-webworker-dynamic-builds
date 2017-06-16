4b5631005299e676e4ea7acf293f9bb54498a28a
Hans Larsen <hans@hansl.ca>
2017-06-16 18:49:10 +0000

    623b9c6 test(compiler): fix typo "mamespace"

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-8a547ee
+ * @license Angular v4.2.3-623b9c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-8a547ee');
+var VERSION = new Version('4.2.3-623b9c6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
