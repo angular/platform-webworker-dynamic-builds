7234483aa40e59c2caf23b5ff1454431c18b13eb
Hans <hans@hansl.ca>
2017-06-16 19:35:26 +0000

    fe126cb fix(aio): remove unused icon reference and file

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-adc1b12
+ * @license Angular v4.2.2-fe126cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-adc1b12');
+var VERSION = new Version('4.2.2-fe126cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
