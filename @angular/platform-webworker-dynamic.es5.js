768d4abe586ff03ffdf9f37260a2431bb3a9146f
Hans <hans@hansl.ca>
2017-06-19 22:29:46 +0000

    8f5836c docs(TRIAGE_AND_LABELS): update labels to reflect the current state

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-319ce18
+ * @license Angular v4.2.2-8f5836c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-319ce18');
+var VERSION = new Version('4.2.2-8f5836c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
