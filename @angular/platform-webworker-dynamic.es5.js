58b89431766bb4c9c9c340b7b55b8324cf7f6df6
Hans <hans@hansl.ca>
2017-06-19 19:06:32 +0000

    aa92f3a docs(RELEASE_SCHEDULE): update the release schedule w/ recent regression patch releases

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-afbb6bb
+ * @license Angular v4.2.2-aa92f3a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-afbb6bb');
+var VERSION = new Version('4.2.2-aa92f3a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
