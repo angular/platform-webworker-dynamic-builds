bcc1322e25e372ba6c8f46fb5cd72bd5df51ccd1
Hans <hans@hansl.ca>
2017-06-19 22:19:14 +0000

    319ce18 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-aa92f3a
+ * @license Angular v4.2.2-319ce18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-aa92f3a');
+var VERSION = new Version('4.2.2-319ce18');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
