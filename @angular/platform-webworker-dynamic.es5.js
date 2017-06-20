18634e9b7b9c3fab81baa3baaf98a1a192190632
Hans Larsen <hans@hansl.ca>
2017-06-20 21:53:54 +0000

    59299de fix(compiler-cli): find lazy routes in nested module import arrays

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-f997937
+ * @license Angular v4.2.3-59299de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-f997937');
+var VERSION = new Version('4.2.3-59299de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
