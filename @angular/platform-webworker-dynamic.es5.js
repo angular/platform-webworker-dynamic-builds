defbfc538e218b545539c29d9a494415415bdf25
Hans <hans@hansl.ca>
2017-06-20 19:21:00 +0000

    4e6be15 fix(language-service): rollup `tslib` into the language service package

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-3e685f9');
+var VERSION = new Version('4.2.2-4e6be15');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.2-3e685f9');
+const VERSION = new Version('4.2.2-4e6be15');
 
 /**
  * @license
