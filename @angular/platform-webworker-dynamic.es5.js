6cc5000b7b99960141411e2e20a05c7650aff259
Hans <hans@hansl.ca>
2017-06-21 21:23:18 +0000

    bb804dd feat(aio): select contributor group with URL “about?group=gde”

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-1d93cf2');
+var VERSION = new Version('4.2.2-bb804dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
