f83d8c0628e9f89edc2927b4440731ef66ab48b4
Hans Larsen <hans@hansl.ca>
2017-06-21 23:59:30 +0000

    74954f1 docs(aio): ward's changes

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-c112232
+ * @license Angular v4.2.3-74954f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.3-c112232');
+const VERSION = new Version('4.2.3-74954f1');
 
 /**
  * @license
