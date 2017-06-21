5a0b3d082f3174af374a611591d499ae7ca6332c
Hans <hans@hansl.ca>
2017-06-21 21:12:28 +0000

    1d93cf2 build(aio): ensure all doc tests are run

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0034bb2
+ * @license Angular v4.2.2-1d93cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-0034bb2');
+var VERSION = new Version('4.2.2-1d93cf2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
