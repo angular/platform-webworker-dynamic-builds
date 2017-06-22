8ba3803edc85499f30b4ce6afad77349f0e70c42
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:11:10 +0000

    68b64a2 docs: add missing colon in the Constants section of NAMING.md

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-8c129d7
+ * @license Angular v4.3.0-beta.0-68b64a2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-8c129d7');
+var VERSION = new Version('4.3.0-beta.0-68b64a2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
