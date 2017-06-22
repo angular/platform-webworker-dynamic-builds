c1b3cde2cb41e97ea1ef00fd8e5b35a08654572a
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:46 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-92bcfef');
+var VERSION = new Version('4.3.0-beta.0-133b5e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
