3fdf7412e00ab98d22fca6a9855bb6efd1c067c0
Hans Larsen <hans@hansl.ca>
2017-06-22 00:17:46 +0000

    d203136 revert: feat(core): update zone.js to 0.8.12

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-74954f1
+ * @license Angular v4.2.3-d203136
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-74954f1');
+var VERSION = new Version('4.2.3-d203136');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
