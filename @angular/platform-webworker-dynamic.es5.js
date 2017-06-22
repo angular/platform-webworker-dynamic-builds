8a2ba823d94592a7f331034912f21bed9190d668
Hans Larsen <hans@hansl.ca>
2017-06-22 00:29:27 +0000

    02a38d3 docs: add changelog for 4.2.4

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
