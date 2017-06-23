b435887267a438fd96612b6d5e66e20da65d5064
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:35:31 +0000

    f2ee1dc fix(animations): do not validate style overlap errors in different transitions

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-60273a9
+ * @license Angular v4.3.0-beta.0-f2ee1dc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-60273a9');
+var VERSION = new Version('4.3.0-beta.0-f2ee1dc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
