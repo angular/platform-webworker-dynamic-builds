041cf41542587b5849d7ede299cefe11cdb3a19a
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:09:06 +0000

    494a0d0 fix(aio): fix topbar nav-item focus style

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-7ba720a
+ * @license Angular v4.3.0-beta.0-494a0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-7ba720a');
+var VERSION = new Version('4.3.0-beta.0-494a0d0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
