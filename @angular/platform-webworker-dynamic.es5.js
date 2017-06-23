2a63aba95275b16fa95171732442747456dd19f1
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:34:43 +0000

    60273a9 docs: fix “under to hood” typo in changelog.md

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-b1fe63d
+ * @license Angular v4.3.0-beta.0-60273a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-b1fe63d');
+var VERSION = new Version('4.3.0-beta.0-60273a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
