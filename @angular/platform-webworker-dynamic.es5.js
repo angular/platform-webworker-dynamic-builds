0fa56381a87b3400f8af6380fd29b18b6b56b8e3
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:14:51 +0000

    81734cf refactor(compiler): add tracking comment for self-import detection

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-b754e60');
+var VERSION = new Version('4.3.0-beta.0-81734cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
