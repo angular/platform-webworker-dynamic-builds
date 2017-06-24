21762b1787976da9f64bab6e21455a0dfa288985
Matias Niemelä <matias@yearofmoo.com>
2017-06-24 00:53:51 +0000

    263c1a1 refactor: remove unused imports of the deprecated Renderer

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-43c187b
+ * @license Angular v4.3.0-beta.0-263c1a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-43c187b');
+var VERSION = new Version('4.3.0-beta.0-263c1a1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
