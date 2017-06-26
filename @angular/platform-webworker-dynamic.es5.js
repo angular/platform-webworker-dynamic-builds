def623786e970ea79bc09f1aa0a4143129c15179
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:40:59 +0000

    233044e docs(aio): fix typo

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-8786ba9
+ * @license Angular v4.3.0-beta.0-233044e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-8786ba9');
+var VERSION = new Version('4.3.0-beta.0-233044e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
