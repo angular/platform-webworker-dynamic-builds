1aa5518534297cf9a8859319003dffbb13df5355
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:06:09 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-e47a77f');
+var VERSION = new Version('4.3.0-beta.0-96b1703');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
