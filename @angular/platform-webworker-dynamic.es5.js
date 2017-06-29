6a1049c095babce517c8dcd4d4ec367c6a0382be
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 22:13:47 +0000

    dfcca66 ci: remove print-logs.sh to try to fix failing builds on Travis

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-1ac9085
+ * @license Angular v4.3.0-beta.1-dfcca66
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-1ac9085');
+var VERSION = new Version('4.3.0-beta.1-dfcca66');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
