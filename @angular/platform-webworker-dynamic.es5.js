60e7ed56cbde1b810086b32643024a7281cad42e
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 19:12:58 +0000

    69ec333 docs(aio): Add resources contributor info

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-67519eb
+ * @license Angular v4.3.0-beta.1-69ec333
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-67519eb');
+var VERSION = new Version('4.3.0-beta.1-69ec333');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
