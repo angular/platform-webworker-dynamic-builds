49ebb446f863846f414abb90a72ace09b046d464
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:06:54 +0000

    c460757 docs(aio): Add resources contributor info

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-1a476b8
+ * @license Angular v4.2.5-c460757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-1a476b8');
+var VERSION = new Version('4.2.5-c460757');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
