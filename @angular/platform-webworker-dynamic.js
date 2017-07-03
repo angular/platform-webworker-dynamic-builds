bef64cbcb2c2482959071fe89e9ad345e5e2d28f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:53:39 +0000

    37b2000 ci(aio): use valid database path for Firebase payload size upload

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-571adbe
+ * @license Angular v4.2.5-37b2000
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.5-571adbe');
+const VERSION = new Version('4.2.5-37b2000');
 
 /**
  * @license
