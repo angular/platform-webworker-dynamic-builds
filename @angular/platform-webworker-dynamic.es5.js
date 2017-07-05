5e2279a1a6ecef9d10663b3def28068587ab3b36
Jason Aden <jasonaden@angular.io>
2017-07-05 22:57:42 +0000

    578bdeb fix(router): export missing UrlMatcher and UrlMatchResult types

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b51697c
+ * @license Angular v4.3.0-beta.1-578bdeb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-b51697c');
+var VERSION = new Version('4.3.0-beta.1-578bdeb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
