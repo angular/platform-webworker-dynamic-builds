510b75d793e68c94d54846236361412ab201d709
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 21:51:32 +0000

    9d15d85 docs: README - use correctly encoded link for npm badge (#17707)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9263da5
+ * @license Angular v4.3.0-beta.1-9d15d85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-9263da5');
+var VERSION = new Version('4.3.0-beta.1-9d15d85');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
