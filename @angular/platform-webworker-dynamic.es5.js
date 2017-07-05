52cc8e9e01a100b2a2c08d5118ddac47000bfcf0
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:31:58 +0000

    b51697c ci: add Chuck to the owners of core (#17892)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-92d7ecf
+ * @license Angular v4.3.0-beta.1-b51697c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-92d7ecf');
+var VERSION = new Version('4.3.0-beta.1-b51697c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
