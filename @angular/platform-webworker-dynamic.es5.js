c1ba5a961dc66d813ccb04ec42c4ade97776b18d
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:42 +0000

    ec58246 docs(aio): add DevExtreme to the UI Components section of resources.json (#17939)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-1bb2476
+ * @license Angular v4.3.0-beta.1-ec58246
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-1bb2476');
+var VERSION = new Version('4.3.0-beta.1-ec58246');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
