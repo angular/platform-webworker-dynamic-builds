b5a8f06edb6975f0603058ddd7bc7c74459f88b5
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-06 14:20:58 +0000

    670f2e5 ci(aio): raise polyfill payload limits

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-408bb89
+ * @license Angular v4.2.5-670f2e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-408bb89');
+var VERSION = new Version('4.2.5-670f2e5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
