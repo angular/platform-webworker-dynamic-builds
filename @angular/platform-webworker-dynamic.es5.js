083b2c1f47245afdce87cf79c1449201a6139b2e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:32:32 +0000

    c69fff1 fix(core): fix re-insertions in the iterable differ (#17891)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-70981c6
+ * @license Angular v4.3.0-beta.1-c69fff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-70981c6');
+var VERSION = new Version('4.3.0-beta.1-c69fff1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
