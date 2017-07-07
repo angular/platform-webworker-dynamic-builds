c6dd8eb261d158275b5d43591f7abd685062c731
Jason Aden <jasonaden@angular.io>
2017-07-07 16:13:27 +0000

    11db3bd fix(upgrade): bring the dynamic version closer to the static one (#17971)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-3b2d2c4');
+var VERSION = new Version('4.3.0-beta.1-11db3bd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
