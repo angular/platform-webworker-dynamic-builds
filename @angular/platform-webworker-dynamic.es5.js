3f44dd71dd6d7dd09e2a29f3447bce3efd11b9ac
GitHub <noreply@github.com>
2017-07-07 00:26:53 +0000

    ae27af7 fix(router): encode URLs the same way AngularJS did (closer to spec) (#17890)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c69fff1
+ * @license Angular v4.3.0-beta.1-ae27af7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-c69fff1');
+var VERSION = new Version('4.3.0-beta.1-ae27af7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
