e34937d2ef83345f481a6a3699e93bdf21b77e09
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:18:47 +0000

    cb16e9c fix(tsc-wrapped): emit exports metadata in flat modules (#17893)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-6bae73c
+ * @license Angular v4.3.0-beta.1-cb16e9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-6bae73c');
+var VERSION = new Version('4.3.0-beta.1-cb16e9c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
