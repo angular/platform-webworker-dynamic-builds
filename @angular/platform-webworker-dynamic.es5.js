1dcda099b75f860f2a4ac49480568e577bd6d9ee
Jason Aden <jasonaden@google.com>
2017-07-08 04:31:21 +0000

    8b8b4cb refactor(upgrade): move shareable functionality to `UpgradeHelper` class (#17971)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-3e61bf7
+ * @license Angular v4.2.5-8b8b4cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-3e61bf7');
+var VERSION = new Version('4.2.5-8b8b4cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
