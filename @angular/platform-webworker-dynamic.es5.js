2d0e76d94a5ab88fc1f7a357091c9f8bc20e087e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-08 00:27:02 +0000

    c723d42 refactor: fix typos (#18000)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9dd550f
+ * @license Angular v4.3.0-beta.1-c723d42
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-9dd550f');
+var VERSION = new Version('4.3.0-beta.1-c723d42');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
