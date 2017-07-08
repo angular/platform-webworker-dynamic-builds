9620236ccfaefef3bdbc377d1ac4657291d97ef5
Jason Aden <jasonaden@google.com>
2017-07-08 05:15:58 +0000

    b69f0fa docs: add changelog for 4.2.6

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-8b8b4cb');
+var VERSION = new Version('4.2.6-b69f0fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
