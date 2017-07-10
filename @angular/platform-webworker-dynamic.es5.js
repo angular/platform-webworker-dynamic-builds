c6b3a2a0f08ebdb8bfad37e37cb4a66fe197f0df
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:02:37 +0000

    2055634 feat(aio): add API endpoint for notifying about PR updates

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b3d9036
+ * @license Angular v4.3.0-rc.0-2055634
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-b3d9036');
+var VERSION = new Version('4.3.0-rc.0-2055634');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
