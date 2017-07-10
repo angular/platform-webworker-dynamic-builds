b327fb476748054e65854de7d8cac8848ff90834
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:54:08 +0000

    ab13f1f build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-b69f0fa');
+var VERSION = new Version('4.2.6-ab13f1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.6-b69f0fa');
+const VERSION = new Version('4.2.6-ab13f1f');
 
 /**
  * @license
