f49ad3c50b17fbd9f75bcea17ce12e0370f7d639
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:25:15 +0000

    e8bbf86 build(aio): add terms from heading to the search index

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b30c5fc
+ * @license Angular v4.3.0-rc.0-e8bbf86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-b30c5fc');
+var VERSION = new Version('4.3.0-rc.0-e8bbf86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
