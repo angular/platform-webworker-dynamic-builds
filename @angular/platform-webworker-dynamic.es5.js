274f13863d2a324a6dac0542fce2ee8cfa072785
Igor Minar <iminar@google.com>
2017-07-11 19:17:40 +0000

    d9b03be test: git ignore e2e test folders (#17984)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-b6aad07');
+var VERSION = new Version('4.3.0-rc.0-d9b03be');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
