52b01833a727cb3797dac233b15f752d67106e40
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:26:01 +0000

    3f29348 ci: add aio/content to aio group

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.6-09261e6');
+const VERSION = new Version('4.2.6-3f29348');
 
 /**
  * @license
