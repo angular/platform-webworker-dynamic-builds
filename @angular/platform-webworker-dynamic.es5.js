16b0364a5c5110ab1f7385ea64783d0398ca696c
Igor Minar <iminar@google.com>
2017-07-11 19:09:35 +0000

    4d45fe6 fix(aio): fix tab animations on CodeTabsComponent

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e131f6b
+ * @license Angular v4.3.0-rc.0-4d45fe6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-e131f6b');
+var VERSION = new Version('4.3.0-rc.0-4d45fe6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
