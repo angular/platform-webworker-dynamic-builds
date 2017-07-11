84febce82ff3d496df50e108efc0b5490c57ec55
Igor Minar <igor@angularjs.org>
2017-07-11 22:48:54 +0000

    afd5d72 docs(aio): Added resource link to amexio-ng-extensions [duplicate of #17785] (#17790)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-dbfd717
+ * @license Angular v4.2.6-afd5d72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-dbfd717');
+var VERSION = new Version('4.2.6-afd5d72');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-dbfd717
+ * @license Angular v4.2.6-afd5d72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.2.6-dbfd717');
+const VERSION = new Version('4.2.6-afd5d72');
 
 /**
  * @license
