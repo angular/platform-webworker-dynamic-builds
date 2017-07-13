02419229fa60187097036f0f95c8573706d74ed6
Igor Minar <iminar@google.com>
2017-07-13 22:52:05 +0000

    45ffe54 fix(tsc-wrapped): support as and class expressions (#16904)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-bc1ea8c
+ * @license Angular v4.3.0-rc.0-45ffe54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-bc1ea8c');
+var VERSION = new Version('4.3.0-rc.0-45ffe54');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
