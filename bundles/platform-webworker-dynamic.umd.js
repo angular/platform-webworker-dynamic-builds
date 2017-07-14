eb27888f4e7ac67920c24af4c4731eddfb51ed87
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 22:27:44 +0000

    55742e4 Revert "revert: revert: ci(aio): exclude changes in aio/content folder"

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a0b06be
+ * @license Angular v4.3.0-55742e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_compiler,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.3.0-a0b06be
+ * @license Angular v4.3.0-55742e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-a0b06be');
+var VERSION = new _angular_core.Version('4.3.0-55742e4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
