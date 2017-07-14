67aff4ace6d3fca8b9eabb344c69edf0f3a817f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 16:54:01 +0000

    8e56c3c docs: fix type error in HttpClient sample

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-7955cac
+ * @license Angular v4.3.0-rc.0-8e56c3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-7955cac');
+var VERSION = new Version('4.3.0-rc.0-8e56c3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
