4fa698f7ab88a027c8529685fe4f12c4599e18a9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:52:44 +0000

    c649da9 fix(aio): remove unused `news.html` file

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-0bf0c35
+ * @license Angular v4.3.0-c649da9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-0bf0c35');
+var VERSION = new Version('4.3.0-c649da9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
