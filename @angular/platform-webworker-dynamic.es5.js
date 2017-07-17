9e19c68512ff03f3ff71112245fb2e0d5994faab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:45:25 +0000

    9f99f4f docs: fix HttpClient sample

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c6ad212
+ * @license Angular v4.3.0-9f99f4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-c6ad212');
+var VERSION = new Version('4.3.0-9f99f4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
