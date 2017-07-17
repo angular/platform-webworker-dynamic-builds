e0a622bbf3e79550f0718762c6a433325c95bf43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:34:36 +0000

    8c81c62 docs: fix HttpClient logging's sample

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-47b3ecd
+ * @license Angular v4.3.0-8c81c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-47b3ecd');
+var VERSION = new Version('4.3.0-8c81c62');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
