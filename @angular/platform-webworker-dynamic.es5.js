802e986f6e2d17766c37b77003bc3794d865e60c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:25:06 +0000

    2a2fe11 docs(aio): fix HttpClient setting new header sample

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-7d0f2cd
+ * @license Angular v4.3.0-2a2fe11
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-7d0f2cd');
+var VERSION = new Version('4.3.0-2a2fe11');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
