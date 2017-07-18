bf2f9ff5154c7161fceab4acebf75ad5a78d2385
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:48:08 +0000

    eb6fb5f fix(router): should navigate to the same url when config changes

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-ad3029e
+ * @license Angular v4.3.0-eb6fb5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-ad3029e');
+var VERSION = new Version('4.3.0-eb6fb5f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
