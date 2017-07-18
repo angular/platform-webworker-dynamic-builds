ed6b65e65603e325792604203a5873b2485ade53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 23:54:26 +0000

    5344be5 fix(animations): make sure @.disabled works in non-animation components

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-23146c9');
+var VERSION = new Version('4.3.0-5344be5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
