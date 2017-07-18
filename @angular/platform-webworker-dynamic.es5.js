95b72574bb5dcc15fda017f5e72271ddfbbd2b29
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:13:20 +0000

    c453b7b build(aio): fail doc-gen if referenced images are missing

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-3f331b5');
+var VERSION = new Version('4.3.0-c453b7b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
