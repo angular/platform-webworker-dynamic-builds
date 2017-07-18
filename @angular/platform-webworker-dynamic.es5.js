10e2b7618ac1184fe667da47141cc80449d277b0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:17:29 +0000

    7d0f2cd fix(aio): remove title attribute from `CodeExampleComponent`

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-36faba1
+ * @license Angular v4.3.0-7d0f2cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-36faba1');
+var VERSION = new Version('4.3.0-7d0f2cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
