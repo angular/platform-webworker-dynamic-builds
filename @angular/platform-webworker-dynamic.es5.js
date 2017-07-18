2cea3a5dd0a2e3a4281539fb073ceba2030497d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:37:43 +0000

    17b7bc3 fix(common): send flushed body as error instead of null

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-37c626e
+ * @license Angular v4.3.0-17b7bc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-37c626e');
+var VERSION = new Version('4.3.0-17b7bc3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
