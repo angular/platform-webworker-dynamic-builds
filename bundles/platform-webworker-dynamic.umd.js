e134e5b56d0573c8a9a7992355a7816f7b971c85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:21:01 +0000

    59c23c7 feat(upgrade): propagate touched state of NgModelController

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3b588fe
+ * @license Angular v4.3.0-59c23c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_compiler,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.3.0-3b588fe
+ * @license Angular v4.3.0-59c23c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-3b588fe');
+var VERSION = new _angular_core.Version('4.3.0-59c23c7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
