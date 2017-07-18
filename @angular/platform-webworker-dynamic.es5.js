5d67a12271ccdb0a538221873d93bcab207111dd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:01:55 +0000

    92179bc fix(aio): do not wrap `<code-tabs>` tab labels

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-cdb069a
+ * @license Angular v4.3.0-92179bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-cdb069a');
+var VERSION = new Version('4.3.0-92179bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
