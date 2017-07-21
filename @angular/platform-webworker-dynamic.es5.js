305e42526bc1a58324939361d062015d27100ca4
Miško Hevery <misko@hevery.com>
2017-07-21 17:41:42 +0000

    a6c635e ci: force precise on Travis (#18282)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-54e0244
+ * @license Angular v5.0.0-beta.0-a6c635e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-54e0244');
+var VERSION = new Version('5.0.0-beta.0-a6c635e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
