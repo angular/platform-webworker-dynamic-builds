c7862c279906847f628a79bb308702de39b38665
Miško Hevery <misko@hevery.com>
2017-07-25 21:14:20 +0000

    6279e50 perf(core): use native addEventListener for faster rendering. (#18107)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-6fc5940
+ * @license Angular v5.0.0-beta.0-6279e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-6fc5940');
+var VERSION = new Version('5.0.0-beta.0-6279e50');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
