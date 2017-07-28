48df94aa7905c56d5245535b1a02dde379183acf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:57:59 +0000

    4620753 ci: short-circuit npm install for aio builds that use yarn only

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-416ed69
+ * @license Angular v5.0.0-beta.1-4620753
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-416ed69');
+var VERSION = new Version('5.0.0-beta.1-4620753');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
