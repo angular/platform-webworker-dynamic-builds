5733adcd9a178192ca53007d924b2074105d2825
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 23:15:09 +0000

    9479a10 build: enable TSLint on the packages folder

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_compiler,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-e64b54b');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-9479a10');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
