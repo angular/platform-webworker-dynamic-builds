fd13e6f1b0f09849f5397417fc4a39d4b97f2b30
Victor Berchet <victor@suumit.com>
2017-08-02 23:11:42 +0000

    9adf40a build(aio): use cli 1.3.0-rc (#18290)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-89c6161
+ * @license Angular v5.0.0-beta.2-9adf40a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-89c6161');
+var VERSION = new Version('5.0.0-beta.2-9adf40a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
