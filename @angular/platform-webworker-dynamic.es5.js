8a444983eca06b283fdf5eb096014c65a70ba143
Victor Berchet <victor@suumit.com>
2017-08-02 22:50:55 +0000

    dbc5c58 docs: improve github labels by introducing "PR target" labels (#18436)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-baf4ce0
+ * @license Angular v4.3.3-dbc5c58
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-baf4ce0');
+var VERSION = new Version('4.3.3-dbc5c58');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
