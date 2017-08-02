b0ecd962729c4a899a4668038b952580325f6755
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:30 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-dbc5c58');
+var VERSION = new Version('4.3.3-b65fe3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
