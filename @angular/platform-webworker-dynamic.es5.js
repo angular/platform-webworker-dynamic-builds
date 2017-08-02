d22a3efeb39f914ad611fb8a48cd976ef84bc451
Victor Berchet <victor@suumit.com>
2017-08-02 23:54:14 +0000

    99017bf fix(aio): correctly redirect `cookbook/a1-a2-quick-reference.html` (#18418)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-4d117fa
+ * @license Angular v5.0.0-beta.2-99017bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-4d117fa');
+var VERSION = new Version('5.0.0-beta.2-99017bf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
