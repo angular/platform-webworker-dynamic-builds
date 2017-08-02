8292ea4f3e42eba2804955fcb7ac411da9f43475
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:43:40 +0000

    24db1ed docs: add changelog for 4.3.3

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.2-da8bb1b');
+var VERSION = new Version('4.3.3-24db1ed');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
