dcac841dc987ab444367af08d8d4087c34312788
Victor Berchet <victor@suumit.com>
2017-08-03 01:16:39 +0000

    3a227a1 refactor(router): compile router cleanly with TypeScript 2.4 (#18465)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-81cb5bc
+ * @license Angular v5.0.0-beta.2-3a227a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.2-81cb5bc');
+const VERSION = new Version('5.0.0-beta.2-3a227a1');
 
 /**
  * @license
