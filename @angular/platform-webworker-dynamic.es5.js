4271f3f80f1b0e2532c07f853a2de44397d316bf
Victor Berchet <victor@suumit.com>
2017-08-03 00:35:17 +0000

    7c47b62 fix(compiler): cleanly compile with TypeScript 2.4 (#18456)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-6a88659
+ * @license Angular v5.0.0-beta.2-7c47b62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-6a88659');
+var VERSION = new Version('5.0.0-beta.2-7c47b62');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
