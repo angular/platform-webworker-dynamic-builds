bf7c0bff31221cd27a64b2e905f2c6110495988a
Victor Berchet <victor@suumit.com>
2017-08-07 19:40:05 +0000

    04b18a9 docs(common): fix the DatePipe API docs (#18548)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-05472cb
+ * @license Angular v5.0.0-beta.2-04b18a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-05472cb');
+var VERSION = new Version('5.0.0-beta.2-04b18a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
