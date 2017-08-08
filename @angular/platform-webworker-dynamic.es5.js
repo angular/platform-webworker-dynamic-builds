711b383706de09146157218271ccf578c5c059c7
Victor Berchet <victor@suumit.com>
2017-08-08 21:32:21 +0000

    f0c3ed0 ci(aio): fix deploying to firebase (#18590)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e0660b1
+ * @license Angular v4.3.3-f0c3ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-e0660b1');
+var VERSION = new Version('4.3.3-f0c3ed0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
