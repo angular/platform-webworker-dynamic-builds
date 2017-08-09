dbcd6d4afc5b439c2ec0110ac12f4d755160f448
Victor Berchet <victor@suumit.com>
2017-08-09 22:01:51 +0000

    f4cb453 test(aio): fix running docs examples against local builds (#18520)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-3efc88f
+ * @license Angular v4.3.3-f4cb453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-3efc88f');
+var VERSION = new Version('4.3.3-f4cb453');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
