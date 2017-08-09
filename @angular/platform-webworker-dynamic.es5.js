5377f93255c8784e6f1f01dcb63c659dc725e2a4
Victor Berchet <victor@suumit.com>
2017-08-09 23:45:28 +0000

    cea0241 docs: add changelog for 5.0.0-beta.3

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-ff5c58b
+ * @license Angular v5.0.0-beta.3-cea0241
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-ff5c58b');
+var VERSION = new Version('5.0.0-beta.3-cea0241');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
