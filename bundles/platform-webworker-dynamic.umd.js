8eed26922174124f8358a7ffee52504588b0f064
Hans <hans@hansl.ca>
2017-08-10 23:27:29 +0000

    4d523fd fix(aio): fix compilation error by using the correct type for `providers`

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-cea0241
+ * @license Angular v5.0.0-beta.3-4d523fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_compiler,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.3-cea0241
+ * @license Angular v5.0.0-beta.3-4d523fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.3-cea0241');
+var VERSION = new _angular_core.Version('5.0.0-beta.3-4d523fd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
