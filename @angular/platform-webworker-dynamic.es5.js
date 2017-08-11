4b21fdd247e5db6b9b7ee56ab5aac724ace39cfa
Hans Larsen <hans@hansl.ca>
2017-08-11 20:58:47 +0000

    d7be4f1 perf(aio): update to new version of build-optimizer

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-06e479f
+ * @license Angular v4.3.4-d7be4f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-06e479f');
+var VERSION = new Version('4.3.4-d7be4f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
