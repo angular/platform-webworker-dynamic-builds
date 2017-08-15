ec5bda0c4350733cd653f5d1e2e16b6cda903d25
Hans <hans@hansl.ca>
2017-08-15 23:58:10 +0000

    77747e1 refactor(core): remove toString() method from DefaultIterableDiffer

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-cce2ab2
+ * @license Angular v5.0.0-beta.3-77747e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-cce2ab2');
+var VERSION = new Version('5.0.0-beta.3-77747e1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
