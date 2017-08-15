30d65762ed56f24c500abf1d5a1a40d924ffd35d
Hans <hans@hansl.ca>
2017-08-15 19:48:45 +0000

    9320f34 docs(aio): add Metadata guide based on Chuck’s docs

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-d2c0d98
+ * @license Angular v5.0.0-beta.3-9320f34
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-d2c0d98');
+var VERSION = new Version('5.0.0-beta.3-9320f34');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-d2c0d98
+ * @license Angular v5.0.0-beta.3-9320f34
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.3-d2c0d98');
+const VERSION = new Version('5.0.0-beta.3-9320f34');
 
 /**
  * @license
