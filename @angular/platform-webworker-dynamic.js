780bcce1e62c36a2d4c6fe12da588c4aef9e6d27
Hans <hans@hansl.ca>
2017-08-16 18:54:11 +0000

    bc22ff1 fix(language-service): remove tsickle dependency

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.3-75d484e');
+const VERSION = new Version('5.0.0-beta.3-bc22ff1');
 
 /**
  * @license
