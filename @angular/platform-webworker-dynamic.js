dd747ddebb860e506d1bceff82a0098bd0e72565
Hans <hans@hansl.ca>
2017-08-16 23:01:14 +0000

    3a50098 feat(compiler): allow multiple exportAs names

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-0d45828
+ * @license Angular v5.0.0-beta.4-3a50098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.4-0d45828');
+const VERSION = new Version('5.0.0-beta.4-3a50098');
 
 /**
  * @license
