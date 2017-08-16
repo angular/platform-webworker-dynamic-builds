d39caba1d6a18883fd3c2142b7debe5a9b28cc21
Hans <hans@hansl.ca>
2017-08-16 00:08:21 +0000

    0a73e8d feat(common): mark NgTemplateOutlet API as stable

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-1cfa79c
+ * @license Angular v5.0.0-beta.3-0a73e8d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-1cfa79c');
+var VERSION = new Version('5.0.0-beta.3-0a73e8d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
