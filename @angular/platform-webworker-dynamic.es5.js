ef97e975e46035402b0258cd149d3bedb6d41951
Miško Hevery <misko@hevery.com>
2017-08-17 03:15:30 +0000

    0cc77b4 refactor(compiler): split compiler and core (#18683)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v5.0.0-beta.4-3a50098
+ * @license Angular v5.0.0-beta.4-0cc77b4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
-import { ResourceLoader, platformCoreDynamic } from '@angular/compiler';
+import { ResourceLoader } from '@angular/compiler';
 import { COMPILER_OPTIONS, PLATFORM_ID, Version, createPlatformFactory } from '@angular/core';
-import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-3a50098');
+var VERSION = new Version('5.0.0-beta.4-0cc77b4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -33,7 +33,7 @@ var VERSION = new Version('5.0.0-beta.4-3a50098');
 /**
  * @experimental API related to bootstrapping are still under review.
  */
-var platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'workerAppDynamic', [
+var platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
     {
         provide: COMPILER_OPTIONS,
         useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
