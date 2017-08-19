69be6eb2ec339c61dc9db645e5dae1cf5fc2c0ff
Miško Hevery <misko@hevery.com>
2017-08-19 00:05:55 +0000

    f2a2a6b refactor(core): remove deprecated `Testability#findBindings` (#18782)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-a56468c
+ * @license Angular v5.0.0-beta.4-f2a2a6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.4-a56468c');
+const VERSION = new Version('5.0.0-beta.4-f2a2a6b');
 
 /**
  * @license
