34525ca01387eacf92d6505a2d6229240de26657
Miško Hevery <misko@hevery.com>
2017-08-19 04:40:10 +0000

    77ebd2b docs(aio): fix card inconsistency (#18726)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-3b571a4
+ * @license Angular v4.3.5-77ebd2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.5-3b571a4');
+var VERSION = new Version('4.3.5-77ebd2b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
