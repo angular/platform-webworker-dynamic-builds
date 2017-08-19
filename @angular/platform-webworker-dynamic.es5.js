24a8ca9abf12084c2d79718f651445a6000243c4
Miško Hevery <misko@hevery.com>
2017-08-19 14:34:16 +0000

    e1f45a3 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-77ebd2b
+ * @license Angular v4.3.5-e1f45a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.5-77ebd2b');
+var VERSION = new Version('4.3.5-e1f45a3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
