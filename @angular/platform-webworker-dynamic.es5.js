4d49354db138ecacc06ed3fd2835c9e30a0c4915
Miško Hevery <misko@hevery.com>
2017-08-19 04:38:21 +0000

    e228f2c fix(compiler-cli): use forward slashes for ts.resolveModuleName (#18784)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-f2a2a6b
+ * @license Angular v5.0.0-beta.4-e228f2c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-f2a2a6b');
+var VERSION = new Version('5.0.0-beta.4-e228f2c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
