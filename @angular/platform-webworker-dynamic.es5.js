69c4148c3d11f31ca7c9f06a179d84717c6f78e1
Miško Hevery <misko@hevery.com>
2017-08-29 00:15:55 +0000

    dfe2bad build: Add GitHub scripts for rebasing PRs (#18359)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-f09a266');
+var VERSION = new Version('4.3.6-dfe2bad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.3.6-f09a266');
+const VERSION = new Version('4.3.6-dfe2bad');
 
 /**
  * @license
