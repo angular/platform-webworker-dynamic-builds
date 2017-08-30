ede86b9cc6eb2ead07bcbaddb8a5ed93cf88b088
Jason Aden <jasonaden@google.com>
2017-08-30 00:43:39 +0000

    65e26d7 fix(common): fix improper packaging for @angular/common/http (#18613)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-fdd5010
+ * @license Angular v5.0.0-beta.5-65e26d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.5-fdd5010');
+const VERSION = new Version('5.0.0-beta.5-65e26d7');
 
 /**
  * @license
