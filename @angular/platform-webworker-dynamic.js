6a0e82a8540edfff4924de0e9ba098c4370cbc4e
Jason Aden <jasonaden@google.com>
2017-08-31 22:11:53 +0000

    a1293b2 build: correctly collect summary files with bazel (#18912)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-56b751e
+ * @license Angular v5.0.0-beta.5-a1293b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.5-56b751e');
+const VERSION = new Version('5.0.0-beta.5-a1293b2');
 
 /**
  * @license
