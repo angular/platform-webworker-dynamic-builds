7b568cdd9c6137207be7c8058d401efa8491a7bb
Jason Aden <jasonaden@google.com>
2017-08-31 01:41:25 +0000

    ba7d70e build: fix changelog to remove AIO line items (#18956)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-497e017');
+var VERSION = new Version('4.3.6-ba7d70e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
