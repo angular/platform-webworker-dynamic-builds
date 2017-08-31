a3907e94080f2f8005c7ea8481a4c553299a0398
Jason Aden <jasonaden@google.com>
2017-08-31 19:24:18 +0000

    56b751e Revert "refactor(compiler-cli): expose ngtools api separately (#18952)"

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-57abe8d
+ * @license Angular v5.0.0-beta.5-56b751e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.5-57abe8d');
+const VERSION = new Version('5.0.0-beta.5-56b751e');
 
 /**
  * @license
