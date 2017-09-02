d1c1983e2174770601c283323253bf84376dd150
Misko Hevery <misko@hevery.com>
2017-09-02 15:15:56 +0000

    6ab4966 build: correct broken build

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-185a941
+ * @license Angular v5.0.0-beta.5-6ab4966
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-185a941');
+var VERSION = new Version('5.0.0-beta.5-6ab4966');
 
 /**
  * @fileoverview added by tsickle
