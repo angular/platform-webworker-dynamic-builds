293c3fb72df731824eab087d988115c91ab2d707
Miško Hevery <misko@hevery.com>
2017-09-06 05:00:06 +0000

    d2707f1 build: cleanup duplicated code now in @bazel/typescript (#19004)

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-15945c8
+ * @license Angular v5.0.0-beta.6-d2707f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-15945c8');
+var VERSION = new Version('5.0.0-beta.6-d2707f1');
 
 /**
  * @fileoverview added by tsickle
