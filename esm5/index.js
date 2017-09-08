292c9a9244313a379846b3c34d2886101c936024
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 20:30:31 +0000

    8f79150 feat(router): add ActivationStart/End events

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-a69172f
+ * @license Angular v5.0.0-beta.6-8f79150
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-a69172f');
+var VERSION = new Version('5.0.0-beta.6-8f79150');
 
 /**
  * @fileoverview added by tsickle
