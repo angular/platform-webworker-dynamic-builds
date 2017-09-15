cdc8b73972cd030c3e1c293423edf371de225a4f
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:56 +0000

    ff87294 docs(changelog): fix the changelog link for comparing 4.4.1 to 4.3.6

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.1-0c72f7d');
+var VERSION = new Version('4.4.1-ff87294');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
