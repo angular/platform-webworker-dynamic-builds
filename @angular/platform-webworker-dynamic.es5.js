6c0444804f992c94846c8444527258c5e6583d50
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:11:21 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.0-RC.0-e8d1858');
+var VERSION = new Version('4.4.0-RC.0-0bafd03');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
