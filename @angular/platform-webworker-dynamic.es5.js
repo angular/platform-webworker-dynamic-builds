b701c8fa7da5ae0883a90a3c7c8b2bde3696ba17
Igor Minar <igor@angularjs.org>
2017-09-19 21:46:47 +0000

    0371538 fix(tsc-wrapped): deduplicate metadata only when the module is the same (#19261)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-351331a
+ * @license Angular v4.4.2-0371538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.2-351331a');
+var VERSION = new Version('4.4.2-0371538');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
