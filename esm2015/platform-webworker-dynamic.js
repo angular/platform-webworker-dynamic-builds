58001e36e87b13276f42d047949e5f0006f639bc
Igor Minar <igor@angularjs.org>
2017-09-20 23:45:38 +0000

    9e84719 test: fix cli-integration tests that got broken by yarn update (#19297)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-2c4107c');
+const VERSION = new Version('5.0.0-beta.7-9e84719');
 
 /**
  * @fileoverview added by tsickle
