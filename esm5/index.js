82ed348b0bbaed1161fbb7d280d0f2564e30ff3e
GitHub <noreply@github.com>
2017-09-20 17:26:51 +0000

    3215c4b build: pin docker image to a tag for CircleCI

--- esm5/index.js
+++ esm5/index.js
@@ -1,12 +1,17 @@
 /**
- * @license Angular v4.1.0-beta.1-683e657
+ * @license Angular v5.0.0-beta.7-3215c4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
-import { ResourceLoader, platformCoreDynamic } from '@angular/compiler';
+import { ResourceLoader } from '@angular/compiler';
 import { COMPILER_OPTIONS, PLATFORM_ID, Version, createPlatformFactory } from '@angular/core';
-import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -20,9 +25,14 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
  * Entry point for all public APIs of the common package.
  */
 /**
- * @stable
+ * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-683e657');
+var VERSION = new Version('5.0.0-beta.7-3215c4b');
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -31,16 +41,21 @@ var VERSION = new Version('4.1.0-beta.1-683e657');
  * found in the LICENSE file at https://angular.io/license
  */
 /**
- * @experimental API related to bootstrapping are still under review.
+ * \@experimental API related to bootstrapping are still under review.
  */
-var platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'workerAppDynamic', [
+var platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
     {
         provide: COMPILER_OPTIONS,
-        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl }] },
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
         multi: true
     },
     { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
 ]);
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -51,8 +66,18 @@ var platformWorkerAppDynamic = createPlatformFactory(platformCoreDynamic, 'worke
 /**
  * @module
  * @description
- * Entry point for all public APIs of the platform-browser-dynamic package.
+ * Entry point for all public APIs of this package.
  */
+
 // This file only reexports content of the `src` folder. Keep it that way.
-export { platformWorkerAppDynamic, VERSION };
-//# sourceMappingURL=platform-webworker-dynamic.es5.js.map
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=index.js.map
