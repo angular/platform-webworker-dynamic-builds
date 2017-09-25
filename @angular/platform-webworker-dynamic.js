a718d3c7dff7ca6f1823f304c0ed7045ad7fac91
Victor Berchet <victor@suumit.com>
2017-09-25 21:01:05 +0000

    c7aa8a1 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-5c99b01
+ * @license Angular v4.4.3-c7aa8a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.4.3-5c99b01');
+const VERSION = new Version('4.4.3-c7aa8a1');
 
 /**
  * @license
