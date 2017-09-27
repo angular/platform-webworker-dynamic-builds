5e3a3b70d3b2636cdeb293dbe02456d8438a550c
Victor Berchet <victor@suumit.com>
2017-09-27 21:05:24 +0000

    48c6ece docs(aio): remove links to top of documents (#16971)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8d8995f
+ * @license Angular v5.0.0-beta.7-48c6ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-8d8995f');
+var VERSION = new Version('5.0.0-beta.7-48c6ece');
 
 /**
  * @fileoverview added by tsickle
