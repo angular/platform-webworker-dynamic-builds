bebb2e9cbbe4fe2713ed6248c3a75a4209fe963a
Victor Berchet <victor@suumit.com>
2017-09-27 21:09:13 +0000

    55a7443 docs(aio): edit summary and next step headers (#16962)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-bc81fbd
+ * @license Angular v4.4.3-55a7443
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-bc81fbd');
+var VERSION = new Version('4.4.3-55a7443');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
