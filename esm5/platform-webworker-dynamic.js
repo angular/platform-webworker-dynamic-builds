900b62653d1d3b25f5497796bf43f0e8da306f60
Victor Berchet <victor@suumit.com>
2017-09-27 21:02:15 +0000

    f7c7038 docs(aio): update metadata documentation to include expression lowering (#19203)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-627f048
+ * @license Angular v5.0.0-beta.7-f7c7038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-627f048');
+var VERSION = new Version('5.0.0-beta.7-f7c7038');
 
 /**
  * @fileoverview added by tsickle
