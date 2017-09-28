579f3f7f59e4079d996ffc2942e6a4334b39bd56
Victor Berchet <victor@suumit.com>
2017-09-28 19:44:43 +0000

    d489ad5 docs: add changelog for 4.4.4

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-7fc2dce
+ * @license Angular v4.4.4-d489ad5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.4.3-7fc2dce');
+const VERSION = new Version('4.4.4-d489ad5');
 
 /**
  * @license
