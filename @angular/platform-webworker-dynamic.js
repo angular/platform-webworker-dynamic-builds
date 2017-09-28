d9a746d077e13e5f04dfd1fec3148b0a82074ba9
Victor Berchet <victor@suumit.com>
2017-09-28 19:26:15 +0000

    7fc2dce fix(compiler): do not consider a reference with members as a reference (#19466)

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c3b39ba
+ * @license Angular v4.4.3-7fc2dce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.4.3-c3b39ba');
+const VERSION = new Version('4.4.3-7fc2dce');
 
 /**
  * @license
