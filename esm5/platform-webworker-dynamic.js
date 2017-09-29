0da455dbc975457e6ef09dae2540b58a3cf233af
Victor Berchet <victor@suumit.com>
2017-09-29 00:15:43 +0000

    d442b68 feat(service-worker): introduce the @angular/service-worker package (#19274)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-30d5a2c
+ * @license Angular v5.0.0-beta.7-d442b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-30d5a2c');
+var VERSION = new Version('5.0.0-beta.7-d442b68');
 
 /**
  * @fileoverview added by tsickle
