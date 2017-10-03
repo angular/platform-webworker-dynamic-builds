080f918c2315b476442ed51fadfd634ad0c8f3b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:28:20 +0000

    7cecaf2 docs(aio): add ngVikings to the events page

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-eef7d8a
+ * @license Angular v5.0.0-rc.0-7cecaf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-eef7d8a');
+const VERSION = new Version('5.0.0-rc.0-7cecaf2');
 
 /**
  * @fileoverview added by tsickle
