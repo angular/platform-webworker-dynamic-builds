ca71c4bb5f6912636f411857c236b405babd6e85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:21:15 +0000

    696af79 fix(compiler): properly work on windows

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-d4d9009');
+var VERSION = new Version('5.0.0-rc.0-696af79');
 
 /**
  * @fileoverview added by tsickle
