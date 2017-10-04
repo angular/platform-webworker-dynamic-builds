d9229e55532522de258b8a2cb23dd9193edd9a3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:33:35 +0000

    f98eb35 docs(aio): document Custom Async validators

--- @angular/platform-webworker-dynamic.js
+++ @angular/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const VERSION = new Version('4.4.4-95f3b1d');
+const VERSION = new Version('4.4.4-f98eb35');
 
 /**
  * @license
