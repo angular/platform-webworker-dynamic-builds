f03e572b6ffe829940b6d4679917529bf35cea8a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:04:43 +0000

    6b96b06 docs(aio): fix typo in preview server config file comment

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-df91fd0
+ * @license Angular v4.4.4-6b96b06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-df91fd0');
+var VERSION = new Version('4.4.4-6b96b06');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
