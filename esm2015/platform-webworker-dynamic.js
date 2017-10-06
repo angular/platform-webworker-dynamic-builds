465016d3c3f632c2bf21602424a3f83b8c57a088
Chuck Jazdzewski <chuckj@google.com>
2017-10-06 22:57:35 +0000

    f83989b fix(compiler-cli): produce smaller source maps for templates (#19578)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-03227e6
+ * @license Angular v5.0.0-rc.1-f83989b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-03227e6');
+const VERSION = new Version('5.0.0-rc.1-f83989b');
 
 /**
  * @fileoverview added by tsickle
