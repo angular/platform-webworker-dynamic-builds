1b1621dfdee19a519d4635b247b18416ff6d6834
Tobias Bosch <tbosch1009@gmail.com>
2017-10-06 19:17:59 +0000

    03227e6 build(aio): example-boilerplate is no longer responsible for yarn install (#19511)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-81e9bdc
+ * @license Angular v5.0.0-rc.1-03227e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-81e9bdc');
+var VERSION = new Version('5.0.0-rc.1-03227e6');
 
 /**
  * @fileoverview added by tsickle
