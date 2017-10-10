f84296316eb75feb5948f240ebeaee4d2a37374b
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 22:00:41 +0000

    b0befd7 fix(compiler): `TestBed.overrideProvider` should keep imported `NgModule`s eager (#19624)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-dfa0973
+ * @license Angular v5.0.0-rc.1-b0befd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-dfa0973');
+const VERSION = new Version('5.0.0-rc.1-b0befd7');
 
 /**
  * @fileoverview added by tsickle
