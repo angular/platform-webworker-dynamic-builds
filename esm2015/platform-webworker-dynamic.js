0243a159c9228efdbedf32add69eece8e494a860
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 20:15:35 +0000

    931cf78 refactor(compiler): introduce `TestBed.deprecatedOverrideProvider` (#19558)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-d7eac7e
+ * @license Angular v5.0.0-rc.1-931cf78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-d7eac7e');
+const VERSION = new Version('5.0.0-rc.1-931cf78');
 
 /**
  * @fileoverview added by tsickle
