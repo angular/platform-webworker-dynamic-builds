b68ed2dc04cd36f66a6d054892be92234919c813
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:53:53 +0000

    3232125 build(aio): remove unnecessary `--` from yarn commands (#19565)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f89ac92
+ * @license Angular v4.4.4-3232125
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-f89ac92');
+var VERSION = new Version('4.4.4-3232125');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
