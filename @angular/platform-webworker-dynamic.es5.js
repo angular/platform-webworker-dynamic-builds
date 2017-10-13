3b402d0584e775819b66db5e529af92ea5502bf6
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:59:58 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-591dcc2');
+var VERSION = new Version('4.4.5-18f1b01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
