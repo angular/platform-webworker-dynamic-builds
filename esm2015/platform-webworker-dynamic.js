cc7d5f22f05b768b8ea998c8f13d6877d9046916
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:35:59 +0000

    e81d1fc docs(animations): add missing bracket to fadeAnimation

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-02394d2
+ * @license Angular v5.0.0-rc.2-e81d1fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-02394d2');
+const VERSION = new Version('5.0.0-rc.2-e81d1fc');
 
 /**
  * @fileoverview added by tsickle
