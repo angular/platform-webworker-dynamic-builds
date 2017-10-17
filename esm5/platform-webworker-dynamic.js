bbc29a740842798df1113a7ee3cc30184e1d0799
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 23:29:59 +0000

    01e4aa5 build: remove required BrowserStack run as it fails with “Access denied” (#19769)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-64b3619
+ * @license Angular v5.0.0-rc.2-01e4aa5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-64b3619');
+var VERSION = new Version('5.0.0-rc.2-01e4aa5');
 
 /**
  * @fileoverview added by tsickle
