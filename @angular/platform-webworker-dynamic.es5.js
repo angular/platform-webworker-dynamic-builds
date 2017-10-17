4de94ef82d0808c5ce1aa2253d0762401beb30ca
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:04:08 +0000

    ce0ac46 style: fix formatting of check-node-modules (#19720)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-1be22df
+ * @license Angular v4.4.5-ce0ac46
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-1be22df');
+var VERSION = new Version('4.4.5-ce0ac46');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
