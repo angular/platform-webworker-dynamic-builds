227928b70771a3992fa4701413bfbab355769dd0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:03:29 +0000

    14380ff build: add warning about changing ngtools_api2

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.3-441e01c');
+var VERSION = new Version('5.0.0-rc.3-14380ff');
 
 /**
  * @fileoverview added by tsickle
