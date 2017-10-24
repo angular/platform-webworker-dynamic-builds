cda65571bd86c2b510f8059655be83382b8a60bf
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 05:18:56 +0000

    a82f863 fix(compiler-cli): report all diagnostic error messages (#19886)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-bde5701
+ * @license Angular v5.0.0-rc.4-a82f863
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.4-bde5701');
+var VERSION = new Version('5.0.0-rc.4-a82f863');
 
 /**
  * @fileoverview added by tsickle
