b94b1c9741d3d887eadf4a66b8220899e0b5fda9
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:44:37 +0000

    951bd33 build: add `release` as a valid commit message subject (#19955)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-420852e');
+var VERSION = new Version('5.0.0-rc.8-951bd33');
 
 /**
  * @fileoverview added by tsickle
