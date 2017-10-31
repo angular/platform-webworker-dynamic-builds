9d253558a3ce766ad686328dca7ec07c84ded14c
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:47:34 +0000

    f5b7f2b docs: add changelog for 5.0.0-rc.9

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-cf5fce8');
+const VERSION = new Version('5.0.0-rc.9-f5b7f2b');
 
 /**
  * @fileoverview added by tsickle
