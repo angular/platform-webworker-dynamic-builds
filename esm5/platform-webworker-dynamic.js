4f19fc10cc811c18e6a45dded5d2d314ad4fb1e6
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:27:33 +0000

    de24d54 fix(compiler): report errors properly in G3 in certain conditions (#20041)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-c295aee
+ * @license Angular v5.0.0-rc.8-de24d54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-c295aee');
+var VERSION = new Version('5.0.0-rc.8-de24d54');
 
 /**
  * @fileoverview added by tsickle
