812f555fbd10a131e4ce4dfa59e30935892eba0d
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:31:36 +0000

    812786f fix: add missing globals from each rollup configuration (#20028)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-de24d54');
+var VERSION = new Version('5.0.0-rc.8-812786f');
 
 /**
  * @fileoverview added by tsickle
