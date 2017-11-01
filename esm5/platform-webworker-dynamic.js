9c424ec20fea60f7d5eb0f593bc8223b54637c04
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:06:10 +0000

    16d7258 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-a1ec65b
+ * @license Angular v5.0.0-16d7258
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-a1ec65b');
+var VERSION = new Version('5.0.0-16d7258');
 
 /**
  * @fileoverview added by tsickle
