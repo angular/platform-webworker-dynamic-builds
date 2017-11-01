5a116a4f53c5a746c5a3c7e5c63e1b9225a01764
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:54:13 +0000

    a1ec65b docs(aio): fix typo in attribute-directives

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-851e1cf
+ * @license Angular v5.0.0-a1ec65b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-851e1cf');
+const VERSION = new Version('5.0.0-a1ec65b');
 
 /**
  * @fileoverview added by tsickle
