d5a2d9b291b75d8635399e46642694cd68dec836
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:53:27 +0000

    280dada docs(aio): fix typo in attribute-directives

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-c30eff8');
+var VERSION = new Version('5.0.0-280dada');
 
 /**
  * @fileoverview added by tsickle
