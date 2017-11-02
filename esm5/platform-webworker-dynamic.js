ac935222f08e1ea96f650c97c535a6fb1defe639
Victor Berchet <victor@suumit.com>
2017-11-02 21:53:53 +0000

    a41558e docs: BrowserSupport guide for CLI (#19843)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-132c071
+ * @license Angular v5.1.0-beta.0-a41558e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-132c071');
+var VERSION = new Version('5.1.0-beta.0-a41558e');
 
 /**
  * @fileoverview added by tsickle
