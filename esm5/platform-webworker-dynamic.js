d6f02043ccd188c9f2bd23c9cf57cfc9cdc21b97
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:41 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-37a740b');
+var VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
