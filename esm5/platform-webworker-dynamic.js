3bd8df7929c01ef5dcdf47826e89247491e37aa5
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:51 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new Version('5.1.0-beta.0-068348e');
 
 /**
  * @fileoverview added by tsickle
