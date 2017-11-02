a7b462d141f6ab88b817263c247238572319efdc
Victor Berchet <victor@suumit.com>
2017-11-02 23:43:39 +0000

    678d1cf fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-25843fe
+ * @license Angular v5.0.0-678d1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-25843fe');
+const VERSION = new Version('5.0.0-678d1cf');
 
 /**
  * @fileoverview added by tsickle
