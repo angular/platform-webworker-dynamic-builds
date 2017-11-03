3b1aed1d723e437eb8559706af5886ffe121505f
Victor Berchet <victor@suumit.com>
2017-11-03 19:14:39 +0000

    03f080b docs: Deployment guide for CLI (#19839)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-26f8299');
+const VERSION = new Version('5.1.0-beta.0-03f080b');
 
 /**
  * @fileoverview added by tsickle
