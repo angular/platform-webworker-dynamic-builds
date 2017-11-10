206c418280a87f7714fe385a06013df795d21fb8
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:40 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-28985cb');
+var VERSION = new Version('5.0.1-424a323');
 
 /**
  * @fileoverview added by tsickle
