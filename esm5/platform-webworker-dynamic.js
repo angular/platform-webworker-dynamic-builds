bfbd041a9a62c0a1926397987ea12c4546876a8d
Jason Aden <jasonaden@google.com>
2017-11-14 22:06:29 +0000

    12af6d3 docs(core): change from deprecated renderer to renderer2 (#19920)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-b9bd320
+ * @license Angular v5.1.0-beta.0-12af6d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-b9bd320');
+var VERSION = new Version('5.1.0-beta.0-12af6d3');
 
 /**
  * @fileoverview added by tsickle
