7332a37085e09506618ecd5f00e5b0254fc5df5b
Miško Hevery <misko@hevery.com>
2017-11-16 06:59:37 +0000

    dcfffbf build: fix build failures (#20470)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-336041a');
+var VERSION = new Version('5.1.0-beta.0-dcfffbf');
 
 /**
  * @fileoverview added by tsickle
