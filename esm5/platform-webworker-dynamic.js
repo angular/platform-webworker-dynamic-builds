d7caf381e31b64e6a110bd961a45a62f91419132
Miško Hevery <misko@hevery.com>
2017-11-16 01:01:34 +0000

    f8658cd Revert "fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)"

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-043e408');
+var VERSION = new Version('5.1.0-beta.0-f8658cd');
 
 /**
  * @fileoverview added by tsickle
