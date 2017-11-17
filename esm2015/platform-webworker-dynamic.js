41dcabfa13ce989d5fb4c8fc4c8fe1cc6c770bba
Miško Hevery <misko@hevery.com>
2017-11-17 04:35:15 +0000

    1861e41 fix(animations): validate against trigger() names that use @ symbols (#20326)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-4064cbe
+ * @license Angular v5.1.0-beta.1-1861e41
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-4064cbe');
+const VERSION = new Version('5.1.0-beta.1-1861e41');
 
 /**
  * @fileoverview added by tsickle
