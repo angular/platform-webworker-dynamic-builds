5c853f320b1078860c603ef83f7feac914bc5f4b
Miško Hevery <misko@hevery.com>
2017-11-17 22:10:15 +0000

    365712e docs(aio): fix typo in Attribute Directives documentation (#20143)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-1861e41');
+const VERSION = new Version('5.1.0-beta.1-365712e');
 
 /**
  * @fileoverview added by tsickle
