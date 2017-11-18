ccbbabbe63b8efaf3bade46e42d9304773f9a965
Miško Hevery <misko@hevery.com>
2017-11-18 00:42:54 +0000

    82aace6 fix(core): should support event.stopImmediatePropagation (#20469)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-b1f8eb1
+ * @license Angular v5.0.2-82aace6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-b1f8eb1');
+var VERSION = new Version('5.0.2-82aace6');
 
 /**
  * @fileoverview added by tsickle
