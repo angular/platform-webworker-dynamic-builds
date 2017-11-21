53e956737f8f54f90721d380204f620bd4b7c3b7
Miško Hevery <misko@hevery.com>
2017-11-21 17:08:14 +0000

    b53ead4 fix(compiler): support event bindings in `fullTemplateTypeCheck` (#20490)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-82aace6
+ * @license Angular v5.0.2-b53ead4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-82aace6');
+var VERSION = new Version('5.0.2-b53ead4');
 
 /**
  * @fileoverview added by tsickle
