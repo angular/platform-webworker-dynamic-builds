6f9c3f334daa2a6042959f83214ee4126802821f
Miško Hevery <misko@hevery.com>
2017-11-29 18:30:54 +0000

    65a2cb8 fix(core): should use native addEventListener in ngZone (#20672)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-c2b3792
+ * @license Angular v5.1.0-beta.2-65a2cb8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-c2b3792');
+var VERSION = new Version('5.1.0-beta.2-65a2cb8');
 
 /**
  * @fileoverview added by tsickle
