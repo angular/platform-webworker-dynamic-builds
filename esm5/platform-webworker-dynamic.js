b91fb06ef2b65db7627327e9fe7e55210986dc7e
Miško Hevery <misko@hevery.com>
2017-11-29 04:45:20 +0000

    c2b3792 fix(animations): ensure multi-level leave animations work (#19455)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-8bb42df');
+var VERSION = new Version('5.1.0-beta.2-c2b3792');
 
 /**
  * @fileoverview added by tsickle
