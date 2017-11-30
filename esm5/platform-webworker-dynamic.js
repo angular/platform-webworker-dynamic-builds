b3761e1c8350b03266ba4a7d00541e1721b4b39e
Miško Hevery <misko@hevery.com>
2017-11-30 03:56:14 +0000

    b9e4d62 docs(aio): remove services plurality (#20696)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba850b3
+ * @license Angular v5.1.0-beta.2-b9e4d62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-ba850b3');
+var VERSION = new Version('5.1.0-beta.2-b9e4d62');
 
 /**
  * @fileoverview added by tsickle
