1aafaf51706f34a9174f56e7c98bc81f4f6fb6b2
Misko Hevery <misko@hevery.com>
2017-11-30 04:51:11 +0000

    9e69d97 style: broken build due to missing new lines

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-b450c83
+ * @license Angular v5.0.3-9e69d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.3-b450c83');
+var VERSION = new Version('5.0.3-9e69d97');
 
 /**
  * @fileoverview added by tsickle
