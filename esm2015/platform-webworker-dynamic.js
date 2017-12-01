c8b63cd32750b9525c4262b2573d8c1c847d722f
Miško Hevery <misko@hevery.com>
2017-12-01 19:49:52 +0000

    445d833 fix(build): accidental character in commit-message.json

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-cbd93fe
+ * @license Angular v5.0.4-445d833
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.4-cbd93fe');
+const VERSION = new Version('5.0.4-445d833');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-cbd93fe
+ * @license Angular v5.0.4-445d833
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.4-cbd93fe');
+var VERSION = new Version('5.0.4-445d833');
 
 /**
  * @fileoverview added by tsickle
