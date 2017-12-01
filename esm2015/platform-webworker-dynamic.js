daa64a14573f04332a1058946d2d2786f38e750c
Misko Hevery <misko@hevery.com>
2017-12-01 04:37:20 +0000

    e95b5d7 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-9e69d97
+ * @license Angular v5.0.3-e95b5d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.3-9e69d97');
+const VERSION = new Version('5.0.3-e95b5d7');
 
 /**
  * @fileoverview added by tsickle
