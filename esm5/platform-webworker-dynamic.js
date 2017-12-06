1ef7fe7b00114a657417b80211c03020148752b0
Igor Minar <igor@angular.io>
2017-12-06 21:00:00 +0000

    c2dbc55 release: cut the 5.1.0 release

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-b78ada1
+ * @license Angular v5.1.0-c2dbc55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.1-b78ada1');
+var VERSION = new Version('5.1.0-c2dbc55');
 
 /**
  * @fileoverview added by tsickle
