d80f31d320bd6a98c809be031ddac772d7001dd5
Igor Minar <igor@angularjs.org>
2017-12-06 06:15:58 +0000

    6d15dd7 docs(aio): architecture review for a11y (#17848)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-a2fde2b
+ * @license Angular v5.0.5-6d15dd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-a2fde2b');
+var VERSION = new Version('5.0.5-6d15dd7');
 
 /**
  * @fileoverview added by tsickle
