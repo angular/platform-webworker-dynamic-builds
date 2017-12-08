ff0d40145cf2685240af2d9f92e9836d926c9563
Jason Aden <jasonaden@google.com>
2017-12-08 18:29:11 +0000

    225baf4 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-46aa0a1
+ * @license Angular v5.1.0-225baf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-46aa0a1');
+var VERSION = new Version('5.1.0-225baf4');
 
 /**
  * @fileoverview added by tsickle
