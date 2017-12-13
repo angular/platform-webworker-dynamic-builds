51c5bd63c0b7267a15ac37e338363e870f76aa2c
Alex Eagle <alexeagle@google.com>
2017-12-13 23:21:46 +0000

    541a391 ci: parallelize bazel build and test

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-d3dbb9d
+ * @license Angular v5.2.0-beta.0-541a391
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-d3dbb9d');
+const VERSION = new Version('5.2.0-beta.0-541a391');
 
 /**
  * @fileoverview added by tsickle
