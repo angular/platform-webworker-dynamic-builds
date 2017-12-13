0e75aabea40efe2357ba82d69f581a2d9d99184a
Jason Aden <jasonaden@google.com>
2017-12-13 19:56:18 +0000

    e3140ae docs: add changelog for 5.1.1

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d6da798
+ * @license Angular v5.1.1-e3140ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-d6da798');
+const VERSION = new Version('5.1.1-e3140ae');
 
 /**
  * @fileoverview added by tsickle
