6c440219f3c04078145d55a54ec3ffe86ff4d1d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:57:47 +0000

    2686219 ci: sync CircleCI config from master to 5.0.x

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-eb57bc3');
+var VERSION = new Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
