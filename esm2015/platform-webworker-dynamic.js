e1d8db0aef5db1d137e0d3ec8131ae841303c59c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:27:57 +0000

    a474276 test(aio): correct usage of fakeAsync and inject together in test

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-d91ff17
+ * @license Angular v5.2.0-beta.0-a474276
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-d91ff17');
+const VERSION = new Version('5.2.0-beta.0-a474276');
 
 /**
  * @fileoverview added by tsickle
