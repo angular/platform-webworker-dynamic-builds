b7f35fcdd6d0882e8c2a8fa8217a3589d963d679
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 22:13:32 +0000

    abca7c0 docs: add changelog for 5.2.0-beta.1

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-135ead6
+ * @license Angular v5.2.0-beta.1-abca7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-135ead6');
+var VERSION = new Version('5.2.0-beta.1-abca7c0');
 
 /**
  * @fileoverview added by tsickle
