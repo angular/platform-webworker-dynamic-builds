8dad2ce63ae4a97e5ef6096e2c3d65c38b363182
Igor Minar <igor@angularjs.org>
2017-12-21 20:24:54 +0000

    d1f4500 fix(animations): renaming issue with DOMAnimation. (#21125)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-6353b77
+ * @license Angular v5.1.2-d1f4500
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-6353b77');
+const VERSION = new Version('5.1.2-d1f4500');
 
 /**
  * @fileoverview added by tsickle
