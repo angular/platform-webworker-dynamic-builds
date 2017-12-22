20677a603073b092aba85392f7daa9a19ee1a30f
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:40 +0000

    bc66d27 docs(service-worker): fix word wrap (#21114)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-27fc458
+ * @license Angular v5.2.0-beta.1-bc66d27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.1-27fc458');
+const VERSION = new Version('5.2.0-beta.1-bc66d27');
 
 /**
  * @fileoverview added by tsickle
