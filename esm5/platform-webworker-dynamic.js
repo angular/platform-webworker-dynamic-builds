adf10539a0a82da0ae154a30a9b6c1bd9d224e06
Igor Minar <igor@angular.io>
2017-12-23 05:58:53 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-5de91fe');
+var VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle
