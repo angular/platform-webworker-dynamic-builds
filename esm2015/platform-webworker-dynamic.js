f22bfcde4c891f373849ddc016336d25f6b78fd9
Miško Hevery <misko@hevery.com>
2018-01-02 19:03:21 +0000

    3e47ea2 fix(language-service): ignore null metadatas (#20557)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-bc7a6d7
+ * @license Angular v5.2.0-beta.1-3e47ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.1-bc7a6d7');
+const VERSION = new Version('5.2.0-beta.1-3e47ea2');
 
 /**
  * @fileoverview added by tsickle
