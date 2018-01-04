60f0dfd323013d7ea63459a15e67a1fbdb91e61b
Kara Erickson <karakara@google.com>
2018-01-04 23:25:33 +0000

    5efb751 build(aio): fix example protractor config for Travis (#21309)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d79a0ba
+ * @license Angular v5.1.3-5efb751
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-d79a0ba');
+var VERSION = new Version('5.1.3-5efb751');
 
 /**
  * @fileoverview added by tsickle
