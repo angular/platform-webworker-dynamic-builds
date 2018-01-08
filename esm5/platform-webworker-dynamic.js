31574ec2393e035b14a730d0f3208837a9f38ad8
Kara Erickson <karakara@google.com>
2018-01-08 21:28:16 +0000

    fda3815 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-4f7c13d
+ * @license Angular v5.1.3-fda3815
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -27,7 +27,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-4f7c13d');
+var VERSION = new Version('5.1.3-fda3815');
 
 /**
  * @fileoverview added by tsickle
