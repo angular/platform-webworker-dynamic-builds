0268538a71dc626ba193339e618a52c531130ec6
Alex Eagle <alexeagle@google.com>
2018-01-10 04:34:54 +0000

    a0166bd docs(bazel): document an installation issue

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-0d1611f
+ * @license Angular v5.2.0-a0166bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-0d1611f');
+const VERSION = new Version('5.2.0-a0166bd');
 
 /**
  * @fileoverview added by tsickle
