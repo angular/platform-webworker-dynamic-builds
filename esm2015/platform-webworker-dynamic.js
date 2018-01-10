a60c687e25e8789f84ddacc73d0335d16209a119
Alex Eagle <alexeagle@google.com>
2018-01-10 22:49:42 +0000

    7b463df fix(aio): preserve static Observable methods (#21351)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-650f5fb');
+const VERSION = new Version('5.2.0-7b463df');
 
 /**
  * @fileoverview added by tsickle
