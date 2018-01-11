f4b4ceb14b752941fdf7363a74e0fa236043585f
Alex Eagle <alexeagle@google.com>
2018-01-11 15:21:47 +0000

    5eaaac3 refactor(ivy): remove `type` from `DirectiveDef` (#21374)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c0080d7
+ * @license Angular v5.2.0-5eaaac3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-c0080d7');
+const VERSION = new Version('5.2.0-5eaaac3');
 
 /**
  * @fileoverview added by tsickle
