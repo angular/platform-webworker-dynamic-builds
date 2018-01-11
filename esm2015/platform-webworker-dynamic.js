9dc203283b78280b04ed1d463187f19401e8013b
Alex Eagle <alexeagle@google.com>
2018-01-11 15:23:10 +0000

    b49d54e docs(forms): clarify note in reactive forms doc (#21134)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-55fd82e');
+const VERSION = new Version('5.2.0-b49d54e');
 
 /**
  * @fileoverview added by tsickle
