c39472c92e646f526bb6b277b62323db9f803ccc
Alex Eagle <alexeagle@google.com>
2018-01-12 20:28:16 +0000

    f23896f docs: fix release schedule date (#21474)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-0e59d18');
+var VERSION = new Version('5.2.0-f23896f');
 
 /**
  * @fileoverview added by tsickle
