69dcbb047215e8179e5273f635c9a5e8963edc9b
Alex Eagle <alexeagle@google.com>
2018-01-17 17:24:38 +0000

    a8fe0cb build(bazel): remove spurious file (#21455)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-030c68d
+ * @license Angular v5.2.0-a8fe0cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-030c68d');
+var VERSION = new Version('5.2.0-a8fe0cb');
 
 /**
  * @fileoverview added by tsickle
