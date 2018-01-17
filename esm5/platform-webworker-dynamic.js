52fe5245bb5edb18bd5446618fde3c52f9c4ca5e
Alex Eagle <alexeagle@google.com>
2018-01-17 15:46:38 +0000

    d3bf54b feat(core): add binding name to content changed error (#20352)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7e3f9a4
+ * @license Angular v5.2.0-d3bf54b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-7e3f9a4');
+var VERSION = new Version('5.2.0-d3bf54b');
 
 /**
  * @fileoverview added by tsickle
