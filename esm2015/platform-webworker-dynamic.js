a5821ec56b769a82cfa57d86480319ef30ef36c2
Misko Hevery <misko@hevery.com>
2018-01-25 05:10:20 +0000

    da9e57b build: Update to latest rules_typescript. (#21675)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-5423882
+ * @license Angular v5.2.1-da9e57b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.1-5423882');
+const VERSION = new Version('5.2.1-da9e57b');
 
 /**
  * @fileoverview added by tsickle
