6022e64dd6aa06cde5e1378b93e47466af4d2d66
Misko Hevery <misko@hevery.com>
2018-01-25 06:43:19 +0000

    5269ce2 feat(ivy): support deep queries through view boundaries (#21700)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-3e03dbe
+ * @license Angular v6.0.0-beta.1-5269ce2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-3e03dbe');
+const VERSION = new Version('6.0.0-beta.1-5269ce2');
 
 /**
  * @fileoverview added by tsickle
