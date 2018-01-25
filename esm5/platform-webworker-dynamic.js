d8d5c535196ed52f0c6bbb0f95b788870720a93f
Miško Hevery <misko@hevery.com>
2018-01-25 22:03:14 +0000

    23596b3 docs(aio): fix missing stylesheet in component-styles example (#21772)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-5269ce2
+ * @license Angular v6.0.0-beta.1-23596b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-5269ce2');
+var VERSION = new Version('6.0.0-beta.1-23596b3');
 
 /**
  * @fileoverview added by tsickle
