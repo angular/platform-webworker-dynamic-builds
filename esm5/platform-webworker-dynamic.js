28603587fc00005d149d3dde65c31da010bb3abd
Misko Hevery <misko@hevery.com>
2018-01-26 07:00:00 +0000

    08aa54e ci: Add back the CLI integration test with pinning (#21555)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-fac4d8d
+ * @license Angular v6.0.0-beta.1-08aa54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-fac4d8d');
+var VERSION = new Version('6.0.0-beta.1-08aa54e');
 
 /**
  * @fileoverview added by tsickle
