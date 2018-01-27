f5e80943b6d6b1366cdd46fc510dd0797338b578
Misko Hevery <misko@hevery.com>
2018-01-27 19:10:38 +0000

    87754ad fix(ivy): emit type `type` fields (#21789)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-6245637
+ * @license Angular v6.0.0-beta.1-87754ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-6245637');
+var VERSION = new Version('6.0.0-beta.1-87754ad');
 
 /**
  * @fileoverview added by tsickle
