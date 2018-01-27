febbc3299169d0817de681f00fbd8aff5e76d864
Jason Aden <jasonaden@google.com>
2018-01-27 00:01:14 +0000

    2af19c9 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dcca799
+ * @license Angular v6.0.0-beta.1-2af19c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-dcca799');
+const VERSION = new Version('6.0.0-beta.1-2af19c9');
 
 /**
  * @fileoverview added by tsickle
