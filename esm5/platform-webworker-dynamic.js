5a609dee86f2dd21575a0c7e4208fe8a6ae7c25e
Jason Aden <jasonaden@google.com>
2018-01-29 19:57:22 +0000

    a89e709 docs: change ”it's" to "its" as needed in several docs. (#21867)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-6a7689d
+ * @license Angular v5.2.2-a89e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-6a7689d');
+var VERSION = new Version('5.2.2-a89e709');
 
 /**
  * @fileoverview added by tsickle
