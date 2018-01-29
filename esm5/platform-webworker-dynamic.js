2c5a7dade589b9e4f522042be4fa5971a1f0bb6c
Jason Aden <jasonaden@google.com>
2018-01-29 20:02:00 +0000

    72265f7 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-6e8bc31
+ * @license Angular v6.0.0-beta.1-72265f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-6e8bc31');
+var VERSION = new Version('6.0.0-beta.1-72265f7');
 
 /**
  * @fileoverview added by tsickle
