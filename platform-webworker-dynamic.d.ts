2635c91852655f2b1b67e6de91d1504e794f4c50
GitHub <noreply@github.com>
2018-01-30 23:05:50 +0000

    27a2515 ci: mark PRs with rejection as not green

--- platform-webworker-dynamic.d.ts
+++ platform-webworker-dynamic.d.ts
@@ -1,13 +1,4 @@
 /**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * Generated bundle index. Do not edit.
  */
-/**
- * @module
- * @description
- * Entry point for all public APIs of the platform-browser-dynamic package.
- */
-export * from './src/platform-webworker-dynamic';
+export * from './public_api';
