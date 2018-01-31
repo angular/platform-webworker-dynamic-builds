5d75666a4cabb037fa191b0f98ab2973e4aedd4c
Jason Aden <jasonaden@google.com>
2018-01-31 18:40:38 +0000

    06d6c76 fix(ivy): compiler should emit new refresh pattern (#21862)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-407b5cf
+ * @license Angular v6.0.0-beta.1-06d6c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-407b5cf');
+var VERSION = new Version('6.0.0-beta.1-06d6c76');
 
 /**
  * @fileoverview added by tsickle
