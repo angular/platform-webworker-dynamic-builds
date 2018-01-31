1f8c201d7d3c967cc29fad378e6cc78839bdf236
Jason Aden <jasonaden@google.com>
2018-01-31 18:45:50 +0000

    c83c416 build(aio): upgrade to dgeni-packages 0.24.0 (#21802)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-9dca5f2
+ * @license Angular v6.0.0-beta.1-c83c416
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-9dca5f2');
+const VERSION = new Version('6.0.0-beta.1-c83c416');
 
 /**
  * @fileoverview added by tsickle
