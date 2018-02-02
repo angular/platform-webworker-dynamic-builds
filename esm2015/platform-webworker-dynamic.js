bf194d0ae6a36f3f19b21cba39b87c5a17fb3439
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:32:23 +0000

    447783e docs: add docs for IE (#21824)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-743d8bc
+ * @license Angular v6.0.0-beta.2-447783e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-743d8bc');
+const VERSION = new Version('6.0.0-beta.2-447783e');
 
 /**
  * @fileoverview added by tsickle
