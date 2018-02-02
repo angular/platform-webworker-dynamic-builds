033f9af05a35844c632e4696af94cafcab09611f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:38:27 +0000

    de25d18 docs(aio): update docs changelog with links to ts-to-js guide (#21763)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-447783e
+ * @license Angular v6.0.0-beta.2-de25d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-447783e');
+var VERSION = new Version('6.0.0-beta.2-de25d18');
 
 /**
  * @fileoverview added by tsickle
