aca9550123025f99654bea8362a3cf58aaf09256
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:10:57 +0000

    9d3326c docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-0846784
+ * @license Angular v6.0.0-beta.2-9d3326c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-0846784');
+const VERSION = new Version('6.0.0-beta.2-9d3326c');
 
 /**
  * @fileoverview added by tsickle
