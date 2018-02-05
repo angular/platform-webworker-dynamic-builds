8de546155da6473358acd352ef958541af774e32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:46:45 +0000

    9a0700f build(aio): add API static members to search index (#21988)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-ae7bc22
+ * @license Angular v6.0.0-beta.2-9a0700f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-ae7bc22');
+var VERSION = new Version('6.0.0-beta.2-9a0700f');
 
 /**
  * @fileoverview added by tsickle
