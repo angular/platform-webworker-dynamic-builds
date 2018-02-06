31618d4d16eaab15c51a47967391612931793101
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:22:04 +0000

    c6645e7 fix(core): fix proper propagation of subscriptions in EventEmitter (#22016)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-f0396f1
+ * @license Angular v5.2.3-c6645e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-f0396f1');
+var VERSION = new Version('5.2.3-c6645e7');
 
 /**
  * @fileoverview added by tsickle
