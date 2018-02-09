84132e501a68fca0357db24575dabdf8f51a1767
Miško Hevery <misko@hevery.com>
2018-02-09 21:31:31 +0000

    7007f51 feat(aio): first pass API docs redesign (#21874)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-cf8d512
+ * @license Angular v6.0.0-beta.3-7007f51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-cf8d512');
+var VERSION = new Version('6.0.0-beta.3-7007f51');
 
 /**
  * @fileoverview added by tsickle
