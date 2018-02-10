d45cb022231f5bceebf4c54a54146395a509728a
Miško Hevery <misko@hevery.com>
2018-02-10 01:37:28 +0000

    b081dfe fix(bazel): allow TS to read ambient typings (#21876)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-c878d55
+ * @license Angular v6.0.0-beta.3-b081dfe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-c878d55');
+const VERSION = new Version('6.0.0-beta.3-b081dfe');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-c878d55
+ * @license Angular v6.0.0-beta.3-b081dfe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-c878d55');
+var VERSION = new Version('6.0.0-beta.3-b081dfe');
 
 /**
  * @fileoverview added by tsickle
