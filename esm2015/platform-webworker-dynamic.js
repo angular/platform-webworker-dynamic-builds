252846d76f44d5467165193a9480a44b284d034e
Miško Hevery <misko@hevery.com>
2018-02-12 18:42:10 +0000

    88bec23 fix(aio): remove broken span closing tag (#22146)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a57df4e
+ * @license Angular v6.0.0-beta.3-88bec23
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-a57df4e');
+const VERSION = new Version('6.0.0-beta.3-88bec23');
 
 /**
  * @fileoverview added by tsickle
