30a34f8be372c9e5b823f4ca5a07d0675770bad0
Miško Hevery <misko@hevery.com>
2018-02-12 18:25:28 +0000

    92a5876 refactor(router): move activation to private method (#22033)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-92d7060
+ * @license Angular v6.0.0-beta.3-92a5876
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-92d7060');
+const VERSION = new Version('6.0.0-beta.3-92a5876');
 
 /**
  * @fileoverview added by tsickle
