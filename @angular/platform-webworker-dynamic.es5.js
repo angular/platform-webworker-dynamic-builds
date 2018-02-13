0f7f6d0f3b0ccc2d75e6ecd1fc95e20ed21ee0b4
Miško Hevery <misko@hevery.com>
2018-02-13 18:39:03 +0000

    2c5cf19 fix(core): use appropriate inert document strategy for Firefox & Safari (#22077)

--- @angular/platform-webworker-dynamic.es5.js
+++ @angular/platform-webworker-dynamic.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.6-7231f5e
+ * @license Angular v4.4.6-2c5cf19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.4.6-7231f5e');
+var VERSION = new Version('4.4.6-2c5cf19');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
