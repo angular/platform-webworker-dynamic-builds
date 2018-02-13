176d9b20643c29130f192f24b8dd7f28dfe50707
Miško Hevery <misko@hevery.com>
2018-02-13 19:04:09 +0000

    4f19491 ci: remove conditional clause for bazel install (#22170)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-2de0d4c
+ * @license Angular v6.0.0-beta.3-4f19491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-2de0d4c');
+var VERSION = new Version('6.0.0-beta.3-4f19491');
 
 /**
  * @fileoverview added by tsickle
