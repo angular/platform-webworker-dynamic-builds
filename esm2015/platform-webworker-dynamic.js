aff18c1c8febad9f90e21bcd5b9cde7be54c172b
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 23:54:46 +0000

    0c88d5d style: fix typos boostrap to bootstrap (#21917)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-0b8b06e
+ * @license Angular v5.2.5-0c88d5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-0b8b06e');
+const VERSION = new Version('5.2.5-0c88d5d');
 
 /**
  * @fileoverview added by tsickle
