d851acbc080f9caf505723c6054b1d7278b61db2
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 23:13:54 +0000

    f628797 Revert "refactor(ivy): assertion (#22189)"

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-ba9cd5b');
+var VERSION = new Version('6.0.0-beta.4-f628797');
 
 /**
  * @fileoverview added by tsickle
