9c706dc56264f54682a36490b24d8e710b75a2ac
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 18:02:49 +0000

    0aa9b46 Revert "build: allow bazel build ... (#22168)"

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0b68312
+ * @license Angular v6.0.0-beta.4-0aa9b46
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-0b68312');
+var VERSION = new Version('6.0.0-beta.4-0aa9b46');
 
 /**
  * @fileoverview added by tsickle
