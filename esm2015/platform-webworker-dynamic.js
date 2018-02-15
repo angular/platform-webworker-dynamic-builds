5d579167037a4a58aaddf22adf0cf70421922a18
Victor Berchet <victor@suumit.com>
2018-02-15 21:37:12 +0000

    03d93c9 Revert: "build: merge-pr new checks that all requested changes have been addressed (#21817)"

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-0aa9b46');
+const VERSION = new Version('6.0.0-beta.4-03d93c9');
 
 /**
  * @fileoverview added by tsickle
