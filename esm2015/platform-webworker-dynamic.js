46c9ffb8e9de1e4c1dd01c7bb5af9e7c545a368b
Victor Berchet <victor@suumit.com>
2018-02-16 17:35:07 +0000

    f1a0632 feat(core): set preserveWhitespaces to false by default (#22046)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-d241532
+ * @license Angular v6.0.0-beta.4-f1a0632
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-d241532');
+const VERSION = new Version('6.0.0-beta.4-f1a0632');
 
 /**
  * @fileoverview added by tsickle
