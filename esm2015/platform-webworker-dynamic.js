e6653f84be6a2ea9e9e764fbcca5ddc369923789
Victor Berchet <victor@suumit.com>
2018-02-16 23:16:27 +0000

    185a6ab build: add esm5 build (#22258)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-6b45784
+ * @license Angular v5.2.5-185a6ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-6b45784');
+const VERSION = new Version('5.2.5-185a6ab');
 
 /**
  * @fileoverview added by tsickle
