bf2ba733cf12993c8120a32f705207990c02cead
Victor Berchet <victor@suumit.com>
2018-02-17 02:27:11 +0000

    f693be3 feat(ivy): add pureFunction0 instruction (#22214)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-e1bf067
+ * @license Angular v6.0.0-beta.4-f693be3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-e1bf067');
+const VERSION = new Version('6.0.0-beta.4-f693be3');
 
 /**
  * @fileoverview added by tsickle
