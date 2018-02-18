cfa56107c73a08eca5762932ef6f0b8f41eb2739
Victor Berchet <victor@suumit.com>
2018-02-18 21:34:05 +0000

    396bc0d build: use authenticated mode for the merge script (#22269)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-861250b
+ * @license Angular v5.2.5-396bc0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-861250b');
+var VERSION = new Version('5.2.5-396bc0d');
 
 /**
  * @fileoverview added by tsickle
