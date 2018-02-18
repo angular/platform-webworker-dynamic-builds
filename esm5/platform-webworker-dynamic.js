57d7128a1c5e5c8abe32047d5f5475dec736b9c8
Victor Berchet <victor@suumit.com>
2018-02-18 22:10:41 +0000

    9dae97c docs: correct grammar mistakes in CONTRIBUTING.md (#22285)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-228eb9f
+ * @license Angular v5.2.5-9dae97c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-228eb9f');
+var VERSION = new Version('5.2.5-9dae97c');
 
 /**
  * @fileoverview added by tsickle
