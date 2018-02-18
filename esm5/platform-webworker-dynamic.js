386d6d06df9b31b12b80d786a3c033ca11b16c8e
Victor Berchet <victor@suumit.com>
2018-02-18 23:32:54 +0000

    c726d1d docs(aio): add angular-buch to resources (#22163)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-9dae97c
+ * @license Angular v5.2.5-c726d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-9dae97c');
+var VERSION = new Version('5.2.5-c726d1d');
 
 /**
  * @fileoverview added by tsickle
