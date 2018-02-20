19ce8b02789bac8660dde7de824cd111c83efa6e
Victor Berchet <victor@suumit.com>
2018-02-20 18:33:30 +0000

    a346d28 test(language-service): fix minor typos (#21372) PR Close #21372

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-09b4612
+ * @license Angular v5.2.5-a346d28
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-09b4612');
+var VERSION = new Version('5.2.5-a346d28');
 
 /**
  * @fileoverview added by tsickle
