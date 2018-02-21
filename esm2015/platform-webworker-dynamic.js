a9eee7da5e79d082ae138398460510e0dec93d71
Victor Berchet <victor@suumit.com>
2018-02-21 00:04:04 +0000

    d20a08b docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5bdb3ac
+ * @license Angular v5.2.5-d20a08b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-5bdb3ac');
+const VERSION = new Version('5.2.5-d20a08b');
 
 /**
  * @fileoverview added by tsickle
