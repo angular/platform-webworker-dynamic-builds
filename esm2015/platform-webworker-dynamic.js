2057ee130dbfd39086aba73516e50bf244237439
Victor Berchet <victor@suumit.com>
2018-02-22 00:21:41 +0000

    3e6a86f fix(forms): set state before emitting a value from ngModelChange (#21514)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-a7ebf5a
+ * @license Angular v6.0.0-beta.4-3e6a86f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-a7ebf5a');
+const VERSION = new Version('6.0.0-beta.4-3e6a86f');
 
 /**
  * @fileoverview added by tsickle
