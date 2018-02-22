bb4f9b938cf18c2377a0bf4cda22a6d4a062bd14
Victor Berchet <victor@suumit.com>
2018-02-22 22:21:32 +0000

    45eff4c fix(router): don't mutate route configs (#22358)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-b3ffeaa');
+const VERSION = new Version('6.0.0-beta.5-45eff4c');
 
 /**
  * @fileoverview added by tsickle
