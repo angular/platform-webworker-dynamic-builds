3c4f6071b02b0c73f4062407154c84232cd07ce9
Victor Berchet <victor@suumit.com>
2018-02-22 21:56:10 +0000

    f194d00 build: disable bazel-out symlink (#22375)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-b7544cc');
+var VERSION = new Version('6.0.0-beta.5-f194d00');
 
 /**
  * @fileoverview added by tsickle
