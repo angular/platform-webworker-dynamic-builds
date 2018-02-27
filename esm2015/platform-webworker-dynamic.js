18025dd5b7745bba97ae2ae5721140c442488396
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:55 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-de02a7a');
+const VERSION = new Version('5.2.6-d033106');
 
 /**
  * @fileoverview added by tsickle
