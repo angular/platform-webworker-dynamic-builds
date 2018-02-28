65b9454ececec2d28ef39fa064128023952dbd65
Alex Eagle <alexeagle@google.com>
2018-02-28 19:28:17 +0000

    c82cef8 docs: fix dynamic component loader example (#22181)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-7d65356
+ * @license Angular v6.0.0-beta.5-c82cef8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-7d65356');
+const VERSION = new Version('6.0.0-beta.5-c82cef8');
 
 /**
  * @fileoverview added by tsickle
