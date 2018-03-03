998a0f8b1affc3f54b25cf4edc3691db2a8d5072
Alex Eagle <alexeagle@google.com>
2018-03-03 17:47:50 +0000

    4f60968 test(bazel): Build and test ts-api-guardian locally (#22544)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-d7e5d45
+ * @license Angular v6.0.0-beta.6-4f60968
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-d7e5d45');
+var VERSION = new Version('6.0.0-beta.6-4f60968');
 
 /**
  * @fileoverview added by tsickle
