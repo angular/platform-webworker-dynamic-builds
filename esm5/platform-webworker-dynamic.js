54a5f49633886591b0c32b8cd0cbf7d7ef72cf71
Alex Eagle <alexeagle@google.com>
2018-03-06 05:48:18 +0000

    f86d8ae docs(compiler): fix a line about ivy library (#22579) PR Close #22579

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-94bc277');
+var VERSION = new Version('6.0.0-beta.6-f86d8ae');
 
 /**
  * @fileoverview added by tsickle
