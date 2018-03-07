87476578088cdd08ccd4a27fc1f8e34aed8fdda9
Alex Eagle <alexeagle@google.com>
2018-03-07 19:13:15 +0000

    4aeb04d docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-5876fb0');
+var VERSION = new Version('5.2.7-4aeb04d');
 
 /**
  * @fileoverview added by tsickle
