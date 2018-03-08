7f99776fb690c25181e44ef145959be5584d56ff
Kara Erickson <karakara@google.com>
2018-03-08 20:32:09 +0000

    2fee5cc test(ivy): add injection canonical specs (#22595)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f13f4db
+ * @license Angular v6.0.0-beta.7-2fee5cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-f13f4db');
+var VERSION = new Version('6.0.0-beta.7-2fee5cc');
 
 /**
  * @fileoverview added by tsickle
