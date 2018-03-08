75db9cb32c78f86c041df281a7f6a81a420cc63f
Kara Erickson <karakara@google.com>
2018-03-08 05:17:40 +0000

    5d4fa7f test(ivy): add canonical examples of bindings on elements (#22403)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8fb34bc
+ * @license Angular v6.0.0-beta.7-5d4fa7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-8fb34bc');
+var VERSION = new Version('6.0.0-beta.7-5d4fa7f');
 
 /**
  * @fileoverview added by tsickle
