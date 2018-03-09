3e1e57abb1d1b6ba2e7e07efccd9573f39b51c7c
Kara Erickson <karakara@google.com>
2018-03-09 01:06:50 +0000

    5412e10 docs(core): replace ancient live demos (#22665)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-489fec1');
+const VERSION = new Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
