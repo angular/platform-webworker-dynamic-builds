253811c9c47e17da009b105e73a5a7eec06a1d7a
Kara Erickson <karakara@google.com>
2018-03-12 05:41:10 +0000

    40315be fix(compiler-cli): enableResourceInlining handles both styles and styleUrls (#22688)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-fa451bc');
+var VERSION = new Version('6.0.0-beta.7-40315be');
 
 /**
  * @fileoverview added by tsickle
