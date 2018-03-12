3ab7d932609db1ac209052519bbb4a42a446fd81
Kara Erickson <karakara@google.com>
2018-03-12 05:41:20 +0000

    aa7dba2 feat(ivy): support checkNoChanges (#22710)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-40315be
+ * @license Angular v6.0.0-beta.7-aa7dba2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-40315be');
+const VERSION = new Version('6.0.0-beta.7-aa7dba2');
 
 /**
  * @fileoverview added by tsickle
