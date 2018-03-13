bc1a8149a71e6ebc4c8367037c944cb92d91b996
Kara Erickson <karakara@google.com>
2018-03-13 17:06:32 +0000

    58b94e6 feat(animations): expose `element` and `params` within transition matchers (#22693)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-db56836
+ * @license Angular v6.0.0-beta.7-58b94e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-db56836');
+const VERSION = new Version('6.0.0-beta.7-58b94e6');
 
 /**
  * @fileoverview added by tsickle
