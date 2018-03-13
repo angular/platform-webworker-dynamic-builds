8713084e9e52cf5d382174d8d045daa0ef8bf6f4
Kara Erickson <karakara@google.com>
2018-03-13 20:50:22 +0000

    bd9d4df refactor(ivy): remove inputsPropertyName (#22716)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-34e355a
+ * @license Angular v6.0.0-beta.7-bd9d4df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-34e355a');
+const VERSION = new Version('6.0.0-beta.7-bd9d4df');
 
 /**
  * @fileoverview added by tsickle
