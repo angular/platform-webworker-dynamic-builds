ac669330415ca8022a40090fd46d0735c16a70c6
Miško Hevery <misko@hevery.com>
2018-03-15 19:03:45 +0000

    19e6b8d build(aio): fix `addNotYetDocumentedProperty` processor (#22770)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f256c02
+ * @license Angular v6.0.0-beta.7-19e6b8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-f256c02');
+var VERSION = new Version('6.0.0-beta.7-19e6b8d');
 
 /**
  * @fileoverview added by tsickle
