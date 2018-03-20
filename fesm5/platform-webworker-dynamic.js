c1d2f38e38b4717e1c83559bb5da49fd4c24f2bb
Igor Minar <igor@angular.io>
2018-03-20 20:55:41 +0000

    689f351 build: expose flatModuleOutFile option on ng_module (#22814)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-67f570c
+ * @license Angular v6.0.0-beta.7-689f351
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-67f570c');
+var VERSION = new Version('6.0.0-beta.7-689f351');
 
 /**
  * @fileoverview added by tsickle
