d94144f70f6df9e796ec1418f3e1f71d6b4291d2
Igor Minar <igor@angular.io>
2018-03-20 21:02:29 +0000

    e6c731f fix(router): don't use spread operator to workaround an issue in closure compiler (#22884)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-7d095b9
+ * @license Angular v6.0.0-beta.7-e6c731f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-7d095b9');
+const VERSION = new Version('6.0.0-beta.7-e6c731f');
 
 /**
  * @fileoverview added by tsickle
