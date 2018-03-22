54cfdfdbc7476c2090d17d8ef441b7bc1d54e234
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:23:53 +0000

    641cc49 fix(animations): avoid animation insertions during router back/refresh (#21977)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a846abb
+ * @license Angular v5.2.9-641cc49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-a846abb');
+const VERSION = new Version('5.2.9-641cc49');
 
 /**
  * @fileoverview added by tsickle
