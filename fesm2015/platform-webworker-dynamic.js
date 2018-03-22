22bb6ea2ef24c15e9089f110383221953c4fddf5
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:25:18 +0000

    99711b1 test(core): hello world i18n demo (#22654)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-d77bb46
+ * @license Angular v6.0.0-rc.0-99711b1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-d77bb46');
+const VERSION = new Version('6.0.0-rc.0-99711b1');
 
 /**
  * @fileoverview added by tsickle
