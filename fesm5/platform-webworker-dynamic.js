ec68d3ca95a4b87249a53cc209277b8bc85fa00b
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:11:52 +0000

    8ca26a9 ci(compiler): run compiler tests in bazel (#22900)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3498106
+ * @license Angular v6.0.0-rc.0-8ca26a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-3498106');
+var VERSION = new Version('6.0.0-rc.0-8ca26a9');
 
 /**
  * @fileoverview added by tsickle
