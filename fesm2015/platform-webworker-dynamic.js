c4262f863d7e9944e48d0444fa31b30ab050bc3e
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 21:54:15 +0000

    ed53c5c refactor(language-service): Remove angularOnlyResults (#22636)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-d28ce50
+ * @license Angular v6.0.0-rc.0-ed53c5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-d28ce50');
+const VERSION = new Version('6.0.0-rc.0-ed53c5c');
 
 /**
  * @fileoverview added by tsickle
