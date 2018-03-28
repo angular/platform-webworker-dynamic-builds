b6693f81a6f9c48071ac17dd33b8e5951262e2fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:42:54 +0000

    0049743 fix(bazel): don't inline tslib into fesms (#23044)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-4e004f3
+ * @license Angular v6.0.0-rc.0-0049743
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-4e004f3');
+var VERSION = new Version('6.0.0-rc.0-0049743');
 
 /**
  * @fileoverview added by tsickle
