ff79dbb16b397020c37025ebe3d3a724fb0a47a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 15:23:51 +0000

    e1ea7ed docs: update examples for tree-shakeable providers (#22961)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-ed53c5c
+ * @license Angular v6.0.0-rc.0-e1ea7ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-ed53c5c');
+var VERSION = new Version('6.0.0-rc.0-e1ea7ed');
 
 /**
  * @fileoverview added by tsickle
