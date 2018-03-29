39c1e52556e45d7938aaba580c0828287b96177f
Igor Minar <igor@angular.io>
2018-03-29 21:36:35 +0000

    0d9140c fix(bazel): ng_package should include private exports in fesms (#23054)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-9fb08e2
+ * @license Angular v6.0.0-rc.0-0d9140c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-9fb08e2');
+var VERSION = new Version('6.0.0-rc.0-0d9140c');
 
 /**
  * @fileoverview added by tsickle
