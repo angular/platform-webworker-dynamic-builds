214c9c1236474976b815ecaf81f2deaf535bbf91
Igor Minar <igor@angular.io>
2018-03-30 02:18:11 +0000

    481b22e fix(bazel): downlevel decorators in fesm5 files (#23078)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-45e090b
+ * @license Angular v6.0.0-rc.0-481b22e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-45e090b');
+var VERSION = new Version('6.0.0-rc.0-481b22e');
 
 /**
  * @fileoverview added by tsickle
