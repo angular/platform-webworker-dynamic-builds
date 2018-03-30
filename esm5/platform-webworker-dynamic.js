9a0c083fe62a611af8b4c514eb5e358deb493643
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:03:54 +0000

    4473da7 ci(aio): add monitoring for angular.io (#23093)

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-4f7c369
+ * @license Angular v5.2.9-4473da7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-4f7c369');
+var VERSION = new Version('5.2.9-4473da7');
 
 /**
  * @fileoverview added by tsickle
