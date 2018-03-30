e54f814bca8d37b78cc2ea251dc083c5d53ef3e4
Igor Minar <igor@angular.io>
2018-03-30 03:59:25 +0000

    439030f fix(bazel): complete the rollup globals list for all angular entrypoints (#23080)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-481b22e
+ * @license Angular v6.0.0-rc.0-439030f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-481b22e');
+var VERSION = new Version('6.0.0-rc.0-439030f');
 
 /**
  * @fileoverview added by tsickle
