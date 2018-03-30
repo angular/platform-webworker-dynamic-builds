662a70672aa61d5cbbd25870d645ad5944404c5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:15:06 +0000

    9cd4465 refactor(compiler): generate flat css selectors (#23074)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-6cb1adf
+ * @license Angular v6.0.0-rc.1-9cd4465
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.1-6cb1adf');
+const VERSION = new Version('6.0.0-rc.1-9cd4465');
 
 /**
  * @fileoverview added by tsickle
