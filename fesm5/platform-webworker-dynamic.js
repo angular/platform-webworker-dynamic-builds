8d1e6c0a0adfc3ab011a94e8f3b34db21dff6816
Igor Minar <igor@angular.io>
2018-03-30 01:08:14 +0000

    45e090b fix(bazel): pass --global option to rollup (#23073)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-0d9140c
+ * @license Angular v6.0.0-rc.0-45e090b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-0d9140c');
+var VERSION = new Version('6.0.0-rc.0-45e090b');
 
 /**
  * @fileoverview added by tsickle
