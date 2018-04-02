e2b79240f19cc76e4b1c42e9732808f93d24a7f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:47:32 +0000

    a2330ff fix(upgrade): propagate return value of resumeBootstrap (#22754)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ff34d5e
+ * @license Angular v6.0.0-rc.1-a2330ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-ff34d5e');
+var VERSION = new Version('6.0.0-rc.1-a2330ff');
 
 /**
  * @fileoverview added by tsickle
