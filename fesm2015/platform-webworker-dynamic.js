4e51132e23b6b327b9ca8b9aed8ff927a057d1ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 18:13:16 +0000

    580f05b build: flatten esm5 sources before rollup (#23131)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-92724b3
+ * @license Angular v6.0.0-rc.1-580f05b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.1-92724b3');
+const VERSION = new Version('6.0.0-rc.1-580f05b');
 
 /**
  * @fileoverview added by tsickle
