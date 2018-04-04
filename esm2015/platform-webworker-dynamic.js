1f3c8802ceafbd0f9b5b6bc5ac9f7adc3520bfa7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:15:58 +0000

    3900c36 refactor(common): simplify NgClass code, add comments (#21937)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-9703079
+ * @license Angular v5.2.9-3900c36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-9703079');
+const VERSION = new Version('5.2.9-3900c36');
 
 /**
  * @fileoverview added by tsickle
