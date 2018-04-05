df06a73ceb35c2392316399d8da8918b6924c6c6
Igor Minar <igor@angular.io>
2018-04-05 01:09:55 +0000

    7c03961 test: update hello_world__closure to google-closure-compiler@20180319.0.0 (#23149)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-6699fb5
+ * @license Angular v6.0.0-rc.1-7c03961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-6699fb5');
+var VERSION = new Version('6.0.0-rc.1-7c03961');
 
 /**
  * @fileoverview added by tsickle
