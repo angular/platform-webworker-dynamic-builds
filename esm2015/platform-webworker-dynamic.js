32667c91c22cc5991f389d01cab33edc46a8e654
Igor Minar <igor@angular.io>
2018-04-05 17:55:56 +0000

    73172dd ci(aio): upload the preview before checking the bundle sizes (#23123)

--- esm2015/platform-webworker-dynamic.js
+++ esm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a50ce65
+ * @license Angular v5.2.9-73172dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-a50ce65');
+const VERSION = new Version('5.2.9-73172dd');
 
 /**
  * @fileoverview added by tsickle
