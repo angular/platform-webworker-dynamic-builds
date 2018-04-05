410dc60d8df73a9a0b63eaa767e031b3e6537b6f
Igor Minar <igor@angular.io>
2018-04-05 23:54:18 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-629629d');
+var VERSION = new Version('6.0.0-rc.2-b5f41f2');
 
 /**
  * @fileoverview added by tsickle
