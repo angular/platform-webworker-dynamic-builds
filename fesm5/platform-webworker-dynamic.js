528d9e04cbda44eb198e70edb44195aab946ab07
Igor Minar <igor@angular.io>
2018-04-05 22:27:14 +0000

    628303d fix(ivy): instantiate dirs in correct order (#23178)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-d80e930
+ * @license Angular v6.0.0-rc.2-628303d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-d80e930');
+var VERSION = new Version('6.0.0-rc.2-628303d');
 
 /**
  * @fileoverview added by tsickle
