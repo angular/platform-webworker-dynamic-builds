1e3215b8af93a26f32b45705ccc1884cf68130e1
Igor Minar <igor@angular.io>
2018-04-06 18:53:06 +0000

    3a9d916 test(ivy): remove outdated render3 tests (#23229)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-eb9968a
+ * @license Angular v6.0.0-rc.2-3a9d916
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-eb9968a');
+var VERSION = new Version('6.0.0-rc.2-3a9d916');
 
 /**
  * @fileoverview added by tsickle
