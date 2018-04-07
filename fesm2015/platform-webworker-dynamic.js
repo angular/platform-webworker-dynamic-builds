eceb1822d854cebbcc5263490dcfd33264df3e75
Igor Minar <igor@angular.io>
2018-04-07 05:17:11 +0000

    1fac5f4 docs: recommend best practice for providers (#22934)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-33f8120
+ * @license Angular v6.0.0-rc.3-1fac5f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.3-33f8120');
+const VERSION = new Version('6.0.0-rc.3-1fac5f4');
 
 /**
  * @fileoverview added by tsickle
