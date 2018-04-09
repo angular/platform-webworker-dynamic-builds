23b0f6b41fee447d6927cb1bef69df20e5d592a5
Igor Minar <igor@angular.io>
2018-04-09 23:37:49 +0000

    bb3f0e5 feat(ivy): support projection of ViewContainerRef (#23272)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37c1634
+ * @license Angular v6.0.0-rc.3-bb3f0e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-37c1634');
+var VERSION = new Version('6.0.0-rc.3-bb3f0e5');
 
 /**
  * @fileoverview added by tsickle
