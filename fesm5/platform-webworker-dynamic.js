a48bddab6025140e8852f917a6aae8c32850a82a
Igor Minar <igor@angular.io>
2018-04-10 20:37:46 +0000

    10ecdb1 ci: do bazel build and test in parallel (#23290)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-bb3f0e5
+ * @license Angular v6.0.0-rc.3-10ecdb1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-bb3f0e5');
+var VERSION = new Version('6.0.0-rc.3-10ecdb1');
 
 /**
  * @fileoverview added by tsickle
