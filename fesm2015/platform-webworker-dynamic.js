369af9d854fa86de0ba123ba8c6ccd2d9493e98e
Igor Minar <igor@angular.io>
2018-04-11 06:23:20 +0000

    b3a10e0 build: update Bazel dependency to 0.11.1 (#23297)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.3-0cb4f12');
+const VERSION = new Version('6.0.0-rc.3-b3a10e0');
 
 /**
  * @fileoverview added by tsickle
