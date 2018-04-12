01056d3ed11002a09485babd656e2219c484912a
Igor Minar <igor@angular.io>
2018-04-12 07:45:53 +0000

    1fc72e5 build(core): update zone.js peer dep to ~0.8.26

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-bbfa1d3
+ * @license Angular v6.0.0-rc.3-1fc72e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-bbfa1d3');
+var VERSION = new Version('6.0.0-rc.3-1fc72e5');
 
 /**
  * @fileoverview added by tsickle
