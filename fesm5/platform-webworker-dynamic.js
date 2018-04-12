634bf799f18827980242adf35d725eeb485eb9b6
Igor Minar <igor@angular.io>
2018-04-12 08:46:51 +0000

    01975ff docs: add changelog for 6.0.0-rc.4

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fc72e5
+ * @license Angular v6.0.0-rc.4-01975ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-1fc72e5');
+var VERSION = new Version('6.0.0-rc.4-01975ff');
 
 /**
  * @fileoverview added by tsickle
