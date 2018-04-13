0fabe4bfe265f8e393027c8342972bad246049cd
Igor Minar <igor@angular.io>
2018-04-13 08:08:48 +0000

    c2a53bb fix(aio): remove additional 'googlebot' reference (#23249)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-3bd682f
+ * @license Angular v6.0.0-rc.4-c2a53bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-3bd682f');
+const VERSION = new Version('6.0.0-rc.4-c2a53bb');
 
 /**
  * @fileoverview added by tsickle
