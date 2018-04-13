722213fb4bfe5417decb4af0da1f2e40b2df52db
Igor Minar <igor@angular.io>
2018-04-13 07:40:30 +0000

    2e270bb build: include tslib in umd bundles (#23354)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-a025f7e
+ * @license Angular v6.0.0-rc.4-2e270bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-a025f7e');
+const VERSION = new Version('6.0.0-rc.4-2e270bb');
 
 /**
  * @fileoverview added by tsickle
