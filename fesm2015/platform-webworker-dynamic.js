0d125cccbd3ca6702348d2b69a447930fe4d5741
Igor Minar <igor@angular.io>
2018-04-13 07:44:13 +0000

    6199ea5 fix(compiler-cli): shorten resolved module name in fileNameToModuleName to npm package name for typings (#23231)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-2e270bb
+ * @license Angular v6.0.0-rc.4-6199ea5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-2e270bb');
+const VERSION = new Version('6.0.0-rc.4-6199ea5');
 
 /**
  * @fileoverview added by tsickle
