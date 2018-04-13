bf1025d021b669c1a4f1cf8898afa28a8ea736f8
Igor Minar <igor@angular.io>
2018-04-13 07:48:27 +0000

    2bb7838 fix(ivy): support ViewContainerRef on nodes projected into an embedded view (#23333)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-6199ea5
+ * @license Angular v6.0.0-rc.4-2bb7838
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-6199ea5');
+const VERSION = new Version('6.0.0-rc.4-2bb7838');
 
 /**
  * @fileoverview added by tsickle
