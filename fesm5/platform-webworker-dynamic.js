1aa1a49d20fbaaa4253d74f97410905ac2ba7f31
Igor Minar <igor@angular.io>
2018-04-13 20:36:26 +0000

    639d52f refactor: ensure all 'TODO's are consistent (#23252)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-aa27155
+ * @license Angular v6.0.0-rc.4-639d52f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-aa27155');
+var VERSION = new Version('6.0.0-rc.4-639d52f');
 
 /**
  * @fileoverview added by tsickle
