29135ed209dd70120a2ff8f0b870223291c66f1d
Igor Minar <igor@angular.io>
2018-04-13 20:52:49 +0000

    6e73300 refactor(ivy): cleanup directives & pipes import (#23369)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-6c2c958
+ * @license Angular v6.0.0-rc.4-6e73300
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-6c2c958');
+var VERSION = new Version('6.0.0-rc.4-6e73300');
 
 /**
  * @fileoverview added by tsickle
