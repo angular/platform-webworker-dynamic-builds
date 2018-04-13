f739adc0389f4c259e6eeda7e29ef4c90b70ff99
Igor Minar <igor@angular.io>
2018-04-13 23:53:23 +0000

    d1177c7 refactor: update CLDR data for closure locale & use a const for undefined (#23372) PR Close #23372

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-12f90ef
+ * @license Angular v6.0.0-rc.4-d1177c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-12f90ef');
+var VERSION = new Version('6.0.0-rc.4-d1177c7');
 
 /**
  * @fileoverview added by tsickle
