2dd348cfbd5cf0d2fcef4dadd9bab5824bfefa8c
Igor Minar <igor@angular.io>
2018-04-16 08:11:08 +0000

    d979230 docs: release notes for the 5.2.10 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-896811d
+ * @license Angular v6.0.0-rc.5-d979230
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-896811d');
+const VERSION = new Version('6.0.0-rc.5-d979230');
 
 /**
  * @fileoverview added by tsickle
