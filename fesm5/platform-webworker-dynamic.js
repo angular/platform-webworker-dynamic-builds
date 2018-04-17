84012167363c1170e478e8d4f7e9127b5d6eb9c8
Jason Aden <jasonaden@google.com>
2018-04-17 21:42:25 +0000

    41b5149 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-06f8656
+ * @license Angular v6.0.0-rc.5-41b5149
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-06f8656');
+var VERSION = new Version('6.0.0-rc.5-41b5149');
 
 /**
  * @fileoverview added by tsickle
