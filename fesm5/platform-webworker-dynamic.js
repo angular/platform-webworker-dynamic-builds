8ca5c04190de6bba7e29e977f65198b1f4de9ed4
Victor Berchet <victor@suumit.com>
2018-04-18 22:13:47 +0000

    acf6781 test(core): add a symbols test for renderer2 code (#23436)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-fd48e53
+ * @license Angular v6.0.0-rc.5-acf6781
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-fd48e53');
+var VERSION = new Version('6.0.0-rc.5-acf6781');
 
 /**
  * @fileoverview added by tsickle
