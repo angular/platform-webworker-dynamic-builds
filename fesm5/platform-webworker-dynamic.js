dda6fdd8ef59e1d7c0d8f1ecb529580672de0cdb
Victor Berchet <victor@suumit.com>
2018-04-19 23:56:23 +0000

    9757347 feat(ivy): add an IVY local the compiler which avoids analyzeModule (#23441)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-1d1e75e
+ * @license Angular v6.0.0-rc.5-9757347
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-1d1e75e');
+var VERSION = new Version('6.0.0-rc.5-9757347');
 
 /**
  * @fileoverview added by tsickle
