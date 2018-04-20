add6c3a6cecdd26e7759fc4a47b2c69dffce2e06
Victor Berchet <victor@suumit.com>
2018-04-20 02:23:44 +0000

    ca776c5 fix(compiler): handle undefined annotation metadata (#23349)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-f2563ca
+ * @license Angular v6.0.0-rc.5-ca776c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-f2563ca');
+var VERSION = new Version('6.0.0-rc.5-ca776c5');
 
 /**
  * @fileoverview added by tsickle
