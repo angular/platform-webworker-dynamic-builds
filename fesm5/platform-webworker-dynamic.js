1b4d90161cee52d40cf1c713abcfb79749b20a0a
Victor Berchet <victor@suumit.com>
2018-04-20 23:34:27 +0000

    4662878 refactor(compiler): refactor template binding parsing (#23460)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-ca776c5
+ * @license Angular v6.0.0-rc.5-4662878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-ca776c5');
+var VERSION = new Version('6.0.0-rc.5-4662878');
 
 /**
  * @fileoverview added by tsickle
