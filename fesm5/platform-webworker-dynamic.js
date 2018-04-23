92683ff211626645016ad06455b139378d0f3c94
Victor Berchet <victor@suumit.com>
2018-04-23 21:07:15 +0000

    60e5507 docs(aio): Add UpgradingAngularJS to education resources (#23169)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4cfa571
+ * @license Angular v6.0.0-rc.5-60e5507
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-4cfa571');
+var VERSION = new Version('6.0.0-rc.5-60e5507');
 
 /**
  * @fileoverview added by tsickle
