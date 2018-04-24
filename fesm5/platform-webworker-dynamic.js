f97ad4d83cfc61e5a628aea7fcdff043fe560c63
Victor Berchet <victor@suumit.com>
2018-04-24 22:01:49 +0000

    1b26dd8 docs(benchpress): fix typo in README (#23471) (#23488)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-f721b06
+ * @license Angular v6.0.0-rc.5-1b26dd8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-f721b06');
+var VERSION = new Version('6.0.0-rc.5-1b26dd8');
 
 /**
  * @fileoverview added by tsickle
