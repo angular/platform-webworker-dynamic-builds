db69e29bee2bf677a241290faa24af67b7094074
Igor Minar <igor@angular.io>
2018-04-25 20:46:42 +0000

    64bf6ed docs: update glossary architectural terms (#23045)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-adf6235
+ * @license Angular v6.0.0-rc.5-64bf6ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-adf6235');
+const VERSION = new Version('6.0.0-rc.5-64bf6ed');
 
 /**
  * @fileoverview added by tsickle
