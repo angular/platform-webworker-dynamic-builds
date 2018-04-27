71e312fd3e5292220a97ce0e397dbd9d2e88a1ba
Igor Minar <igor@angular.io>
2018-04-27 15:27:52 +0000

    693a7ba test: add i18n to cli-hello-world integration test (#23527)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-91bf598
+ * @license Angular v6.0.0-rc.5-693a7ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-91bf598');
+var VERSION = new Version('6.0.0-rc.5-693a7ba');
 
 /**
  * @fileoverview added by tsickle
