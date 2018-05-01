96e2c740685d6bc8adb45d597153fbb931ca2cca
Igor Minar <igor@angular.io>
2018-05-01 17:54:35 +0000

    31a435e docs: fix typo in tag name (`my-child` --> `app-child`) (#23606)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-3e92b22
+ * @license Angular v6.0.0-rc.6-31a435e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.6-3e92b22');
+const VERSION = new Version('6.0.0-rc.6-31a435e');
 
 /**
  * @fileoverview added by tsickle
