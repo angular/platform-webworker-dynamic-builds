56be83ed36f2870c628f953e8d458c6788eb5f49
Igor Minar <igor@angular.io>
2018-05-01 18:04:48 +0000

    f11daa2 docs(aio): update Egghead.io URL (#23598)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-31a435e
+ * @license Angular v6.0.0-rc.6-f11daa2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.6-31a435e');
+const VERSION = new Version('6.0.0-rc.6-f11daa2');
 
 /**
  * @fileoverview added by tsickle
