a567ba0386bc7d945d254b2ee1c1af1ec796a9b1
Igor Minar <igor@angular.io>
2018-05-02 23:16:53 +0000

    91d4da0 docs: add missing link to bootstrapping section (#23214)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-22eb8e2
+ * @license Angular v6.0.0-rc.6-91d4da0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.6-22eb8e2');
+var VERSION = new Version('6.0.0-rc.6-91d4da0');
 
 /**
  * @fileoverview added by tsickle
