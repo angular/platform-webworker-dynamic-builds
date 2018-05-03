3da78aff5cc22132f15e53e85414f1418677b4c5
Igor Minar <igor@angular.io>
2018-05-03 20:54:42 +0000

    5bd8c68 docs: improve the GitHub README.md, update links, etc

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-d0ccf5f
+ * @license Angular v6.0.0-5bd8c68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-d0ccf5f');
+const VERSION = new Version('6.0.0-5bd8c68');
 
 /**
  * @fileoverview added by tsickle
