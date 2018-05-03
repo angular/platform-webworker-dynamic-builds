f2d9bac03ac1fd4192012607b9c46025eab3aa37
Igor Minar <igor@angular.io>
2018-05-03 22:30:31 +0000

    a190c45 fix(aio): correct project id for deployment of archive sites

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-9027818
+ * @license Angular v6.0.0-a190c45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-9027818');
+var VERSION = new Version('6.0.0-a190c45');
 
 /**
  * @fileoverview added by tsickle
