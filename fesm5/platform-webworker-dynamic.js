ab5007e07d8579805dbfc7dc00968119c7e5ded0
Igor Minar <igor@angular.io>
2018-05-04 23:01:20 +0000

    07f2098 feat(aio): add v6 release notification (#23690)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-9b53a6e
+ * @license Angular v6.0.0-07f2098
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-var VERSION = new Version('6.0.0-9b53a6e');
+var VERSION = new Version('6.0.0-07f2098');
 
 /**
  * @fileoverview added by tsickle
