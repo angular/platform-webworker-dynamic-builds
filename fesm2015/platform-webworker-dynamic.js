4f5bb27c992a69963e2110614107cdbdb7e1b1b1
Igor Minar <igor@angular.io>
2018-05-04 23:21:29 +0000

    373a47d test: fix firebase deployment script test

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-83f12f3
+ * @license Angular v6.0.0-373a47d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  *
  */
-const VERSION = new Version('6.0.0-83f12f3');
+const VERSION = new Version('6.0.0-373a47d');
 
 /**
  * @fileoverview added by tsickle
