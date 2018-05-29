07867d77814849bdd0e21852488582c521320858
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:21:05 +0000

    7698afe docs(http): correct spelling error (#23675)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+7.sha-84b43da
+ * @license Angular v6.0.3+10.sha-7698afe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.3+7.sha-84b43da');
+const VERSION = new Version('6.0.3+10.sha-7698afe');
 
 /**
  * @fileoverview added by tsickle
