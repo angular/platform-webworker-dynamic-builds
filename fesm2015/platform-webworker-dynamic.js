875aeb1394a74313339b27f5ae3e327e52321140
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:44:38 +0000

    3fd3c2a test(animations): fix Node.js detection in animation tests (#24139)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+243.sha-1eafd04
+ * @license Angular v6.0.0-rc.5+244.sha-3fd3c2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+243.sha-1eafd04');
+const VERSION = new Version('6.0.0-rc.5+244.sha-3fd3c2a');
 
 /**
  * @fileoverview added by tsickle
