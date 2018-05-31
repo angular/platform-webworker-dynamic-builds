8ada3babd8897be32b35f4fd725b8107db2426a2
Victor Berchet <victor@suumit.com>
2018-05-31 17:34:35 +0000

    ec57133 build: update to rules_nodejs 0.9.1 and rules_typescript 0.15.0 (#24212)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+285.sha-3647cb7
+ * @license Angular v6.0.0-rc.5+286.sha-ec57133
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+285.sha-3647cb7');
+const VERSION = new Version('6.0.0-rc.5+286.sha-ec57133');
 
 /**
  * @fileoverview added by tsickle
