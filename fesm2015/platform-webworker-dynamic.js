c155dc97427600d3137e5bd00b46ba2be6f46b97
Victor Berchet <victor@suumit.com>
2018-05-31 00:56:48 +0000

    a10bdef docs(aio): Add GDE Kim Maida to contributors

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+43.sha-2f377db
+ * @license Angular v6.0.3+44.sha-a10bdef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.3+43.sha-2f377db');
+const VERSION = new Version('6.0.3+44.sha-a10bdef');
 
 /**
  * @fileoverview added by tsickle
