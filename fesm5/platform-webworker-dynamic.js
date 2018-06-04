8518d89cde958a5508ff32d32768cb727842e931
Victor Berchet <victor@suumit.com>
2018-06-04 04:11:34 +0000

    5794506 refactor(ivy): move id to TView (#24264)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+300.sha-5db4f1a
+ * @license Angular v6.0.0-rc.5+303.sha-5794506
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+300.sha-5db4f1a');
+var VERSION = new Version('6.0.0-rc.5+303.sha-5794506');
 
 /**
  * @license
