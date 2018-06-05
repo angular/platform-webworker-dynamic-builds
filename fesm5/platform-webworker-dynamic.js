bf67df33ea0f0e4ac266c08825632c20bb1fa059
Victor Berchet <victor@suumit.com>
2018-06-05 21:00:10 +0000

    b750919 feat(ivy): implement ViewContainerRef.remove (#24221)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+311.sha-9c40375
+ * @license Angular v6.0.0-rc.5+312.sha-b750919
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+311.sha-9c40375');
+var VERSION = new Version('6.0.0-rc.5+312.sha-b750919');
 
 /**
  * @license
