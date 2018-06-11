bd273791ff0948170a5327e7578c598fafdc7f81
Miško Hevery <misko@hevery.com>
2018-06-11 16:50:57 +0000

    5e8bf2f build(docs-infra): ensure `dist/` directory is cleaned before running `tsc --watch` (#24372)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+const VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @fileoverview added by tsickle
