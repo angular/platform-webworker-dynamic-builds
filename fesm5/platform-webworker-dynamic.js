61ce6c63bad7c5eb5c323279fda5bb9bb9cc59a0
Miško Hevery <misko@hevery.com>
2018-06-15 17:18:38 +0000

    aa66e84 docs: push animation api doc changes back to stable (#24525)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+26.sha-e1bc3f5
+ * @license Angular v6.0.5+27.sha-aa66e84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.5+26.sha-e1bc3f5');
+var VERSION = new Version('6.0.5+27.sha-aa66e84');
 
 /**
  * @license
