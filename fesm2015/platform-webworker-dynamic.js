bca53e340e35632c9b82e623665cc89824c67a75
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:56 +0000

    3649958 build(docs-infra): move overload short description above syntax (#24526)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+5.sha-8919577
+ * @license Angular v6.0.6+6.sha-3649958
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.6+5.sha-8919577');
+const VERSION = new Version('6.0.6+6.sha-3649958');
 
 /**
  * @fileoverview added by tsickle
