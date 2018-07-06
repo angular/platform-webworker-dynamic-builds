7f594dc43805550052840bb282b47896a90bf66a
Miško Hevery <misko@hevery.com>
2018-07-06 18:17:37 +0000

    25b532e docs: clarify faqs about services (#24079)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+19.sha-b51ce62
+ * @license Angular v6.0.7+18.sha-25b532e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+19.sha-b51ce62');
+var VERSION = new Version('6.0.7+18.sha-25b532e');
 
 /**
  * @license
