975dbb02702f7f2037a77c5e891450d7b467b7e9
Miško Hevery <misko@hevery.com>
2018-07-06 18:13:56 +0000

    69c8226 docs: add app.module to changed documents (#23876)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+15.sha-ada486a
+ * @license Angular v6.0.7+17.sha-69c8226
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+15.sha-ada486a');
+var VERSION = new Version('6.0.7+17.sha-69c8226');
 
 /**
  * @license
