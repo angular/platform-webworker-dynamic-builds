24f3100402c06f30e65441cd61042385ace4cd15
Igor Minar <igor@angular.io>
2018-07-31 00:43:31 +0000

    e99d860 feat(compiler): add "original" placeholder value on extracted XMB (#25079)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+51.sha-24789e9
+ * @license Angular v6.1.0+52.sha-e99d860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+51.sha-24789e9');
+const VERSION = new Version('6.1.0+52.sha-e99d860');
 
 /**
  * @license
