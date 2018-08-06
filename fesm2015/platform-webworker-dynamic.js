11b194e0a26d2082d53e74f65a04de8b8cc01780
Kara Erickson <karakara@google.com>
2018-08-06 22:24:37 +0000

    88f01f5 docs: fix typo (#25331) PR Close #25331

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+10.sha-c66794c
+ * @license Angular v6.1.1+11.sha-88f01f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.1+10.sha-c66794c');
+const VERSION = new Version('6.1.1+11.sha-88f01f5');
 
 /**
  * @license
