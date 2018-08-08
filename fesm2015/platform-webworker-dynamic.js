c289baf2c1a6e89ae138a6d9334dc8c820ffa8f3
Kara <kara@users.noreply.github.com>
2018-08-08 22:11:10 +0000

    2951e72 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25365)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+5.sha-6480d1b
+ * @license Angular v6.1.2+7.sha-2951e72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.2+5.sha-6480d1b');
+const VERSION = new Version('6.1.2+7.sha-2951e72');
 
 /**
  * @fileoverview added by tsickle
