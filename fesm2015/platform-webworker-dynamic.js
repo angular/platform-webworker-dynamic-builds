04cef2d0633d21f4092cf4807be1138d5b8ea77a
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 03:05:52 +0000

    cc0d0a9 fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+1.sha-82f26fe
+ * @license Angular v6.1.4+2.sha-cc0d0a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.4+1.sha-82f26fe');
+const VERSION = new Version('6.1.4+2.sha-cc0d0a9');
 
 /**
  * @fileoverview added by tsickle
