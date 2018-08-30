4239966254c4c41c7c1bbb5a4044734c95a2955d
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 19:00:59 +0000

    2e09115 docs: edit and organize di guide (#21915)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6
+ * @license Angular v6.1.6+1.sha-2e09115
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6');
+const VERSION = new Version('6.1.6+1.sha-2e09115');
 
 /**
  * @fileoverview added by tsickle
