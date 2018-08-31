e254fa1c0e5983548a6a5cfb2786e7384b53bac6
Miško Hevery <misko@hevery.com>
2018-08-31 17:23:34 +0000

    a065dc2 fix(compiler): remove bogus lines

--- esm5/platform-webworker-dynamic.js
+++ esm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-bc27d4a
+ * @license Angular v5.2.11-a065dc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-bc27d4a');
+var VERSION = new Version('5.2.11-a065dc2');
 
 /**
  * @fileoverview added by tsickle
