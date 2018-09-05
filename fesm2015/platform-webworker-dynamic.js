14dfed6b315717631df0b3eca37fd1dd8420f302
Misko Hevery <misko@hevery.com>
2018-09-05 20:35:06 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+22.sha-82e0676');
+const VERSION = new Version('6.1.6+24.sha-e72f741');
 
 /**
  * @fileoverview added by tsickle
