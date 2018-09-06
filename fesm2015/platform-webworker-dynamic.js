6ca540966014e73f78e1a5344b1298d4340b17e5
Misko Hevery <misko@hevery.com>
2018-09-06 06:06:04 +0000

    268e977 release: cut the v7.0.0-beta.5 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+59.sha-dcad054
+ * @license Angular v7.0.0-beta.5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+59.sha-dcad054');
+const VERSION = new Version('7.0.0-beta.5');
 
 /**
  * @fileoverview added by tsickle
