5644b2bf11a3fa84591dbdcca657347ae6d53cb4
Igor Minar <igor@angular.io>
2018-09-12 00:42:12 +0000

    a31cfc5 fix(compiler): Fix look up of entryComponents in AOT Summaries (#24892)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+13.sha-8d7fbb6
+ * @license Angular v6.1.7+12.sha-a31cfc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+13.sha-8d7fbb6');
+const VERSION = new Version('6.1.7+12.sha-a31cfc5');
 
 /**
  * @fileoverview added by tsickle
