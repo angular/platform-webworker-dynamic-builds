e04375c1856623723c37452f70d8d5bd90e935ad
Kara Erickson <karakara@google.com>
2018-09-25 19:13:36 +0000

    42e2e7c build: remove obsolete comment in `env.sh` (#25819)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.8+39.sha-adad170');
+const VERSION = new Version('6.1.8+40.sha-42e2e7c');
 
 /**
  * @license
