faa53cd703fa283cd5a5bf293ef341ee2d6e1a31
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 21:22:17 +0000

    66c2d08 ci(docs-infra): run the  script in the correct folder

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+36.sha-c1bf82a
+ * @license Angular v6.1.9+37.sha-66c2d08
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.9+36.sha-c1bf82a');
+var VERSION = new Version('6.1.9+37.sha-66c2d08');
 
 /**
  * @license
