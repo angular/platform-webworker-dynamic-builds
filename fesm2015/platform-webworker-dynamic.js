fca4101c91b42896822d5e27a6c65640beb2386a
Miško Hevery <misko@hevery.com>
2018-10-16 00:19:12 +0000

    1c9b065 fix(router): fix regression where navigateByUrl promise didn't resolve on CanLoad failure (#26455)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+49.sha-dd8a851
+ * @license Angular v7.0.0-rc.1+50.sha-1c9b065
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
+const VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
 
 /**
  * @fileoverview added by tsickle
