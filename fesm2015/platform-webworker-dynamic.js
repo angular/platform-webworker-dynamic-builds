caaa08456be85cb145485dfee27494ea71ee32e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:56:53 +0000

    929739c Revert "feat(router): add prioritizedGuardValue operator optimization and allowing UrlTree return from guard (#26478)" (#26700)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+53.sha-c8adef2
+ * @license Angular v7.0.0+67.sha-929739c.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.0+53.sha-c8adef2');
+const VERSION = new Version('7.0.0+67.sha-929739c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
