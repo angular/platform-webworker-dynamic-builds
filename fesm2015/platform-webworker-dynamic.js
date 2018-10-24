4bf67254b318780f96a0910ad70a46d0960e0409
Igor Minar <igor@angular.io>
2018-10-24 16:40:26 +0000

    ee0b857 build: rename the ivy compile mode 'local' to 'aot' (#26686)

---
+++ fesm2015/platform-webworker-dynamic.js
@@ -0,0 +1,49 @@
+/**
+ * @license Angular v7.0.0-rc.1+178.sha-ee0b857.with-local-changes
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+import { Version, COMPILER_OPTIONS, PLATFORM_ID, createPlatformFactory } from '@angular/core';
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/** *
+ * \@publicApi
+  @type {?} */
+const VERSION = new Version('7.0.0-rc.1+178.sha-ee0b857.with-local-changes');
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/** *
+ * \@publicApi
+  @type {?} */
+const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
+]);
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
