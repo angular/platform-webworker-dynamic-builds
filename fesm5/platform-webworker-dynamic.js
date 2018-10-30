9f7ab098d425ac7949cb14cf1b1d055a8bf964ca
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:32:31 +0000

    b95089d docs: replace unused variable by _ (#26768)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+66.sha-8a4ec8e
+ * @license Angular v7.1.0-beta.0+67.sha-b95089d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+66.sha-8a4ec8e');
+var VERSION = new Version('7.1.0-beta.0+67.sha-b95089d');
 
 /**
  * @license
