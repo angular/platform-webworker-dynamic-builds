2a084f594d783ce894d0dcf33625cb23524df4bb
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:43:49 +0000

    77f531b release: cut the v7.0.2 release

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.1+56.sha-3041240
+ * @license Angular v7.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.1+56.sha-3041240');
+var VERSION = new Version('7.0.2');
 
 /**
  * @license
