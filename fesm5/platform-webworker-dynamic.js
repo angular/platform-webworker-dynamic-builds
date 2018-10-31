dedc8062c7bb2f4ef0bb3239c4e611f245eb06d6
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 19:46:38 +0000

    d042c4a fix(core): Remove static dependency from @angular/core to @angular/compiler (#26734) PR Close #26734

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+74.sha-332394d
+ * @license Angular v7.1.0-beta.0+77.sha-d042c4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
+var VERSION = new Version('7.1.0-beta.0+77.sha-d042c4a');
 
 /**
  * @license
