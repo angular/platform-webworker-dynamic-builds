ca941eca1b5b05aeec0e6b08785c46308fed5ac8
GitHub <noreply@github.com>
2018-10-31 22:29:35 +0000

    2ac7292 fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

---
+++ fesm2015/platform-webworker-dynamic.js
@@ -0,0 +1,53 @@
+/**
+ * @license Angular v7.1.0-beta.0+81.sha-2ac7292
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+import { Version, COMPILER_OPTIONS, PLATFORM_ID, createPlatformFactory } from '@angular/core';
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/** *
+ * \@publicApi
+  @type {?} */
+const VERSION = new Version('7.1.0-beta.0+81.sha-2ac7292');
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/** *
+ * \@publicApi
+  @type {?} */
+const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
+]);
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
