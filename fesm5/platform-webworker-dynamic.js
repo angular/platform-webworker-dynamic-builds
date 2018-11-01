1e4d4960ef8a724520c0621a193ac6eaad716898
Kara Erickson <karakara@google.com>
2018-11-01 22:14:16 +0000

    ea0a996 fix(ivy): init hooks should be called before host bindings (#26802)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+36.sha-18b6d58
+ * @license Angular v7.1.0-beta.1+39.sha-ea0a996
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+36.sha-18b6d58');
+var VERSION = new Version('7.1.0-beta.1+39.sha-ea0a996');
 
 /**
  * @license
