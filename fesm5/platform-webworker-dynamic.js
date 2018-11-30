1fdc2f710eb0bec86d6182441c67e54897003f25
Igor Minar <igor@angular.io>
2018-11-30 06:11:07 +0000

    816ec0b refactor(ivy): treate LView as the primary global state (#27282)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+52.sha-2d51579
+ * @license Angular v7.1.0+55.sha-816ec0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+52.sha-2d51579');
+var VERSION = new Version('7.1.0+55.sha-816ec0b');
 
 /**
  * @license
