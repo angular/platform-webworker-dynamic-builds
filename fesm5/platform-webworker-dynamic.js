fb913c1f815cb60ab55cc74c8019e03d03382e8f
Igor Minar <igor@angular.io>
2018-11-30 22:50:20 +0000

    a088b8c feat(ivy): introduce "allocHostVars" instruction as a replacement for "hostVars" field (FW-692) (#27299)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+96.sha-0df914e
+ * @license Angular v7.1.0+97.sha-a088b8c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+96.sha-0df914e');
+var VERSION = new Version('7.1.0+97.sha-a088b8c');
 
 /**
  * @license
