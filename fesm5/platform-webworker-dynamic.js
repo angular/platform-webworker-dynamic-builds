1c2ca99dd429ac29b7ebdfc564cd3979372b4a42
Igor Minar <igor@angular.io>
2018-11-30 22:04:33 +0000

    fc2c23e fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+12.sha-184d63d
+ * @license Angular v7.1.1+14.sha-fc2c23e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+12.sha-184d63d');
+var VERSION = new Version('7.1.1+14.sha-fc2c23e');
 
 /**
  * @license
