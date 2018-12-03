dd9d6fa155f6431708c406b519e1723fe76ca75c
Igor Minar <igor@angular.io>
2018-12-03 16:54:13 +0000

    a082f64 fix(ivy): fix style prop instructions to account for zero-based values (#27270)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+105.sha-d847051
+ * @license Angular v7.1.0+106.sha-a082f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+105.sha-d847051');
+var VERSION = new Version('7.1.0+106.sha-a082f64');
 
 /**
  * @license
