346145cf86f6598f4af0a0a1106530735f45a31f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 20:30:29 +0000

    7d89cff fix(ivy): Ensure ComponentFactory uses both injectors to retrieve rendererFactory (#27341)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+99.sha-01fd0cd
+ * @license Angular v7.1.0+109.sha-7d89cff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+99.sha-01fd0cd');
+const VERSION = new Version('7.1.0+109.sha-7d89cff');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+99.sha-01fd0cd
+ * @license Angular v7.1.0+109.sha-7d89cff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+99.sha-01fd0cd');
+var VERSION = new Version('7.1.0+109.sha-7d89cff');
 
 /**
  * @license
