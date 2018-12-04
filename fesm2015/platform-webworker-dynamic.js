64f0847477a55b01202e30d1521ef6e3bfaeb0e2
Igor Minar <igor@angular.io>
2018-12-04 22:39:52 +0000

    d3c08e7 fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+117.sha-7ad6b03
+ * @license Angular v7.1.0+121.sha-d3c08e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16,7 +16,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+117.sha-7ad6b03');
+const VERSION = new Version('7.1.0+121.sha-d3c08e7');
 
 /**
  * @fileoverview added by tsickle
