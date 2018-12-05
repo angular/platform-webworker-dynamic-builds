cf7a37ac9923fb963fb2a4f5bd8cf50a3d554903
Igor Minar <igor@angular.io>
2018-12-05 19:11:37 +0000

    cd85832 feat(ivy): Forward refs now supported (#27439)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+155.sha-2bc3986
+ * @license Angular v7.1.0+156.sha-cd85832
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+155.sha-2bc3986');
+var VERSION = new Version('7.1.0+156.sha-cd85832');
 
 /**
  * @license
