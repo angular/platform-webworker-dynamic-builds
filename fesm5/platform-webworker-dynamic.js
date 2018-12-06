258cc351ead84f78d76c8be84b857d4c8f625acb
Igor Minar <igor@angular.io>
2018-12-06 21:56:32 +0000

    159ab1c fix(ivy): ngtsc should include generic types on injectable definitions (#27037)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+201.sha-7f221d8
+ * @license Angular v7.1.0+202.sha-159ab1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+201.sha-7f221d8');
+var VERSION = new Version('7.1.0+202.sha-159ab1c');
 
 /**
  * @license
