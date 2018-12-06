677bd519ba65c815edf1649c05444affc83c833b
Miško Hevery <misko@hevery.com>
2018-12-06 21:44:13 +0000

    3fa2a5f fix(ivy): add missing AttributeMarker export

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-6fa5383
+ * @license Angular v7.1.0+202.sha-3fa2a5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+163.sha-6fa5383');
+var VERSION = new Version('7.1.0+202.sha-3fa2a5f');
 
 /**
  * @license
