887aead445aa2b79a2a766c5f3f274b723c2af3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:20:59 +0000

    255e672 fix(ivy): define correct SVG namespace (FW-757) (#27529)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+13.sha-f034114
+ * @license Angular v7.2.0-beta.1+15.sha-255e672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+13.sha-f034114');
+var VERSION = new Version('7.2.0-beta.1+15.sha-255e672');
 
 /**
  * @license
