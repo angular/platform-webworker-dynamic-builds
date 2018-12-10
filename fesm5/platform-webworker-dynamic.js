3997c5aa9239615aae8a06ee796211574cbbbe7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 18:59:33 +0000

    54e4bdb docs: remove AngularDart from the version picker (#27550)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+16.sha-44dd764
+ * @license Angular v7.2.0-beta.1+18.sha-54e4bdb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+16.sha-44dd764');
+var VERSION = new Version('7.2.0-beta.1+18.sha-54e4bdb');
 
 /**
  * @license
