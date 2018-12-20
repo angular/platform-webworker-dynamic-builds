95b8614601d7fa9b6cf02d814432cd35fa2dedb0
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:48:43 +0000

    07ada7f fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+13.sha-880e8a5
+ * @license Angular v7.2.0-rc.0+14.sha-07ada7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
+var VERSION = new Version('7.2.0-rc.0+14.sha-07ada7f');
 
 /**
  * @license
