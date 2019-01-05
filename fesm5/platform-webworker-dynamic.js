6c2787556439d536b74f1dc2f08264d942d5473b
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:01:28 +0000

    9576e48 ci: use image based cache

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-8acaed1
+ * @license Angular v7.2.0-rc.0+53.sha-9576e48
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+53.sha-8acaed1');
+var VERSION = new Version('7.2.0-rc.0+53.sha-9576e48');
 
 /**
  * @license
