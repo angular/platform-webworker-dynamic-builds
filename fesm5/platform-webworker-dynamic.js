6d3c3394ba7cc4180d2ce3a02a77e84ad0dca6bc
Kara Erickson <karakara@google.com>
2019-01-07 17:08:28 +0000

    e94e7ee docs: add Alyle UI to resources (#27954) PR Close #27954

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0
+ * @license Angular v7.2.0+2.sha-e94e7ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0');
+var VERSION = new Version('7.2.0+2.sha-e94e7ee');
 
 /**
  * @license
