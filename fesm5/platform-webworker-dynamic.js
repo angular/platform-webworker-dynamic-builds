dbb3358e7fce52132fe4afde44b1671b9f21f8d4
Andrew Kushnir <akushnir@google.com>
2019-01-10 00:18:10 +0000

    8934b73 test(ivy): re-enable passing upgrade tests (#28030)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+80.sha-11325ba
+ * @license Angular v7.2.0+81.sha-8934b73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+80.sha-11325ba');
+var VERSION = new Version('7.2.0+81.sha-8934b73');
 
 /**
  * @license
