0c3b2bc6d3f9c6e406dcea4b00597b5a6241a8b5
Andrew Kushnir <akushnir@google.com>
2019-01-10 23:29:55 +0000

    ad6569c fix(ivy): markForCheck() should not schedule change detection (#28048)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+74.sha-9b2b9b3
+ * @license Angular v7.2.0+101.sha-ad6569c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+74.sha-9b2b9b3');
+var VERSION = new Version('7.2.0+101.sha-ad6569c');
 
 /**
  * @license
