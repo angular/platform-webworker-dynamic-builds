4c3598a680ec978bed23e8c40f7c4064b604399f
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:30:40 +0000

    a64a7a4 test(upgrade): log more info to help debug CI flakes (#28045)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+77.sha-9bda446
+ * @license Angular v7.2.0-rc.0+78.sha-a64a7a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+77.sha-9bda446');
+var VERSION = new Version('7.2.0-rc.0+78.sha-a64a7a4');
 
 /**
  * @license
