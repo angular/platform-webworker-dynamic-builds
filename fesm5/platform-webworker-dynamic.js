5324a4690b68561824cefb109ecfe44679e0dbba
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:57:54 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+198.sha-bac71ef');
+var VERSION = new Version('7.2.0+199.sha-1e5e62e');
 
 /**
  * @license
