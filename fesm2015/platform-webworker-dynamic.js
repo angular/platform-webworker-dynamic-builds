b61b736799ff3ea04dcaf5877be61dc03f5dcc1c
Andrew Kushnir <akushnir@google.com>
2019-01-17 00:25:59 +0000

    279c57f fix(bazel): Fix integration test after v8 bump (#28194)

---
+++ fesm2015/platform-webworker-dynamic.js
@@ -0,0 +1,54 @@
+/**
+ * @license Angular v7.2.0-rc.0+130.sha-279c57f
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { Version, createPlatformFactory, COMPILER_OPTIONS, PLATFORM_ID } from '@angular/core';
+import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * \@publicApi
+ * @type {?}
+ */
+const VERSION = new Version('7.2.0-rc.0+130.sha-279c57f');
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * \@publicApi
+ * @type {?}
+ */
+const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
+]);
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
