8576a34572b11d885e1a0a1c734df0841a52e559
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:31:12 +0000

    49bee4c ci(docs-infra): do not automatically post preview comments on PRs for team (#28211)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+5.sha-d8c75f1
+ * @license Angular v7.2.1+6.sha-49bee4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+5.sha-d8c75f1');
+var VERSION = new Version('7.2.1+6.sha-49bee4c');
 
 /**
  * @license
