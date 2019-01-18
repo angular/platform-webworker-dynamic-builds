12ee64a5fc56ccbe87f87de35c27b7ff7d95b09c
Alex Eagle <alexeagle@angular.io>
2019-01-18 17:43:48 +0000

    7220bfd feat(compiler-cli): expose ngtsc as a TscPlugin

---
+++ bundles/platform-webworker-dynamic.umd.js
@@ -0,0 +1,71 @@
+/**
+ * @license Angular v8.0.0-beta.0+15.sha-7220bfd
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/common'), require('@angular/compiler'), require('@angular/core'), require('@angular/platform-browser-dynamic')) :
+    typeof define === 'function' && define.amd ? define('@angular/platform-webworker-dynamic', ['exports', '@angular/common', '@angular/compiler', '@angular/core', '@angular/platform-browser-dynamic'], factory) :
+    (global = global || self, factory((global.ng = global.ng || {}, global.ng.platformWebworkerDynamic = {}), global.ng.common, global.ng.compiler, global.ng.core, global.ng.platformBrowserDynamic));
+}(this, function (exports, common, compiler, core, platformBrowserDynamic) { 'use strict';
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+    /**
+     * @publicApi
+     */
+    var VERSION = new core.Version('8.0.0-beta.0+15.sha-7220bfd');
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+    /**
+     * @publicApi
+     */
+    var platformWorkerAppDynamic = core.createPlatformFactory(platformBrowserDynamic.ɵplatformCoreDynamic, 'workerAppDynamic', [
+        {
+            provide: core.COMPILER_OPTIONS,
+            useValue: { providers: [{ provide: compiler.ResourceLoader, useClass: platformBrowserDynamic.ɵResourceLoaderImpl, deps: [] }] },
+            multi: true
+        },
+        { provide: core.PLATFORM_ID, useValue: common.ɵPLATFORM_WORKER_UI_ID }
+    ]);
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+    // This file only reexports content of the `src` folder. Keep it that way.
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+
+    /**
+     * Generated bundle index. Do not edit.
+     */
+
+    exports.platformWorkerAppDynamic = platformWorkerAppDynamic;
+    exports.VERSION = VERSION;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=platform-webworker-dynamic.umd.js.map
---
+++ fesm5/platform-webworker-dynamic.js
@@ -0,0 +1,65 @@
+/**
+ * @license Angular v8.0.0-beta.0+15.sha-7220bfd
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { Version, createPlatformFactory, COMPILER_OPTIONS, PLATFORM_ID } from '@angular/core';
+import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @publicApi
+ */
+var VERSION = new Version('8.0.0-beta.0+15.sha-7220bfd');
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @publicApi
+ */
+var platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] },
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵPLATFORM_WORKER_UI_ID }
+]);
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { platformWorkerAppDynamic, VERSION };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
