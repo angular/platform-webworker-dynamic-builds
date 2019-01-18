12916f7fcc2b79420a8b8364ac1980e5e5f9a371
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:23:05 +0000

    93d78c9 build(bazel): Use local rollup & build-optimizer for ng_rollup_bundle (#28215)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+6.sha-49bee4c
+ * @license Angular v7.2.1+7.sha-93d78c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+6.sha-49bee4c');
+var VERSION = new Version('7.2.1+7.sha-93d78c9');
 
 /**
  * @license
