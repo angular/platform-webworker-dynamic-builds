556162e93b4eaff03e35bd961bf8ca796b184a65
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:28:10 +0000

    e0fbe86 docs: add api doc to template_ref (#27380)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+50.sha-94e305f
+ * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+50.sha-94e305f');
+var VERSION = new Version('8.0.0-beta.0+51.sha-e0fbe86');
 
 /**
  * @license
