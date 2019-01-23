f70c55a014f53b188b82e398029cc257581b6b4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:47:30 +0000

    8f1198f release: cut the v8.0.0-beta.1 release

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
+ * @license Angular v8.0.0-beta.1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+66.sha-fe9b3ea');
+var VERSION = new Version('8.0.0-beta.1');
 
 /**
  * @license
