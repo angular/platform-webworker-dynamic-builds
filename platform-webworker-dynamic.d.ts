4616a1245bc60210455e398444ec3a8d60ff5162
Ben Lesh <ben@benlesh.com>
2019-02-20 22:39:32 +0000

    a7e1c0c feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- platform-webworker-dynamic.d.ts
+++ platform-webworker-dynamic.d.ts
@@ -1,4 +1,21 @@
 /**
- * Generated bundle index. Do not edit.
+ * @license Angular v8.0.0-beta.4+56.sha-a7e1c0c
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
  */
-export * from './index';
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ */
+export declare const VERSION: Version;
+
+export { }
