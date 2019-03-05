bb33ba41cf7f81657069a5814eaf37c4aa0992fa
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:09:37 +0000

    3063547 docs(router): align examples in interfaces docs (#29093)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
+ * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
 
 /**
  * @license
