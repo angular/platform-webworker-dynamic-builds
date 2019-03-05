ef68fc5b6006b732016a08b5db5e99014b907fbc
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:09:40 +0000

    7c57293 refactor(language-service): clean up imports language-service and misspellings in public API (#29097)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
+ * @license Angular v8.0.0-beta.6+75.sha-7c57293.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+75.sha-7c57293.with-local-changes');
 
 /**
  * @license
