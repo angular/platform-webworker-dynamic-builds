c361377eaab259199a88fd5af65e2a682207e163
Kara Erickson <karakara@google.com>
2019-03-07 17:57:57 +0000

    479ae51 test(ivy): update native table root causes (#29161)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+17.sha-eccbc78.with-local-changes
+ * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+17.sha-eccbc78.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+18.sha-479ae51.with-local-changes');
 
 /**
  * @license
