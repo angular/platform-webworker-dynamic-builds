8350e512e9b0790322adb3645eb6f19512fe76ec
Kara Erickson <karakara@google.com>
2019-03-11 18:29:58 +0000

    99aa967 test(ivy): remove chip list tests from blocklist (#29228)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+58.sha-6b6fdff.with-local-changes
+ * @license Angular v8.0.0-beta.7+59.sha-99aa967.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+58.sha-6b6fdff.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+59.sha-99aa967.with-local-changes');
 
 /**
  * @license
