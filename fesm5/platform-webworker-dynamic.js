33a9301ed8b0f804ba878b890257e4cc81af0133
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:28:40 +0000

    10734ac fix(ivy): Class selector directives execute properly on container elements (#29383)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+40.sha-64e5628.with-local-changes
+ * @license Angular v8.0.0-beta.9+42.sha-10734ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+40.sha-64e5628.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+42.sha-10734ac.with-local-changes');
 
 /**
  * @license
