60de9f009326c8d06e28d5dab22d72bbbd201641
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:30:24 +0000

    68a9fe8 test: remove symlink workaround (#29426)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+42.sha-10734ac.with-local-changes
+ * @license Angular v8.0.0-beta.9+41.sha-68a9fe8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+42.sha-10734ac.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+41.sha-68a9fe8.with-local-changes');
 
 /**
  * @license
