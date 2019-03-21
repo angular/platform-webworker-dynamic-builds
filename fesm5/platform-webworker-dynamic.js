83e2b7a798482ccf075c11e2ae14d3708d08fa00
Filipe Silva <filipematossilva@gmail.com>
2019-03-21 18:39:29 +0000

    d0d8ce0 ci: add codefresh

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+18.sha-4b7ed54.with-local-changes
+ * @license Angular v8.0.0-beta.8+18.sha-d0d8ce0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+18.sha-4b7ed54.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+18.sha-d0d8ce0.with-local-changes');
 
 /**
  * @license
