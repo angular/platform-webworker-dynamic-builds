85ccb0409ff816b0c5290b2c189e3bc1cfeeaa23
Jason Aden <jasonaden@google.com>
2019-04-01 18:48:45 +0000

    94b0e8f docs: update Jorge Cano GDE bio (#29622)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+40.sha-157b948.with-local-changes
+ * @license Angular v7.2.11+41.sha-94b0e8f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+40.sha-157b948.with-local-changes');
+var VERSION = new Version('7.2.11+41.sha-94b0e8f.with-local-changes');
 
 /**
  * @license
