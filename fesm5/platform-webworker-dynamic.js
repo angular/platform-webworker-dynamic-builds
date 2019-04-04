37fac0ef919e907c6877a55a02f05f89ee229cfa
Igor Minar <igor@angular.io>
2019-04-04 21:43:38 +0000

    2bd9214 fix(bazel): remove karma-jasmine from ts_web_test_suite (#29695)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+13.sha-c3aea73.with-local-changes
+ * @license Angular v8.0.0-beta.11+17.sha-2bd9214.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+13.sha-c3aea73.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
 
 /**
  * @license
