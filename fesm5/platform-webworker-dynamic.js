24b97f9fe431a6be78ec0c58a4b8dd6c8bbd4135
Jason Aden <jasonaden@google.com>
2019-04-04 18:13:14 +0000

    433b7b0 test(docs-infra): fix eslint warnings (#29673)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+10.sha-afd2417.with-local-changes
+ * @license Angular v8.0.0-beta.11+16.sha-433b7b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+10.sha-afd2417.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+16.sha-433b7b0.with-local-changes');
 
 /**
  * @license
