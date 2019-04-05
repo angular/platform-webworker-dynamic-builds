a7f509fcb4f4ab27d8f781399ad2c618fe61d4d1
Igor Minar <igor@angular.io>
2019-04-05 16:55:25 +0000

    f8cdda6 refactor: uniformly access all index sigs with index access. (#29390)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+25.sha-b6f48db.with-local-changes
+ * @license Angular v8.0.0-beta.11+26.sha-f8cdda6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+25.sha-b6f48db.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+26.sha-f8cdda6.with-local-changes');
 
 /**
  * @license
