39095626de2122c3e27394f5062640eb5f234293
Brandon <robertsbt@gmail.com>
2019-04-15 18:42:43 +0000

    afa4c50 docs: simplify components description

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @license Angular v8.0.0-beta.12+16.sha-afa4c50.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ */
+export declare const VERSION: Version;
+
+export { }
