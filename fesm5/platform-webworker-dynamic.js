22652eae50fca01bd871a7fc4914b391d3883195
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:20:28 +0000

    644f898 docs: add Andrew Kushnir to Angular team (#30045)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+2.sha-451d4bf.with-local-changes
+ * @license Angular v7.2.14+3.sha-644f898.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.14+2.sha-451d4bf.with-local-changes');
+var VERSION = new Version('7.2.14+3.sha-644f898.with-local-changes');
 
 /**
  * @license
