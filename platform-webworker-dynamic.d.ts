d5670b575274c3da16856b9c91618a6623004709
GitHub <noreply@github.com>
2019-05-02 13:56:34 +0000

    b46eb3c fixup! docs: fix `targetEntryPointPath` description

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @license Angular v8.0.0-rc.0+81.sha-b46eb3c.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ */
+export declare const VERSION: Version;
+
+export { }
