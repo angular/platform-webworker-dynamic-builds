fc0651d41b5368145052c1dea78f03f85704bf0d
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:39:17 +0000

    45718b3 build(docs-infra): upgrade cli command docs sources to 9a3d36c98 (#30609)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+5.sha-b5756bc.with-local-changes
+ * @license Angular v8.0.0-rc.5+6.sha-45718b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.5+5.sha-b5756bc.with-local-changes');
+const VERSION = new Version('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
