909e872865430aa87bacd3d2829d4d9c2c2766c3
GitHub <noreply@github.com>
2019-05-24 10:40:48 +0000

    05479f8 chore: static-query migration should use permalink for migration guide

---
+++ fesm2015/platform-webworker-dynamic.js
@@ -0,0 +1,57 @@
+/**
+ * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { Version, createPlatformFactory, COMPILER_OPTIONS, PLATFORM_ID } from '@angular/core';
+import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * \@publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @type {?}
+ */
+const VERSION = new Version('8.0.0-rc.0+336.sha-05479f8.with-local-changes');
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+const ɵ0 = { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] }, ɵ1 = ɵPLATFORM_WORKER_UI_ID;
+/**
+ * \@publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @type {?}
+ */
+const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: ɵ0,
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵ1 }
+]);
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const VERSION: Version;
+
+export { }
