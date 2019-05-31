9327890ae47a0d8db1703287baead0f1d26f213c
Misko Hevery <misko@hevery.com>
2019-05-31 04:11:45 +0000

    fa35488 ci: add reviewer group for deprecations guide (#30722) PR Close #30722

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+2.sha-d82adbe.with-local-changes
+ * @license Angular v8.1.0-beta.0+3.sha-fa35488.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+2.sha-d82adbe.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+3.sha-fa35488.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
