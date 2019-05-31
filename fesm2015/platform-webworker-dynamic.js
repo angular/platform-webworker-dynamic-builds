72d6ad2c456524d711b489a92fd922c83aad9ace
Miško Hevery <misko@hevery.com>
2019-05-31 17:04:24 +0000

    cdf586d build(docs-infra): upgrade cli command docs sources to e8bdfe6b9 (#30777)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+17.sha-0f352b6.with-local-changes
+ * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0+17.sha-0f352b6.with-local-changes');
+const VERSION = new Version('8.0.0+19.sha-cdf586d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
