791e363a8bd8d8bcd049999c02bb1c93cd0e810a
Misko Hevery <misko@hevery.com>
2019-06-03 16:36:27 +0000

    b4e6802 refactor(ivy): add ɵɵupdateSyntheticHostBinding command (#30670)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+20.sha-680d385.with-local-changes
+ * @license Angular v8.1.0-beta.0+21.sha-b4e6802.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+20.sha-680d385.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
