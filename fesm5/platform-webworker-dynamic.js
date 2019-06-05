b2520e5617e0665427af1f367b1f2fb975dbbf02
Jeremy Elbourn <jelbourn@google.com>
2019-06-05 17:33:59 +0000

    95ad7f1 fix(bazel): exclude components schematics from build

---
+++ fesm5/platform-webworker-dynamic.js
@@ -0,0 +1,68 @@
+/**
+ * @license Angular v8.1.0-beta.0+24.sha-95ad7f1.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { Version, createPlatformFactory, COMPILER_OPTIONS, PLATFORM_ID } from '@angular/core';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+var VERSION = new Version('8.1.0-beta.0+24.sha-95ad7f1.with-local-changes');
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+var ɵ0 = { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] }, ɵ1 = ɵPLATFORM_WORKER_UI_ID;
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+var platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: ɵ0,
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵ1 }
+]);
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { platformWorkerAppDynamic, ɵ0, ɵ1, VERSION };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
