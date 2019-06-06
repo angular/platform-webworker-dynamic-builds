fac6578422dc114e9b7b97d6a63ea5f27ced34b8
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:50 +0000

    30efb6b fix(ivy): don't project removed placeholders with runtime i18n (#30783)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
+ * @license Angular v8.1.0-next.1+5.sha-30efb6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.1+1.sha-17bfedd.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+5.sha-30efb6b.with-local-changes');
 
 /**
  * @license
