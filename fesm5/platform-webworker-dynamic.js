9c5bd316891381dd2e8584f63b2f5d000818cd01
Miško Hevery <misko@hevery.com>
2019-06-06 21:08:22 +0000

    11a4454 perf(ivy): remove check for function type in renderStringify (#30838)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
+ * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+10.sha-11a4454.with-local-changes');
 
 /**
  * @license
