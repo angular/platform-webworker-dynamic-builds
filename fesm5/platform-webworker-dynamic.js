93dcdab6927670b8d0f8f1471e00fddcf8e3b46e
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:30 +0000

    17bfedd fix(bazel): Load global stylesheet in dev and prod (#30879)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+3.sha-b51d8dd.with-local-changes
+ * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.1+3.sha-b51d8dd.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+1.sha-17bfedd.with-local-changes');
 
 /**
  * @license
