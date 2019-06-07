8a4d78e6b0d93945564c896862bd64a1bbebeb8b
Misko Hevery <misko@hevery.com>
2019-06-07 16:11:46 +0000

    f7ee91a build(docs-infra): remove unneeded `allowEmptyCodegenFiles` option in Ivy mode (#30911)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+42.sha-be82270.with-local-changes
+ * @license Angular v8.0.0+44.sha-f7ee91a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0+42.sha-be82270.with-local-changes');
+var VERSION = new Version('8.0.0+44.sha-f7ee91a.with-local-changes');
 
 /**
  * @license
