c04efdf28df9d441225ae7bc7284e62add263115
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-09 14:40:50 +0000

    99331c0 test: remove rsync

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @license Angular v8.1.0-beta.0+32.sha-99331c0.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const VERSION: Version;
+
+export { }
