7e5fe28ee1f001b3fbc085dc338c3124bc0a3fb0
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:00 +0000

    e2fd628 docs: edit template ref vars copy and example (#27371)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+17.sha-2c2135d.with-local-changes
+ * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.2+17.sha-2c2135d.with-local-changes');
+var VERSION = new Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
 
 /**
  * @license
