a3db34276c7f918ee0f4858df31e1612434d5f8c
Kara Erickson <karakara@google.com>
2019-06-25 03:40:59 +0000

    f7e9659c4 fix(docs-infra): return full width highlight to sidenav (#31246)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+51.sha-16717fa.with-local-changes
+ * @license Angular v8.1.0-next.3+52.sha-f7e9659.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+51.sha-16717fa.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+52.sha-f7e9659.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
