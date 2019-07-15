ce06e1a7c74ff5ab41ccb53fe2fcfc0153cbe916
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 21:27:03 +0000

    40705f336 docs: include svg files in stackblitz and download examples (#31559)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+53.sha-19bc111.with-local-changes
+ * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.1+53.sha-19bc111.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+54.sha-40705f3.with-local-changes');
 
 /**
  * @license
