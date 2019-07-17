6179fd343f0c195731c614e3d1ccc4c268289580
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 04:21:45 +0000

    12fd06916 fix(ivy): don't match directives against attribute bindings (#31541)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+66.sha-9e83822.with-local-changes
+ * @license Angular v8.2.0-next.1+67.sha-12fd069.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.1+66.sha-9e83822.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+67.sha-12fd069.with-local-changes');
 
 /**
  * @license
