1500a952bd4cbaf7e02b32df260cb85e8b52d579
Miško Hevery <misko@hevery.com>
2019-07-25 20:31:07 +0000

    6b67cd562 feat(ivy): index template references, variables, bound attributes/events (#31535)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+88.sha-0cd4c01.with-local-changes
+ * @license Angular v8.2.0-next.2+89.sha-6b67cd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
