10c1886b7d582518dd87698dfebf460afc54ae9f
Andrew Kushnir <akushnir@google.com>
2019-07-26 20:19:14 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+94.sha-6ece7db.with-local-changes
+ * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+94.sha-6ece7db.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
 
 /**
  * @license
