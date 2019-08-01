1d80209be1bf94df1c40de767cbe5c23d3576819
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:43 +0000

    e8b8f6d09 fix(language-service): getSourceFile() should only be called on TS files (#31920)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+6.sha-82b9728.with-local-changes
+ * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.0+6.sha-82b9728.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+10.sha-e8b8f6d.with-local-changes');
 
 /**
  * @license
