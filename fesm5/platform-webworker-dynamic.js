35d8d749829e9250d9227610c4bd3e4d3df6183a
Kara Erickson <karakara@google.com>
2019-08-09 18:14:55 +0000

    eb5412d76 fix(ivy): reuse compilation scope for incremental template changes. (#31932)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+10.sha-fd6ed17.with-local-changes
+ * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.1+10.sha-fd6ed17.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
 
 /**
  * @license
