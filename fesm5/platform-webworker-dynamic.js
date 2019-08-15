026609cf8b97777bed75c170f845982a4679e286
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:03:36 +0000

    6f2e8afae docs: fix typo of Typescript to TypeScript (#32153)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+17.sha-adc869f.with-local-changes
+ * @license Angular v8.2.2+19.sha-6f2e8af.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+17.sha-adc869f.with-local-changes');
+var VERSION = new Version('8.2.2+19.sha-6f2e8af.with-local-changes');
 
 /**
  * @license
