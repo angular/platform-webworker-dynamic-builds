b22172854dc90847e86a369dc95c18c1bc6c439a
atscott <atscott@google.com>
2019-08-21 22:37:16 +0000

    44f233c02 docs: edit and organize aot doc (#32028)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+29.sha-e3c9f9d.with-local-changes
+ * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.2.2+29.sha-e3c9f9d.with-local-changes');
+const VERSION = new Version('8.2.3+1.sha-44f233c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+29.sha-e3c9f9d.with-local-changes
+ * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+29.sha-e3c9f9d.with-local-changes');
+var VERSION = new Version('8.2.3+1.sha-44f233c.with-local-changes');
 
 /**
  * @license
