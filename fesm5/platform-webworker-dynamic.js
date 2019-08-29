4be98838b83086d713127a32681b8fbec74723fb
Miško Hevery <misko@hevery.com>
2019-08-29 19:57:07 +0000

    9d2348b9a docs: fix typo in compiler options guide (#32348)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+4.sha-e7bca07.with-local-changes
+ * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.4+4.sha-e7bca07.with-local-changes');
+var VERSION = new Version('8.2.4+5.sha-9d2348b.with-local-changes');
 
 /**
  * @license
