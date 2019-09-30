03e2bd3a043bc40a7ac8a49906d10905797fea8b
atscott <atscott@google.com>
2019-09-30 17:48:43 +0000

    d5071651d docs: add filename headers to code examples (#32701)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+20.sha-877f9ad.with-local-changes
+ * @license Angular v9.0.0-next.8+22.sha-d507165.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.8+20.sha-877f9ad.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+22.sha-d507165.with-local-changes');
 
 /**
  * @license
