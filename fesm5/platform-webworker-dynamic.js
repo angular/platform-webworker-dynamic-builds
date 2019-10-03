78c627733e13d836cac943c908141a37fda0cc66
atscott <atscott01@gmail.com>
2019-10-03 17:19:15 +0000

    e24393c35 test(upgrade): add unit tests for AngularJSUrlCodec's parse method (#32976)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+10.sha-8237e95.with-local-changes
+ * @license Angular v8.2.9+11.sha-e24393c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.9+10.sha-8237e95.with-local-changes');
+var VERSION = new Version('8.2.9+11.sha-e24393c.with-local-changes');
 
 /**
  * @license
