220353b1c2501d2c527ba67b58ed8fbac578cfa5
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:15:54 +0000

    0e08ad628a fix(ivy): throw better error for missing generic type in ModuleWithProviders (#33187)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+74.sha-e0059c7.with-local-changes
+ * @license Angular v9.0.0-next.11+75.sha-0e08ad6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+74.sha-e0059c7.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+75.sha-0e08ad6.with-local-changes');
 
 /**
  * @license
