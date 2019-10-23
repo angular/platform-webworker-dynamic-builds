632ae2034207409b94a496865521a819966b9fca
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:21:19 +0000

    ba29e4d953 feat(docs-infra): implement figure styles (#33259)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+65.sha-355e54a.with-local-changes
+ * @license Angular v9.0.0-next.12+66.sha-ba29e4d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12+65.sha-355e54a.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+66.sha-ba29e4d.with-local-changes');
 
 /**
  * @license
