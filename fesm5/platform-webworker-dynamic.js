a28341c9f20deab5cc40f904b5ffc75f9e5e2462
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:18:49 +0000

    efbbae5a48 ci: publish tarballs for all Angular packages as build artifacts on PR builds (#33321)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+7.sha-3d11355.with-local-changes
+ * @license Angular v9.0.0-next.13+10.sha-efbbae5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.13+7.sha-3d11355.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+10.sha-efbbae5.with-local-changes');
 
 /**
  * @license
