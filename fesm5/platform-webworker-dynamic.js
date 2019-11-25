fafbc7d6f5bb6993f246e46c15436ef9239c12af
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:07:02 +0000

    457ac3ac46 perf(ivy): fix creation time micro-benchmarks (#34031)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+63.sha-688b188.with-local-changes
+ * @license Angular v9.0.0-rc.3+64.sha-457ac3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.3+63.sha-688b188.with-local-changes');
+var VERSION = new Version('9.0.0-rc.3+64.sha-457ac3a.with-local-changes');
 
 /**
  * @license
