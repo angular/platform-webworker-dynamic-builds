3a175957a99dd406d39bdf507f57301c2e4abe57
Andrew Kushnir <akushnir@google.com>
2019-12-06 18:48:26 +0000

    613dc1122a fix(language-service): Simplify resolution logic in banner (#34262)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+393.sha-3805172.with-local-changes
+ * @license Angular v9.0.0-rc.1+394.sha-613dc11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+393.sha-3805172.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+394.sha-613dc11.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
