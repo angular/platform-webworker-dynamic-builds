7b95f4ce29cddf21478f5647ef7adda5e92962ac
Kara Erickson <karakara@google.com>
2019-12-16 16:03:28 +0000

    3bbd12d560 build: update to yarn@1.21.1 (#34384)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+472.sha-f79110c.with-local-changes
+ * @license Angular v9.0.0-rc.1+476.sha-3bbd12d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+472.sha-f79110c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+476.sha-3bbd12d');
 
 /**
  * @license
