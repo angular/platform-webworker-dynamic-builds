08b062f3701ba63d408a92d540c66e0c77a1a685
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:53:45 +0000

    ba2fd31e62 fix(language-service): The pipe method should not include parentheses (#34485)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+529.sha-eab7f9f
+ * @license Angular v9.0.0-rc.1+530.sha-ba2fd31
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+529.sha-eab7f9f');
+const VERSION = new Version('9.0.0-rc.1+530.sha-ba2fd31');
 
 /**
  * @fileoverview added by tsickle
