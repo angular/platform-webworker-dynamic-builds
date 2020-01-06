f8e114345c60f9078d7644064297ce0259367e53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:23:33 +0000

    8410278b6d docs: copy edit getting started step 4 (#32839)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+521.sha-9b9116c
+ * @license Angular v9.0.0-rc.1+522.sha-8410278
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+521.sha-9b9116c');
+const VERSION = new Version('9.0.0-rc.1+522.sha-8410278');
 
 /**
  * @fileoverview added by tsickle
