6cacab3a766a5d95ff65b2f4d52cba09a9138468
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:40:22 +0000

    1de49ba369 refactor(ivy): consistently translate types to `ts.TypeNode` (#34021)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+523.sha-19944c2
+ * @license Angular v9.0.0-rc.1+525.sha-1de49ba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+523.sha-19944c2');
+const VERSION = new Version('9.0.0-rc.1+525.sha-1de49ba');
 
 /**
  * @fileoverview added by tsickle
