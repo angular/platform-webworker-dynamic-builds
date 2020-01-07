136d86af504698942d0965e17a68169c66eca729
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 18:59:59 +0000

    0254cba4b3 docs: Fix double 'as an' in description at line 2069 (#34373)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+534.sha-effb92d
+ * @license Angular v9.0.0-rc.1+535.sha-0254cba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+534.sha-effb92d');
+const VERSION = new Version('9.0.0-rc.1+535.sha-0254cba');
 
 /**
  * @fileoverview added by tsickle
