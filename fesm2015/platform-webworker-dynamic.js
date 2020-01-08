7e5595d21f827e1210d5fdcebd1a650e534d62d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 21:43:56 +0000

    10acac07e1 build: update `firebase-tools` to the latest version (#34682)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.7+37.sha-e549846
+ * @license Angular v9.0.0-rc.7+38.sha-10acac0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.7+37.sha-e549846');
+const VERSION = new Version('9.0.0-rc.7+38.sha-10acac0');
 
 /**
  * @fileoverview added by tsickle
