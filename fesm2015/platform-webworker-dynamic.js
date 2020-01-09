69181193a699e8598d02311f132e951597266af2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 00:07:33 +0000

    8815ace418 fix(ngcc): insert definitions after statement (#34677)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+557.sha-02e4921
+ * @license Angular v9.0.0-rc.1+574.sha-8815ace
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+557.sha-02e4921');
+const VERSION = new Version('9.0.0-rc.1+574.sha-8815ace');
 
 /**
  * @fileoverview added by tsickle
