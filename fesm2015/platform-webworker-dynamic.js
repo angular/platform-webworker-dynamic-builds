b6598e1ce6b64af0c8d8aad557128fb52525624e
atscott <atscott@google.com>
2020-01-09 21:52:04 +0000

    0083443050 docs: add note on importing AppRoutingModule (#34710)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+574.sha-8815ace
+ * @license Angular v9.0.0-rc.1+575.sha-0083443
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+574.sha-8815ace');
+const VERSION = new Version('9.0.0-rc.1+575.sha-0083443');
 
 /**
  * @fileoverview added by tsickle
