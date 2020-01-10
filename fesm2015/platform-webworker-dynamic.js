1c688824ce2ffcf9397f7a227dd2e6df02ea060a
atscott <atscott@google.com>
2020-01-10 17:00:59 +0000

    a5cdad57fe docs: move ngcc def from page to glossary (#34196)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+30.sha-f312848
+ * @license Angular v9.0.0-rc.8+55.sha-a5cdad5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+30.sha-f312848');
+const VERSION = new Version('0.0.0');
 
 /**
  * @fileoverview added by tsickle
