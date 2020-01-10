8a41941fc97ab33554cbd5109a850ba88aa78b85
atscott <atscott@google.com>
2020-01-10 23:04:38 +0000

    6d28a209e4 test(language-service): Remove test cases from parsing-cases.ts (#34716)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+629.sha-e88d652
+ * @license Angular v9.0.0-rc.1+631.sha-6d28a20
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+629.sha-e88d652');
+const VERSION = new Version('9.0.0-rc.1+631.sha-6d28a20');
 
 /**
  * @fileoverview added by tsickle
