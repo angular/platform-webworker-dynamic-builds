ba31ba144c5b62a797e69f70836a60b92fd9a8bc
atscott <atscott@google.com>
2020-01-11 00:16:55 +0000

    6d534f10e6 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+631.sha-6d28a20
+ * @license Angular v9.0.0-rc.1+632.sha-6d534f1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+631.sha-6d28a20');
+const VERSION = new Version('9.0.0-rc.1+632.sha-6d534f1');
 
 /**
  * @fileoverview added by tsickle
