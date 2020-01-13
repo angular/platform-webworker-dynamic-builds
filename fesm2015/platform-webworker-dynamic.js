fef871c294c3279a97d61717b8ed6d85f6ae37da
atscott <atscott@google.com>
2020-01-13 15:45:30 +0000

    a0eb57fb81 docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v9.0.0-rc.8+76.sha-08c2581
- * (c) 2010-2019 Google LLC. https://angular.io/
+ * @license Angular v9.0.0-rc.8+85.sha-a0eb57f
+ * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+76.sha-08c2581');
+const VERSION = new Version('9.0.0-rc.8+85.sha-a0eb57f');
 
 /**
  * @fileoverview added by tsickle
