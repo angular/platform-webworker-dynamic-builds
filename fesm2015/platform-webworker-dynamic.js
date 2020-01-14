9439c5f25a716b13aa420eaa3ef503e85e436484
atscott <atscott@google.com>
2020-01-14 18:52:57 +0000

    ec147bd839 docs: remove old contributors (#34486)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+659.sha-280aae6
+ * @license Angular v9.0.0-rc.1+668.sha-ec147bd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+659.sha-280aae6');
+const VERSION = new Version('9.0.0-rc.1+668.sha-ec147bd');
 
 /**
  * @fileoverview added by tsickle
