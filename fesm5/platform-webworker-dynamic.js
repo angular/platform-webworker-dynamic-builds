e40606d44a23cc1e849e2df7fdfc742be48df495
atscott <atscott@google.com>
2020-01-14 23:41:03 +0000

    c3c72f689a fix(ivy): handle overloaded constructors in ngtsc (#34590)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+672.sha-9ceee07
+ * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+672.sha-9ceee07');
+var VERSION = new Version('9.0.0-rc.1+673.sha-c3c72f6');
 
 /**
  * @license
