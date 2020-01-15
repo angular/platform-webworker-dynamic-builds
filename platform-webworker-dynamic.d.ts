33fc243e584a2a5cd79c87824fc5f57733675be4
GitHub <noreply@github.com>
2020-01-15 11:23:21 +0000

    ae9c061fd8 docs: fix public api tool name

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const VERSION: Version;
+
+export { }
