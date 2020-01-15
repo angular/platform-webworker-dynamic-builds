e49b7ae0be65fae2739f1f6155b73b26dd9eebb6
atscott <atscott@google.com>
2020-01-15 01:02:21 +0000

    bf8ba89394 test: update typings test to use ts 3.7.4 instead of 3.7.3 (#33717)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+675.sha-d7ea389
+ * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+675.sha-d7ea389');
+var VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
 
 /**
  * @license
