9613c454defca3597024a598229458caf72883c7
GitHub <noreply@github.com>
2020-01-15 12:14:23 +0000

    50e52c8e1b fixup! docs: fix public api tool name

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+689.sha-ae9c061');
+const VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+689.sha-ae9c061');
+var VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
 
 /**
  * @license
