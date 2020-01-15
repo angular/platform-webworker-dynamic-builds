c08e58f7d6848c13ea210f900880cc261778735a
atscott <atscott@google.com>
2020-01-15 00:12:45 +0000

    fa39a8c026 feat(language-service): provide completion for $event variable (#34566)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
+var VERSION = new Version('9.0.0-rc.8+119.sha-fa39a8c');
 
 /**
  * @license
