d1cda37df53c0d151ab4c375b3f33ba9e6463f99
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:43:50 +0000

    cc7fca4dc1 feat(language-service): specific suggestions for template context diags (#34751)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+37.sha-6a4186c
+ * @license Angular v9.0.0-rc.10+38.sha-cc7fca4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+37.sha-6a4186c');
+var VERSION = new Version('9.0.0-rc.10+38.sha-cc7fca4');
 
 /**
  * @license
