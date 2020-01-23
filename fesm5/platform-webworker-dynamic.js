bf0f564b8c920ba1f4582ded8a5b0a5e3a3a1097
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:35:35 +0000

    1ea04ffc05 feat(language-service): support multiple symbol definitions (#34782)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
+ * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+762.sha-48f8ca5');
+var VERSION = new Version('9.0.0-rc.1+763.sha-1ea04ff');
 
 /**
  * @license
