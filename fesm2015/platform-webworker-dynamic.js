c2edad531325b1f58df79fbae4b642e35cb3d4be
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:35:57 +0000

    68025ce09a test(language-service): recognize inputs/ouputs declared in decorator (#34875)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+const VERSION = new Version('9.0.0-rc.1+764.sha-68025ce');
 
 /**
  * @fileoverview added by tsickle
