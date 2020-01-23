64ff15e273b4fb481685cd802f3b7e487fcd1dc7
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:49:45 +0000

    fa4ea34401 ci: rename `material-unit-tests` job to `components-repo-unit-tests` (#34898)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+779.sha-3049878
+ * @license Angular v9.0.0-rc.1+780.sha-fa4ea34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+779.sha-3049878');
+const VERSION = new Version('9.0.0-rc.1+780.sha-fa4ea34');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+779.sha-3049878
+ * @license Angular v9.0.0-rc.1+780.sha-fa4ea34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+779.sha-3049878');
+var VERSION = new Version('9.0.0-rc.1+780.sha-fa4ea34');
 
 /**
  * @license
