7e294301bd34bf4861166a31ed74e4282977a03f
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:02:57 +0000

    eb4b7d2d8b build: vendor yarn using the .yarnrc yarn-path value (#34902)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+775.sha-970b22f
+ * @license Angular v9.0.0-rc.1+776.sha-eb4b7d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+775.sha-970b22f');
+var VERSION = new Version('9.0.0-rc.1+776.sha-eb4b7d2');
 
 /**
  * @license
