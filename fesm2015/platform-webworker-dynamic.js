c24054e547f9cd0ce865da24eae10de14af6b63c
Miško Hevery <misko@hevery.com>
2020-01-24 20:09:34 +0000

    0eaf8747e6 refactor(ivy): change styling to use programmatic API on updates (#34804)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+745.sha-ad68b61
+ * @license Angular v9.0.0-rc.1+804.sha-0eaf874
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('0.0.0');
+const VERSION = new Version('9.0.0-rc.1+804.sha-0eaf874');
 
 /**
  * @fileoverview added by tsickle
