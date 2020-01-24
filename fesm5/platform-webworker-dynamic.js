cb3f4c7044b5efdfb2099d331754c3664e54d8ee
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:15:20 +0000

    c9db7bdb8a fix(language-service): Diagnostic span should point to class name (#34932)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+47.sha-080a8bf
+ * @license Angular v9.0.0-rc.10+49.sha-c9db7bd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+47.sha-080a8bf');
+var VERSION = new Version('9.0.0-rc.10+49.sha-c9db7bd');
 
 /**
  * @license
