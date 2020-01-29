748f619162d2ff9de6e9e3d7ffed0a5fbe448aee
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:36:33 +0000

    a3de110b52 build(docs-infra): update aio to @angular/cli@9.0.0-rc.11 (#35043)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+861.sha-95383ee
+ * @license Angular v9.0.0-rc.1+859.sha-a3de110
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+861.sha-95383ee');
+const VERSION = new Version('9.0.0-rc.1+859.sha-a3de110');
 
 /**
  * @fileoverview added by tsickle
