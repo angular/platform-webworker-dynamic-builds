8c7a686d8709d0d9420e48baca03f1437a247c44
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:51:04 +0000

    d43187f7ef ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job (for real) (#35033)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+848.sha-fb852f7
+ * @license Angular v9.0.0-rc.1+849.sha-d43187f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+848.sha-fb852f7');
+const VERSION = new Version('9.0.0-rc.1+849.sha-d43187f');
 
 /**
  * @fileoverview added by tsickle
