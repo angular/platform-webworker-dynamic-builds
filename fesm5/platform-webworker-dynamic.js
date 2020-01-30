bc5651214c86455fe0a353d099e2374e02e926fc
Andrew Kushnir <akushnir@google.com>
2020-01-30 18:46:57 +0000

    d72cfc9d57 docs: remove deployment section on golang because of errors (#34099)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+866.sha-37f39d6
+ * @license Angular v9.0.0-rc.1+867.sha-d72cfc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+866.sha-37f39d6');
+var VERSION = new Version('9.0.0-rc.1+867.sha-d72cfc9');
 
 /**
  * @license
