e320b84f3857c2db822ff3a4bb435b45ec339e70
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:21:44 +0000

    3bdf1164ce ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+67.sha-36dfca9');
+const VERSION = new Version('9.0.0-rc.12+1.sha-3bdf116');
 
 /**
  * @fileoverview added by tsickle
