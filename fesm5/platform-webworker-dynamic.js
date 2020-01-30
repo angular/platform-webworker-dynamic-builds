34d8c946b5c9b43f476ae0945a335842e08a73a0
Andrew Kushnir <akushnir@google.com>
2020-01-30 17:29:18 +0000

    3b612afff3 build(compiler-cli): update to chokidar 3.x (#35047)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+62.sha-36b8c03
+ * @license Angular v9.0.0-rc.11+63.sha-3b612af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.11+62.sha-36b8c03');
+var VERSION = new Version('9.0.0-rc.11+63.sha-3b612af');
 
 /**
  * @license
