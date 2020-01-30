c66bd529fb23089f5b57fa61a90a8a78be547e20
Andrew Kushnir <akushnir@google.com>
2020-01-30 21:09:51 +0000

    9f5b490800 Revert "refactor(compiler): refactor and export Bazel host creation. (#34686)" (#35063)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+869.sha-cd9ae66
+ * @license Angular v9.0.0-rc.1+870.sha-9f5b490
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+869.sha-cd9ae66');
+var VERSION = new Version('9.0.0-rc.1+870.sha-9f5b490');
 
 /**
  * @license
