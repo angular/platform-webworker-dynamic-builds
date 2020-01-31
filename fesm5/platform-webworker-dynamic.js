2d57c02e20be038cab0980a13e915d7c1b00de5b
Misko Hevery <misko@hevery.com>
2020-01-31 22:55:04 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+887.sha-58b29f1
+ * @license Angular v9.0.0-rc.1+888.sha-6d11a81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+887.sha-58b29f1');
+var VERSION = new Version('9.0.0-rc.1+888.sha-6d11a81');
 
 /**
  * @license
