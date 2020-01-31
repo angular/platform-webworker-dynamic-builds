13fae207003955db445ccfdaf53da4c30675fe5f
Misko Hevery <misko@hevery.com>
2020-01-31 21:08:22 +0000

    fcfce99e9e fix(common): remove extra & in http params (#34896)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+876.sha-c070037
+ * @license Angular v9.0.0-rc.1+878.sha-fcfce99
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+876.sha-c070037');
+const VERSION = new Version('9.0.0-rc.1+878.sha-fcfce99');
 
 /**
  * @fileoverview added by tsickle
