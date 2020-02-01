0c7d702ab2130fad87dc8c0f56d3a6c865da56ce
Misko Hevery <misko@hevery.com>
2020-02-01 01:23:36 +0000

    58dbe662ff build: temporarily disable components-repo-unit-tests (#35079)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.12+17.sha-7f96fbb
+ * @license Angular v9.0.0-rc.12+24.sha-58dbe66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.12+17.sha-7f96fbb');
+var VERSION = new Version('9.0.0-rc.12+24.sha-58dbe66');
 
 /**
  * @license
