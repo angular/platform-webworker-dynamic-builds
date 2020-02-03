7d27bd66cdab297bedaf730bbe6927c6d32316e9
Miško Hevery <misko@hevery.com>
2020-02-03 22:28:04 +0000

    3c30474417 docs: update zone-bluebird patch document for angular (#34536)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+904.sha-9601b5d
+ * @license Angular v9.0.0-rc.1+905.sha-3c30474
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+904.sha-9601b5d');
+const VERSION = new Version('9.0.0-rc.1+905.sha-3c30474');
 
 /**
  * @fileoverview added by tsickle
