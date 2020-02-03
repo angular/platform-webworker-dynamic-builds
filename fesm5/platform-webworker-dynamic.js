812dbcb4e2146c9a75bf601b559bfb5200f22a7a
Miško Hevery <misko@hevery.com>
2020-02-03 22:24:32 +0000

    9601b5d18a refactor(ngcc): remove unused function (#35122)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+903.sha-ab931cf
+ * @license Angular v9.0.0-rc.1+904.sha-9601b5d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+903.sha-ab931cf');
+var VERSION = new Version('9.0.0-rc.1+904.sha-9601b5d');
 
 /**
  * @license
