70946ee134ab31674ee26c813740ffb145100708
Miško Hevery <misko@hevery.com>
2020-02-04 17:22:03 +0000

    7369dd6f24 docs: fix animations example/remove 1st person (#35046)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+907.sha-128d4ac
+ * @license Angular v9.0.0-rc.1+909.sha-7369dd6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+907.sha-128d4ac');
+const VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
 
 /**
  * @fileoverview added by tsickle
