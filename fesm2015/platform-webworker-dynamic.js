f1c929aa4b701345612cc0e194b9f64e709bf719
Misko Hevery <misko@hevery.com>
2020-02-06 23:19:05 +0000

    60b887090c docs: consilidate release notes for the v8.0.0 release (#35197)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+9.sha-bcb539f
+ * @license Angular v9.0.0+10.sha-60b8870
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0+9.sha-bcb539f');
+const VERSION = new Version('9.0.0+10.sha-60b8870');
 
 /**
  * @fileoverview added by tsickle
