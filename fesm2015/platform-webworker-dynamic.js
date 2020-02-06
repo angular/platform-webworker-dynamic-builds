cbc9eaf0fabcef7a31d768936aa389e564dcab06
Misko Hevery <misko@hevery.com>
2020-02-06 02:40:15 +0000

    483ba6aef7 docs(ivy): document breaking change with host attribute priority (#35175)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+22.sha-24555db
+ * @license Angular v9.0.0-rc.14+23.sha-483ba6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.14+22.sha-24555db');
+const VERSION = new Version('9.0.0-rc.14+23.sha-483ba6a');
 
 /**
  * @fileoverview added by tsickle
