e94c85bb7e4d26fb27364b30b68c7be66a24692f
Kara Erickson <karakara@google.com>
2020-02-06 22:51:49 +0000

    bcb539f6de docs: remove the release schedule from docs (#34474)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+7.sha-f4b9d66
+ * @license Angular v9.0.0+9.sha-bcb539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0+7.sha-f4b9d66');
+const VERSION = new Version('9.0.0+9.sha-bcb539f');
 
 /**
  * @fileoverview added by tsickle
