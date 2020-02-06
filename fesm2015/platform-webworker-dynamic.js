ce41cf258834d05f068461b3f40f2cb165e6fbff
Misko Hevery <misko@hevery.com>
2020-02-06 22:06:39 +0000

    053dabb7ed docs: consolidate and remove rc/next notes from the v9.0.0 release (#35195)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+937.sha-22acdc2
+ * @license Angular v9.0.0-rc.1+938.sha-053dabb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+937.sha-22acdc2');
+const VERSION = new Version('9.0.0-rc.1+938.sha-053dabb');
 
 /**
  * @fileoverview added by tsickle
