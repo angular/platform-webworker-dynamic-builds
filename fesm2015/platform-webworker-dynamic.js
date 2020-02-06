424d0df8a4a689aa36f9a0c8de49b9db7be8968d
Misko Hevery <misko@hevery.com>
2020-02-06 22:13:09 +0000

    f4b9d664f2 docs(ivy): add a not about compile-time constants to the ivy compatibility guide (#35194)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+5.sha-becab76
+ * @license Angular v9.0.0+7.sha-f4b9d66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0+5.sha-becab76');
+const VERSION = new Version('9.0.0+7.sha-f4b9d66');
 
 /**
  * @fileoverview added by tsickle
