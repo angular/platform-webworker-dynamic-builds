56b29f9c5010a047f5fc5878c4f8c318dee196d1
Misko Hevery <misko@hevery.com>
2020-02-06 01:45:22 +0000

    24555dbb2d build: update @angular/cli to rc 14 (#35162)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+21.sha-7de8772
+ * @license Angular v9.0.0-rc.14+22.sha-24555db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.14+21.sha-7de8772');
+const VERSION = new Version('9.0.0-rc.14+22.sha-24555db');
 
 /**
  * @fileoverview added by tsickle
