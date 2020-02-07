9b213b4d42a7a71ae4e2c204fcd26b9566ea4776
Misko Hevery <misko@hevery.com>
2020-02-07 19:45:49 +0000

    1c85c24ed3 docs(changelog): formatting fix (#35224) PR Close #35224

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+959.sha-0cbdd54
+ * @license Angular v9.0.0-rc.1+961.sha-1c85c24
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+959.sha-0cbdd54');
+const VERSION = new Version('9.0.0-rc.1+961.sha-1c85c24');
 
 /**
  * @fileoverview added by tsickle
