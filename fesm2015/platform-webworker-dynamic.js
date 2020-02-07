bd10bb34a09d9ef24068e915dfde1b469e55e525
Kara Erickson <karakara@google.com>
2020-02-07 20:04:27 +0000

    daac33cdc8 feat: add basic jest support (#35080)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+965.sha-f7e1c21
+ * @license Angular v9.0.0-rc.1+966.sha-daac33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+965.sha-f7e1c21');
+const VERSION = new Version('9.0.0-rc.1+966.sha-daac33c');
 
 /**
  * @fileoverview added by tsickle
