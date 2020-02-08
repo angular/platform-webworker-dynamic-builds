c72da540c3bb39a3727d53e9112286bb5354945c
Kara Erickson <karakara@google.com>
2020-02-08 01:43:36 +0000

    480a4c3061 fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+36.sha-18ed9dc
+ * @license Angular v9.0.0+37.sha-480a4c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0+36.sha-18ed9dc');
+var VERSION = new Version('9.0.0+37.sha-480a4c3');
 
 /**
  * @license
