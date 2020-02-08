7d3e4df62608f9dfb990bff1cf4ee5f2e12050e1
Kara Erickson <karakara@google.com>
2020-02-08 00:38:47 +0000

    18ed9dcd83 refactor(benchpress): delete outdated/unused folder (#35147)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+32.sha-5b5dc81
+ * @license Angular v9.0.0+36.sha-18ed9dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0+32.sha-5b5dc81');
+var VERSION = new Version('9.0.0+36.sha-18ed9dc');
 
 /**
  * @license
