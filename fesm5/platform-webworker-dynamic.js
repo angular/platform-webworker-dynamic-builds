72949ece9e3b1874c27c507cb0d2b709e04f6191
Kara Erickson <karakara@google.com>
2020-02-10 17:43:00 +0000

    bfcf6d09e1 fix(docs-infra): lighthouse reporting links to cross-origin destinations are unsafe (#35253)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+37.sha-480a4c3
+ * @license Angular v9.0.0+40.sha-bfcf6d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0+37.sha-480a4c3');
+var VERSION = new Version('9.0.0+40.sha-bfcf6d0');
 
 /**
  * @license
