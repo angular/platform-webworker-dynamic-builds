d124a6d4c592c8f2ad462ab17f5f2be606e077e1
Kara Erickson <karakara@google.com>
2020-02-11 20:03:40 +0000

    332937ef24 feat: make mocha a zone module. (#34719)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+983.sha-81241af
+ * @license Angular v9.0.0-rc.1+984.sha-332937e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+983.sha-81241af');
+const VERSION = new Version('9.0.0-rc.1+984.sha-332937e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+983.sha-81241af
+ * @license Angular v9.0.0-rc.1+984.sha-332937e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+983.sha-81241af');
+var VERSION = new Version('9.0.0-rc.1+984.sha-332937e');
 
 /**
  * @license
