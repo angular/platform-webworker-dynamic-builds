ed6e7aa933de738cf8e39e9128b5f2e0702a8e01
Kara Erickson <karakara@google.com>
2020-02-13 01:09:28 +0000

    e8ea3be843 docs: remove service from region where it was added before it was created (#35354)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
+ * @license Angular v9.1.0-next.0+6.sha-e8ea3be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+991.sha-4aeb6c4');
+const VERSION = new Version('9.1.0-next.0+6.sha-e8ea3be');
 
 /**
  * @fileoverview added by tsickle
