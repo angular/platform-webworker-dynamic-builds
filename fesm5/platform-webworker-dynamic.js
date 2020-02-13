b01f7a589a23f2cd61264f97892429c2db856228
Kara Erickson <karakara@google.com>
2020-02-13 18:36:17 +0000

    84c9c6ecc2 docs: minor typo fix (#35423) PR Close #35423

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+7.sha-a30fd29
+ * @license Angular v9.0.1+16.sha-84c9c6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.1+7.sha-a30fd29');
+var VERSION = new Version('9.0.1+16.sha-84c9c6e');
 
 /**
  * @license
