789a2fac5df26cb15b1d5e5282f5dfd5c014cf4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-13 21:55:00 +0000

    db693f482f docs: Fix minor typos and coding styles (#35325)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+16.sha-84c9c6e
+ * @license Angular v9.0.1+17.sha-db693f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.1+16.sha-84c9c6e');
+var VERSION = new Version('9.0.1+17.sha-db693f4');
 
 /**
  * @license
