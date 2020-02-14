9262d9bb72ba45a09881037dc142f5b8c3518d7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:46:07 +0000

    be5e75c804 docs: change `item` to `items` to match the code (#35433)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+23.sha-2ca7984');
+const VERSION = new Version('9.1.0-next.0+24.sha-be5e75c');
 
 /**
  * @fileoverview added by tsickle
