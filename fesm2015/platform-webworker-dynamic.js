4b5b8531def6d6021afe92e2558b2282c2b14507
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 22:19:05 +0000

    183a8629a4 fix(docs-infra): set `th`/`td` to proper width (#35437)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+36.sha-2f18e85
+ * @license Angular v9.1.0-next.0+37.sha-183a862
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+36.sha-2f18e85');
+const VERSION = new Version('9.1.0-next.0+37.sha-183a862');
 
 /**
  * @fileoverview added by tsickle
