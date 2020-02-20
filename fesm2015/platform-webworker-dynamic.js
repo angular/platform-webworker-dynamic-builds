2a9b8f05f99e048a4b88c71060f2ba1536b4245c
Miško Hevery <misko@hevery.com>
2020-02-20 19:16:47 +0000

    1b72fc10eb docs: edit displaying-data page to introduce section (#35507)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+1.sha-8e12707
+ * @license Angular v9.1.0-next.1+5.sha-1b72fc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+1.sha-8e12707');
+const VERSION = new Version('9.1.0-next.1+5.sha-1b72fc1');
 
 /**
  * @fileoverview added by tsickle
