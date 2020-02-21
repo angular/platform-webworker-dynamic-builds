3b7395a8c5a48f646f6e0e1a2b041e8b57c2117c
Miško Hevery <misko@hevery.com>
2020-02-21 17:37:09 +0000

    54fd33fb80 fix(language-service): infer context type of structural directives (#35537) (#35561)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+22.sha-8cb1f65
+ * @license Angular v9.1.0-next.1+27.sha-54fd33f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+22.sha-8cb1f65');
+const VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
 
 /**
  * @fileoverview added by tsickle
