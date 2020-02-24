64389eaa2762f4d4bc53407ebdf62ad0404a3fc3
Miško Hevery <misko@hevery.com>
2020-02-24 17:32:51 +0000

    71b5970450 fix(ngcc): capture path-mapped entry-points that start with same string (#35592)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+43.sha-a7d5c55
+ * @license Angular v9.1.0-next.1+45.sha-71b5970
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+43.sha-a7d5c55');
+const VERSION = new Version('9.1.0-next.1+45.sha-71b5970');
 
 /**
  * @fileoverview added by tsickle
