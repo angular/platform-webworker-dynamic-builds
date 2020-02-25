4331d00159e219c7536c2ed4ab9ef0a73bf5d421
Miško Hevery <misko@hevery.com>
2020-02-25 21:39:54 +0000

    1e20b2ca36 build(packaging): add repository.directory field to package.jsons (#27544)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+58.sha-0a1a989
+ * @license Angular v9.1.0-next.1+61.sha-1e20b2c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+58.sha-0a1a989');
+var VERSION = new Version('9.1.0-next.1+61.sha-1e20b2c');
 
 /**
  * @license
