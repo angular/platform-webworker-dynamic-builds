ede2c73e1ff3f7cc7686f73b40bf1ff82baa43ac
Miško Hevery <misko@hevery.com>
2020-02-25 21:47:58 +0000

    835618c678 fix(ivy): error when accessing NgModuleRef.componentFactoryResolver in constructor (#35637)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+59.sha-8e354da
+ * @license Angular v9.1.0-next.1+62.sha-835618c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+59.sha-8e354da');
+var VERSION = new Version('9.1.0-next.1+62.sha-835618c');
 
 /**
  * @license
