f197b1c16597e2b28d7c5ae5afba6a8c0e63ce61
Miško Hevery <misko@hevery.com>
2020-02-26 21:13:11 +0000

    8fbb966ca7 docs(core): describe your change... (#34513)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+64.sha-6f68eae
+ * @license Angular v9.1.0-next.1+65.sha-8fbb966
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+64.sha-6f68eae');
+var VERSION = new Version('9.1.0-next.1+65.sha-8fbb966');
 
 /**
  * @license
