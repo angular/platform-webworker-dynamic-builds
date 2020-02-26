5f07ac54ec2ea5e344806be13098f692cbf8f57e
Miško Hevery <misko@hevery.com>
2020-02-26 21:00:43 +0000

    bde4cd7982 docs: add documentation of NgZone with zone.js (#34295)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+62.sha-835618c
+ * @license Angular v9.1.0-next.1+63.sha-bde4cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+62.sha-835618c');
+const VERSION = new Version('9.1.0-next.1+63.sha-bde4cd7');
 
 /**
  * @fileoverview added by tsickle
