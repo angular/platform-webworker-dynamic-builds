541066a0ce4f28952225498c758b032ebe3f89f1
Miško Hevery <misko@hevery.com>
2020-02-26 17:28:13 +0000

    406ce8c884 refactor(platform-browser): Hoist functions to workaround optimization bug. (#32230)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+54.sha-d690488
+ * @license Angular v9.0.2+55.sha-406ce8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.2+54.sha-d690488');
+var VERSION = new Version('9.0.2+55.sha-406ce8c');
 
 /**
  * @license
