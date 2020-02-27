83d2ec5185fcea2b993e7f442e8a4fde5c3b275b
atscott <atscott@google.com>
2020-02-27 19:17:13 +0000

    20b0c80b0b fix(ngcc): allow deep-import warnings to be ignored (#35683)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+4.sha-30f29ee
+ * @license Angular v9.1.0-next.2+3.sha-20b0c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+4.sha-30f29ee');
+const VERSION = new Version('9.1.0-next.2+3.sha-20b0c80');
 
 /**
  * @fileoverview added by tsickle
