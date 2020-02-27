26d7e72f941cb2e86834db5494318d0ee818f2da
atscott <atscott@google.com>
2020-02-27 00:56:51 +0000

    91934ebb5c ci: increase AIO payload size limit (#35702)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+72.sha-975a11b
+ * @license Angular v9.1.0-next.1+85.sha-91934eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+72.sha-975a11b');
+const VERSION = new Version('9.1.0-next.1+85.sha-91934eb');
 
 /**
  * @fileoverview added by tsickle
