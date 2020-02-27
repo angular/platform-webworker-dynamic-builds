3e6e696ad5593033955b18592eb47d31e574a3f0
atscott <atscott@google.com>
2020-02-27 22:23:13 +0000

    8fed1fe792 perf(core): adding micro benchmark for host bindings (#35705)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+10.sha-bc7b589
+ * @license Angular v9.1.0-next.2+11.sha-8fed1fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+10.sha-bc7b589');
+const VERSION = new Version('9.1.0-next.2+11.sha-8fed1fe');
 
 /**
  * @fileoverview added by tsickle
