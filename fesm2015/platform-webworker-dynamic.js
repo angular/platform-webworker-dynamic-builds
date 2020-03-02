f3f013123ce0056fdd35949c69939fe658839c0f
atscott <atscott@google.com>
2020-03-02 16:53:25 +0000

    a153b61098 fix(core): treat `[class]` and `[className]` as unrelated bindings (#35668)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+33.sha-40da51f
+ * @license Angular v9.1.0-next.2+35.sha-a153b61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+33.sha-40da51f');
+const VERSION = new Version('9.1.0-next.2+35.sha-a153b61');
 
 /**
  * @fileoverview added by tsickle
