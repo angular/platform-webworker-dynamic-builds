011d6f3efe4f5fd08ac32e4bf6bddb4ecfdb5a9d
atscott <atscott@google.com>
2020-03-03 22:17:36 +0000

    ed44073a58 build(zone.js): sets rollup_bundle to use the --silent flag (#35835)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+58.sha-2c41bb8
+ * @license Angular v9.1.0-next.2+59.sha-ed44073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+58.sha-2c41bb8');
+var VERSION = new Version('9.1.0-next.2+59.sha-ed44073');
 
 /**
  * @license
