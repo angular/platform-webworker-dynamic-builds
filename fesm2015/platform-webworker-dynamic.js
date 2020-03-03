23025fa95c4bb67334094a2da581d238f0b75369
atscott <atscott@google.com>
2020-03-03 17:29:58 +0000

    f13ee3e692 build: update to rules_nodejs 1.4.0 (#35813)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+51.sha-89b5b97
+ * @license Angular v9.1.0-next.2+49.sha-f13ee3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+51.sha-89b5b97');
+const VERSION = new Version('9.1.0-next.2+49.sha-f13ee3e');
 
 /**
  * @fileoverview added by tsickle
