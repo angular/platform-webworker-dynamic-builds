29a09a7f6ab393fdd087da339be7167cb67a5517
atscott <atscott@google.com>
2020-03-03 17:46:13 +0000

    93fc392549 build: fix flakiness in integration/bazel-schematics by disabling symlinked_node_modules (#35808)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+50.sha-3b0b905
+ * @license Angular v9.1.0-next.2+52.sha-93fc392
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+50.sha-3b0b905');
+const VERSION = new Version('9.1.0-next.2+52.sha-93fc392');
 
 /**
  * @fileoverview added by tsickle
