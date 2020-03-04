18e3013107e101c31eac60bad7ad2d7d0d804cb1
atscott <atscott@google.com>
2020-03-04 00:30:28 +0000

    d2d4225ce6 Revert "fix(core): log error instead of warning for unknown properties and elements (#35798)" (#35845)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+44.sha-218e82e
+ * @license Angular v9.0.4+45.sha-d2d4225
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.4+44.sha-218e82e');
+const VERSION = new Version('9.0.4+45.sha-d2d4225');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+44.sha-218e82e
+ * @license Angular v9.0.4+45.sha-d2d4225
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.4+44.sha-218e82e');
+var VERSION = new Version('9.0.4+45.sha-d2d4225');
 
 /**
  * @license
