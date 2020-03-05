0c523348821698d837abd5a25bf9b96dd9a5abc0
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:29:27 +0000

    716d50aa21 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+76.sha-8ef29b6
+ * @license Angular v9.1.0-next.2+79.sha-716d50a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+76.sha-8ef29b6');
+const VERSION = new Version('9.1.0-next.2+79.sha-716d50a');
 
 /**
  * @fileoverview added by tsickle
