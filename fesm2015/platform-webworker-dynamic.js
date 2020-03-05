a24a571ae5a0a1d6a0aa6899ffa3459d4d7819c3
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 20:57:25 +0000

    a1d0396c55 build: fix integration tests flakes using local yarn cache for bazel-schematics & ng_elements_schematics demos (#35877)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+74.sha-de67978
+ * @license Angular v9.1.0-next.2+75.sha-a1d0396
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+74.sha-de67978');
+const VERSION = new Version('9.1.0-next.2+75.sha-a1d0396');
 
 /**
  * @fileoverview added by tsickle
