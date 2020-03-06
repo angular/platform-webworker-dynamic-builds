549f5f77d2f0aa00fcfc44806f34fb606490115b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:05:35 +0000

    59078c444c docs: move ngIndia 2020 to past events on events page (#35901)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+91.sha-59607dc
+ * @license Angular v9.1.0-next.2+89.sha-59078c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+91.sha-59607dc');
+var VERSION = new Version('9.1.0-next.2+89.sha-59078c4');
 
 /**
  * @license
