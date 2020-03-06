814d49fb4f43414f5e9f1eb9f0b6693998caf97f
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:00:53 +0000

    44f9fa6ce5 docs: add missing closing bracket (#35890)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+85.sha-b42e2e0
+ * @license Angular v9.1.0-next.2+87.sha-44f9fa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+85.sha-b42e2e0');
+var VERSION = new Version('9.1.0-next.2+87.sha-44f9fa6');
 
 /**
  * @license
