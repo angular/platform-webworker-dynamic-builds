dae53613b21f023b06d03b6a2907061e9405034b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:59:57 +0000

    82a6fc5ef9 build(docs-infra): use local version of Zone.js when testing against local packages (#35858)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+99.sha-55b3f97
+ * @license Angular v9.1.0-next.2+96.sha-82a6fc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+99.sha-55b3f97');
+const VERSION = new Version('9.1.0-next.2+96.sha-82a6fc5');
 
 /**
  * @fileoverview added by tsickle
