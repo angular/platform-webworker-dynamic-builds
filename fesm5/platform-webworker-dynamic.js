5abfafba24262c9fe94b73bbbc7f2e1b5a6cd3db
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:42:57 +0000

    4cc519eb10 refactor(benchpress): remove benchmarks_external from ..pullapprove.yml (#35670)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+22.sha-9e23a69
+ * @license Angular v9.0.5+29.sha-4cc519e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.5+22.sha-9e23a69');
+var VERSION = new Version('9.0.5+29.sha-4cc519e');
 
 /**
  * @license
