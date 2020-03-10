33b3936b62c7acaeac411ea93986f3cc4fe713c0
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:15:33 +0000

    3d46a45fa8 fix(language-service): resolve the variable from the template context first (#35982)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+117.sha-81cb54f
+ * @license Angular v9.1.0-next.2+116.sha-3d46a45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+117.sha-81cb54f');
+const VERSION = new Version('9.1.0-next.2+116.sha-3d46a45');
 
 /**
  * @fileoverview added by tsickle
