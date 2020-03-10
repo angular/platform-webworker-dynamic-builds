82949884d9475383439ba97fd8c4a5250f618cde
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:13:43 +0000

    fc71032dc4 test(router): use pageYOffset in testing when scrollY is not available (#35976)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+117.sha-81cb54f
+ * @license Angular v9.1.0-next.2+118.sha-fc71032
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+117.sha-81cb54f');
+const VERSION = new Version('9.1.0-next.2+118.sha-fc71032');
 
 /**
  * @fileoverview added by tsickle
