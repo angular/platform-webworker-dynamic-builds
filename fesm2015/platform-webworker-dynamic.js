c5e97c5d21617e6e30299839b9c5a3ad3116fd5b
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:14 +0000

    e4b1e6c622 build: Add pullapprove verification tool to dev-infra-private package (#35911)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+122.sha-983f481
+ * @license Angular v9.1.0-next.2+127.sha-e4b1e6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+122.sha-983f481');
+const VERSION = new Version('9.1.0-next.2+127.sha-e4b1e6c');
 
 /**
  * @fileoverview added by tsickle
