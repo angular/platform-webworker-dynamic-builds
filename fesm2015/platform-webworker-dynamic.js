7c175401f0c08f6aec7545351153904b1b10de90
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:07:35 +0000

    a73e125c04 feat(dev-infra): add dev-infra to the commit message scopes (#35992)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+115.sha-406419b
+ * @license Angular v9.1.0-next.2+114.sha-a73e125
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+115.sha-406419b');
+const VERSION = new Version('9.1.0-next.2+114.sha-a73e125');
 
 /**
  * @fileoverview added by tsickle
