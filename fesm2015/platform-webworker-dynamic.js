541b1c19b41e80d09478baabc77968ff04056b85
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:40:09 +0000

    13495c64f7 docs(dev-infra): update triage and contributing docs for dev-infra (#35995)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+3.sha-6f95bc9
+ * @license Angular v9.1.0-next.4+7.sha-13495c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+3.sha-6f95bc9');
+const VERSION = new Version('9.1.0-next.4+7.sha-13495c6');
 
 /**
  * @fileoverview added by tsickle
