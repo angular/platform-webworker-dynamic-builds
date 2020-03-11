58a59b28a7b52f600fc206f5bdb53b2a03ef2c2b
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:08:25 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+34.sha-99ea5d7
+ * @license Angular v9.1.0-next.4+35.sha-2b6028b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+34.sha-99ea5d7');
+const VERSION = new Version('9.1.0-next.4+35.sha-2b6028b');
 
 /**
  * @fileoverview added by tsickle
