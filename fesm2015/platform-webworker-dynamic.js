91ff0a634477c32b60c553409dcc9d7d7f29126d
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:31:07 +0000

    8ea61a19cd feat(ngcc): support invalidating the entry-point manifest (#35931)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+28.sha-5af0e75
+ * @license Angular v9.1.0-next.4+33.sha-8ea61a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+28.sha-5af0e75');
+const VERSION = new Version('9.1.0-next.4+33.sha-8ea61a1');
 
 /**
  * @fileoverview added by tsickle
