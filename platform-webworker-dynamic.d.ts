17f1dfc908ac63b86fdeb799613835066690887a
Joey Perrott <josephperrott@gmail.com>
2020-03-11 17:39:23 +0000

    066be76a9a ci: prevent api golden tests on windows CI

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @license Angular v9.1.0-next.4+8.sha-066be76
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ */
+export declare const VERSION: Version;
+
+export { }
