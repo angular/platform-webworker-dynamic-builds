47a2fccc115d57dcee5876159825219026112aee
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:34:49 +0000

    13d85e10a2 docs: add developer survey 2020 (#35655)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+24.sha-7af0648
+ * @license Angular v9.0.6+25.sha-13d85e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.6+24.sha-7af0648');
+var VERSION = new Version('9.0.6+25.sha-13d85e1');
 
 /**
  * @license
