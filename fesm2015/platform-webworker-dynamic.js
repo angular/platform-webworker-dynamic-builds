5c2060b8e58bad55800f49a99c64ca74e53dea51
Andrew Kushnir <akushnir@google.com>
2020-03-13 19:56:36 +0000

    05d0586223 test(core): Update transplanted views benchpress test to support VE (#36058)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+51.sha-6b4c528
+ * @license Angular v9.1.0-next.4+52.sha-05d0586
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+51.sha-6b4c528');
+const VERSION = new Version('9.1.0-next.4+52.sha-05d0586');
 
 /**
  * @fileoverview added by tsickle
