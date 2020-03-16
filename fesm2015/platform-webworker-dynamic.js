ca6935885636a184ea7cd5a41dca8a607733a684
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:22:33 +0000

    0f8e710c7c feat(zone.js): Monkey patch MessagePort.prototype onproperties (#34610)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+54.sha-8456c5e
+ * @license Angular v9.1.0-next.4+53.sha-0f8e710
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+54.sha-8456c5e');
+const VERSION = new Version('9.1.0-next.4+53.sha-0f8e710');
 
 /**
  * @fileoverview added by tsickle
