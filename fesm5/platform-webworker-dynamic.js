0304fbbe8d648e0ff41f92a28362b98e72c7ce2a
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:21:53 +0000

    46fe8fb8b4 build: depend on bazelisk rather than directly on Bazel (#36078)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+59.sha-31bec8c
+ * @license Angular v9.1.0-next.4+60.sha-46fe8fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
+var VERSION = new Version('9.1.0-next.4+60.sha-46fe8fb');
 
 /**
  * @license
