25583038db6ecefc1ff8081d287189db87f6fe3a
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:30:48 +0000

    3fa895298d fix(zone.js): zone.js patches rxjs should check null for unsubscribe (#35990)

---
+++ fesm2015/platform-webworker-dynamic.js
@@ -0,0 +1,61 @@
+/**
+ * @license Angular v9.1.0-next.4+56.sha-3fa8952
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
+import { ResourceLoader } from '@angular/compiler';
+import { Version, createPlatformFactory, COMPILER_OPTIONS, PLATFORM_ID } from '@angular/core';
+import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-browser-dynamic';
+
+/**
+ * @fileoverview added by tsickle
+ * Generated from: packages/platform-webworker-dynamic/src/version.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * \@publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @type {?}
+ */
+const VERSION = new Version('9.1.0-next.4+56.sha-3fa8952');
+
+/**
+ * @fileoverview added by tsickle
+ * Generated from: packages/platform-webworker-dynamic/src/platform-webworker-dynamic.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+const ɵ0 = { providers: [{ provide: ResourceLoader, useClass: ɵResourceLoaderImpl, deps: [] }] }, ɵ1 = ɵPLATFORM_WORKER_UI_ID;
+/**
+ * \@publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @type {?}
+ */
+const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
+    {
+        provide: COMPILER_OPTIONS,
+        useValue: ɵ0,
+        multi: true
+    },
+    { provide: PLATFORM_ID, useValue: ɵ1 }
+]);
+
+/**
+ * @fileoverview added by tsickle
+ * Generated from: packages/platform-webworker-dynamic/public_api.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * Generated from: packages/platform-webworker-dynamic/index.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { VERSION, platformWorkerAppDynamic };
+//# sourceMappingURL=platform-webworker-dynamic.js.map
