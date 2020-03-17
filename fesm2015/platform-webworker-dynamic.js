521c638e4d9be24270cf20687ab556b892b5a0f4
Andrew Kushnir <akushnir@google.com>
2020-03-17 18:03:43 +0000

    afc9839f43 build(zone.js): Update `zone.js` release process to use `google-wombot` (#36110)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+67.sha-e3ecdc6
+ * @license Angular v9.1.0-next.4+68.sha-afc9839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+67.sha-e3ecdc6');
+const VERSION = new Version('9.1.0-next.4+68.sha-afc9839');
 
 /**
  * @fileoverview added by tsickle
