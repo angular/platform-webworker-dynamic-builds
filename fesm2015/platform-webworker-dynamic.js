ff48f25d2ed6b4954229a9c9f6159ef855a2f26b
Andrew Kushnir <akushnir@google.com>
2020-03-17 17:59:56 +0000

    35d94e9d08 build: fix ts-api-guardian golden approval not working on windows (#36096)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+44.sha-8da4adc
+ * @license Angular v9.0.6+46.sha-35d94e9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.6+44.sha-8da4adc');
+const VERSION = new Version('9.0.6+46.sha-35d94e9');
 
 /**
  * @fileoverview added by tsickle
