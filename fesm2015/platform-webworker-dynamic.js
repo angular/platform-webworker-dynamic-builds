01b5697291d8e79281afa4ebb20b59aa6dd3906b
Misko Hevery <misko@hevery.com>
2020-03-20 20:17:47 +0000

    ae3eaf8b16 test(compiler): remove whitespace in spans (#36169)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+6.sha-9126921
+ * @license Angular v9.1.0-rc.0+8.sha-ae3eaf8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+6.sha-9126921');
+const VERSION = new Version('9.1.0-rc.0+8.sha-ae3eaf8');
 
 /**
  * @fileoverview added by tsickle
