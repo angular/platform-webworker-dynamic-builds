0caf6ae1c6f2351312db311b4d5c0276c38db32e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:23:13 +0000

    1c385a1c22 build(docs-infra): do not include CI-specific config in docs examples ZIP archives (#36018)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
+const VERSION = new Version('9.1.0-rc.0+44.sha-1c385a1');
 
 /**
  * @fileoverview added by tsickle
