9a2ad49d97412e1f9e45877a74bc5148aab3b015
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:23:54 +0000

    5b6ced5599 build: update to rules_nodejs 1.5.0 (#36307)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+59.sha-a383116
+ * @license Angular v9.1.0-rc.0+60.sha-5b6ced5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+59.sha-a383116');
+const VERSION = new Version('9.1.0-rc.0+60.sha-5b6ced5');
 
 /**
  * @fileoverview added by tsickle
