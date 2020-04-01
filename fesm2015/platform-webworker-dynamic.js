0d359322817a59ac5c7abda0340a2230ef15f881
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:56:47 +0000

    cb0a2a055d fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+27.sha-beb3f19
+ * @license Angular v9.1.0+43.sha-cb0a2a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0+27.sha-beb3f19');
+const VERSION = new Version('9.1.0+43.sha-cb0a2a0');
 
 /**
  * @fileoverview added by tsickle
