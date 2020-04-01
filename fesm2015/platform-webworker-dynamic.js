e6e38433a3d68d040bce5ca7ba2a153d51958ecb
Kara Erickson <karakara@google.com>
2020-04-01 22:50:14 +0000

    24a92472bf ci: use dev-infra cli from local sources (#36326)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+87.sha-4d458db
+ * @license Angular v9.1.0-rc.0+86.sha-24a9247
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
+const VERSION = new Version('9.1.0-rc.0+86.sha-24a9247');
 
 /**
  * @fileoverview added by tsickle
