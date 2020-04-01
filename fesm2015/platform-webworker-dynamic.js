8c22ac542c3ac9bf4b22384de2e60912b8764c48
Kara Erickson <karakara@google.com>
2020-04-01 23:02:29 +0000

    a5eb0e56b6 docs(zone.js): fix typos and align formatting (#35907)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+86.sha-24a9247
+ * @license Angular v9.1.0-rc.0+88.sha-a5eb0e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+86.sha-24a9247');
+const VERSION = new Version('9.1.0-rc.0+88.sha-a5eb0e5');
 
 /**
  * @fileoverview added by tsickle
