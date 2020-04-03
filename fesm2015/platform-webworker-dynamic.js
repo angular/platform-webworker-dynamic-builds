4faaad739e561124a507e522cc55695ac5e82a5c
Kara Erickson <karakara@google.com>
2020-04-03 21:24:10 +0000

    bfa55162de docs: adding Doguhan Uluca as GDE resource (#34789)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+67.sha-776f991
+ * @license Angular v9.1.0+68.sha-bfa5516
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0+67.sha-776f991');
+const VERSION = new Version('9.1.0+68.sha-bfa5516');
 
 /**
  * @fileoverview added by tsickle
