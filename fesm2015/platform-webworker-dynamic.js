2a9b047537d70fa9b5410cead718da64f22447b3
Kara Erickson <karakara@google.com>
2020-04-07 17:53:02 +0000

    95fc3d4c5c fix(core): ngOnDestroy on multi providers called with incorrect context (#35840)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+136.sha-e145fa1
+ * @license Angular v9.1.0-rc.0+137.sha-95fc3d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+136.sha-e145fa1');
+const VERSION = new Version('9.1.0-rc.0+137.sha-95fc3d4');
 
 /**
  * @fileoverview added by tsickle
