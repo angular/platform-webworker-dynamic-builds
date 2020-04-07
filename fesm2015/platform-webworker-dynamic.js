811a8882cd97a6a5941f7b88ca953c9f58f0968b
Kara Erickson <karakara@google.com>
2020-04-07 22:34:14 +0000

    96a3de6364 style(docs-infra): removed extra , from _resources.scss file (#35935)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+144.sha-81195a2
+ * @license Angular v9.1.0-rc.0+143.sha-96a3de6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+144.sha-81195a2');
+const VERSION = new Version('9.1.0-rc.0+143.sha-96a3de6');
 
 /**
  * @fileoverview added by tsickle
