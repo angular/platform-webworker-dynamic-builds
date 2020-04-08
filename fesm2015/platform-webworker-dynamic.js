f76632fdd00a37c7e860e2af26765b31b5d42e0e
Kara Erickson <karakara@google.com>
2020-04-08 00:00:02 +0000

    1beccc1a6c docs: release notes for the v9.1.1 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+149.sha-0075017
+ * @license Angular v9.1.0-rc.0+150.sha-1beccc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+149.sha-0075017');
+const VERSION = new Version('9.1.0-rc.0+150.sha-1beccc1');
 
 /**
  * @fileoverview added by tsickle
