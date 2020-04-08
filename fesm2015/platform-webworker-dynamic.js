676a5ad25ba2ccf22be41a2217c74d92c9e03446
atscott <atscott@google.com>
2020-04-08 22:54:12 +0000

    e92fce1c27 fix(language-service): remove circular dependency instance (#36463)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+3.sha-83a9159
+ * @license Angular v10.0.0-next.1+4.sha-e92fce1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+3.sha-83a9159');
+const VERSION = new Version('10.0.0-next.1+4.sha-e92fce1');
 
 /**
  * @fileoverview added by tsickle
