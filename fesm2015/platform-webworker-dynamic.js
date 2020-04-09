9adfa51b233bc03e6014e92891ca6f41a162bb60
atscott <atscott@google.com>
2020-04-09 18:50:52 +0000

    078b0be4dc fix(common): `locales/global/*.js` are not ES5 compliant (#36342)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+11.sha-99b93e5
+ * @license Angular v10.0.0-next.1+13.sha-078b0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+11.sha-99b93e5');
+const VERSION = new Version('10.0.0-next.1+13.sha-078b0be');
 
 /**
  * @fileoverview added by tsickle
