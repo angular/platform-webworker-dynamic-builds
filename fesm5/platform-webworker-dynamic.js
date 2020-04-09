2f38b319b69471aa196f112d7eef12c19f0b5dfc
atscott <atscott@google.com>
2020-04-09 20:57:59 +0000

    58d028178f fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+25.sha-e06512b
+ * @license Angular v9.1.1+26.sha-58d0281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.1+25.sha-e06512b');
+var VERSION = new Version('9.1.1+26.sha-58d0281');
 
 /**
  * @license
