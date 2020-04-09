dc6fb75fd533dce7c9fd56908235d48c90fc4071
atscott <atscott@google.com>
2020-04-09 23:52:10 +0000

    fee316161d build: update `REQUIRED_BASE_SHA` in merge script to clang 1.4.0 upgrade commit (#36547)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+18.sha-2e4244d
+ * @license Angular v10.0.0-next.1+19.sha-fee3161
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+18.sha-2e4244d');
+const VERSION = new Version('10.0.0-next.1+19.sha-fee3161');
 
 /**
  * @fileoverview added by tsickle
