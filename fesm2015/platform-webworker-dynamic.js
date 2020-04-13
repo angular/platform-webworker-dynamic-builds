68c5342c16af39b40632680520733690366f538b
atscott <atscott@google.com>
2020-04-13 15:49:24 +0000

    2d16b4711e fix(docs-infra): fix About page button text being truncated on small screens (#36576)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+25.sha-d972821
+ * @license Angular v10.0.0-next.1+24.sha-2d16b47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+25.sha-d972821');
+const VERSION = new Version('10.0.0-next.1+24.sha-2d16b47');
 
 /**
  * @fileoverview added by tsickle
