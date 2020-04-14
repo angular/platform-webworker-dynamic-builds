63726e99ac2bac5494b9996cd9a7b90a20aa8755
atscott <atscott@google.com>
2020-04-14 00:56:06 +0000

    c8f2ca2349 docs: edit to setup-local (#36168)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+34.sha-fd76982
+ * @license Angular v9.1.1+36.sha-c8f2ca2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.1+34.sha-fd76982');
+var VERSION = new Version('9.1.1+36.sha-c8f2ca2');
 
 /**
  * @license
