2fd029996584ef4e9d7b8e057ffe69762815ad5a
atscott <atscott@google.com>
2020-04-14 17:37:20 +0000

    29122c518b style: lint (#36580)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
+const VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
 
 /**
  * @fileoverview added by tsickle
