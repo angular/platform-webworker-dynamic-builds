211b96f61ea1d1c52814431fc803037a623f8018
atscott <atscott@google.com>
2020-04-14 01:02:13 +0000

    0cc53fb398 fix(upgrade): update $locationShim to handle Location changes before initialization (#36498)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+28.sha-f88e635
+ * @license Angular v10.0.0-next.1+27.sha-0cc53fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+28.sha-f88e635');
+const VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
 
 /**
  * @fileoverview added by tsickle
