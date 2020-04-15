6e1e1fbc478da0d018315e05d932cf2397c5b172
atscott <atscott@google.com>
2020-04-15 16:44:46 +0000

    12266b2042 fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+42.sha-c5e7251
+ * @license Angular v9.1.1+45.sha-12266b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.1+42.sha-c5e7251');
+const VERSION = new Version('9.1.1+45.sha-12266b2');
 
 /**
  * @fileoverview added by tsickle
