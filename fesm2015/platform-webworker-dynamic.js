4f9cb7315e0dc2693ebb8e850fb6eeeb53df438e
atscott <atscott@google.com>
2020-04-16 17:56:27 +0000

    297b7da166 docs: add string-union type note (#35859)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+13.sha-88b0985
+ * @license Angular v10.0.0-next.2+17.sha-297b7da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+13.sha-88b0985');
+const VERSION = new Version('10.0.0-next.2+17.sha-297b7da');
 
 /**
  * @fileoverview added by tsickle
