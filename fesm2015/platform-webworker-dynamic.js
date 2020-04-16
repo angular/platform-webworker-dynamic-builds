0be0e3f37a0cef781b54f838eaa6d6934bc033a0
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:30:25 +0000

    8be0972836 fix(language-service): properly evaluate types in comparable expressions (#36529)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+27.sha-c332d4d
+ * @license Angular v10.0.0-next.2+29.sha-8be0972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+27.sha-c332d4d');
+const VERSION = new Version('10.0.0-next.2+29.sha-8be0972');
 
 /**
  * @fileoverview added by tsickle
