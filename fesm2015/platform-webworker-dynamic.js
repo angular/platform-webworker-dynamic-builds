c343805f2faa673312fbeb605f73dbd36e9e548e
atscott <atscott@google.com>
2020-04-16 17:08:21 +0000

    b1f1d3ffd2 fix(core): handle empty translations correctly (#36499)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
+ * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
+const VERSION = new Version('10.0.0-next.2+3.sha-b1f1d3f');
 
 /**
  * @fileoverview added by tsickle
