6a6bad2a0fa79ddc3fff17ab01a21135811756bd
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:36:44 +0000

    0fa5ac8d0d ci: remove reliance on Github API for CI setup (#36500)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+29.sha-f2fca3e
+ * @license Angular v9.1.2+30.sha-0fa5ac8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.2+29.sha-f2fca3e');
+var VERSION = new Version('9.1.2+30.sha-0fa5ac8');
 
 /**
  * @license
