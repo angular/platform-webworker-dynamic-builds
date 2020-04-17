7b30140284cf43ece0bd13fc5559fee095a6ad60
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:39:21 +0000

    81d23b33ef fix(core): pipes injecting viewProviders when used on a component host node (#36512)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+40.sha-bb150c2
+ * @license Angular v10.0.0-next.2+41.sha-81d23b3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+40.sha-bb150c2');
+const VERSION = new Version('10.0.0-next.2+41.sha-81d23b3');
 
 /**
  * @fileoverview added by tsickle
