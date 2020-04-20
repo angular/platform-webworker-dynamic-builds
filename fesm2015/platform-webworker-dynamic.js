89111e57a41638b2788ca932d055e2deeb6a3180
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:51:14 +0000

    4341743b4a fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+48.sha-b5e92b9
+ * @license Angular v10.0.0-next.2+49.sha-4341743
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+48.sha-b5e92b9');
+const VERSION = new Version('10.0.0-next.2+49.sha-4341743');
 
 /**
  * @fileoverview added by tsickle
