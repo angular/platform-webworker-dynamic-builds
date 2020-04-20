b0417dfc45795772edd64765d8732f53cb5d64cc
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:58:18 +0000

    f4ef8a92f8 docs: fix typo (#36665) PR Close #36665

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+45.sha-d7e9d87
+ * @license Angular v10.0.0-next.2+46.sha-f4ef8a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+45.sha-d7e9d87');
+const VERSION = new Version('10.0.0-next.2+46.sha-f4ef8a9');
 
 /**
  * @fileoverview added by tsickle
