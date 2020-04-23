7be981f06fc133e1e6c69f430e9efd395ff49783
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:37:12 +0000

    7743c43529 test(router): fix router test failing on IE (#36742)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.3+6.sha-d40bcce');
+var VERSION = new Version('9.1.3+9.sha-7743c43');
 
 /**
  * @license
