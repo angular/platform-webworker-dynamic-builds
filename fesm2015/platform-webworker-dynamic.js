574233349e1ea627567b6e2d6340f819a221d95f
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:35:56 +0000

    e5d2853070 docs: correct ngSwitch definition (#35489)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+13.sha-dd806b0
+ * @license Angular v10.0.0-next.3+16.sha-e5d2853
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+13.sha-dd806b0');
+const VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
 
 /**
  * @fileoverview added by tsickle
