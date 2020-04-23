acef74a66c21b5de06015db654942c2a9a662d22
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:57:46 +0000

    d840503d35 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+15.sha-f898c9a
+ * @license Angular v9.1.3+18.sha-d840503
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.3+15.sha-f898c9a');
+var VERSION = new Version('9.1.3+18.sha-d840503');
 
 /**
  * @license
