201c6fd2b2fe17a013563df6fd19831d6e332c62
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:27:38 +0000

    dd049caf0a fix(language-service): disable update the `@angular/core` module (#36783)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+24.sha-e6161ca
+ * @license Angular v10.0.0-next.3+26.sha-dd049ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+24.sha-e6161ca');
+const VERSION = new Version('10.0.0-next.3+26.sha-dd049ca');
 
 /**
  * @fileoverview added by tsickle
