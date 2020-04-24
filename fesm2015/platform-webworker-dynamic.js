7173b44d5854977d9cc75163c9afed806130229c
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:26:48 +0000

    bcd31cb857 build(docs-infra): upgrade cli command docs sources to 730657c86 (#36790)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+26.sha-dd049ca
+ * @license Angular v10.0.0-next.3+25.sha-bcd31cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+26.sha-dd049ca');
+const VERSION = new Version('10.0.0-next.3+25.sha-bcd31cb');
 
 /**
  * @fileoverview added by tsickle
