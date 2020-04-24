5666387f3c322a53f237cf81f47b258b859a8926
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:28:25 +0000

    76190c71fc ci: migrate payload size tracking goldens to the golden directory (#36455)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+25.sha-bcd31cb
+ * @license Angular v10.0.0-next.3+27.sha-76190c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+25.sha-bcd31cb');
+const VERSION = new Version('10.0.0-next.3+27.sha-76190c7');
 
 /**
  * @fileoverview added by tsickle
