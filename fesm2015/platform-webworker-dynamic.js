4c49fef0f2a7244fd1df5a4213ec50c38bf8ab64
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:25:07 +0000

    89c589085d fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+44.sha-a6a7e1b
+ * @license Angular v10.0.0-next.3+45.sha-89c5890
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+44.sha-a6a7e1b');
+const VERSION = new Version('10.0.0-next.3+45.sha-89c5890');
 
 /**
  * @fileoverview added by tsickle
