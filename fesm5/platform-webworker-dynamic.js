edec54794225c8a2e475b05d3d25565c7bc1737d
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:42:51 +0000

    0ce96f1d78 refactor(localize): remove unused code (#36834)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+28.sha-0f389fa
+ * @license Angular v9.1.3+34.sha-0ce96f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.3+28.sha-0f389fa');
+var VERSION = new Version('9.1.3+34.sha-0ce96f1');
 
 /**
  * @license
