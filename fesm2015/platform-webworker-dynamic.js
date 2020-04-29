cf78538c4b17887fea49afc5920a7ddbadb76051
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:59:06 +0000

    1786586747 fix(core): Refresh transplanted views at insertion point only (#35968)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+13.sha-45c0941
+ * @license Angular v10.0.0-next.4+14.sha-1786586
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
+const VERSION = new Version('10.0.0-next.4+14.sha-1786586');
 
 /**
  * @fileoverview added by tsickle
