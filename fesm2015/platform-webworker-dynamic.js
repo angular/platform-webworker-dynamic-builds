871b56f9927c032810375f2181ce9c994ba967f4
Andrew Kushnir <akushnir@google.com>
2020-04-29 20:28:15 +0000

    cebd4fada7 build: use sh instead of exec for release scripts (#36862)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+46.sha-3d82aa7
+ * @license Angular v10.0.0-next.3+47.sha-cebd4fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+46.sha-3d82aa7');
+const VERSION = new Version('10.0.0-next.3+47.sha-cebd4fa');
 
 /**
  * @fileoverview added by tsickle
