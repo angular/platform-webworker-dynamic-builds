693e87bbf9731b850602a524fa349d536b129a0b
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:39:35 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+48.sha-c89421b');
+const VERSION = new Version('10.0.0-next.4');
 
 /**
  * @fileoverview added by tsickle
