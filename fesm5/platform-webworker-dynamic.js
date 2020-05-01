71b270d8c41791b35ee87fb6c8e43fffbab52717
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:25:51 +0000

    e9300c979e build: use @angular/dev-infra-private for bazel stamping (#36844)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+30.sha-95a407d
+ * @license Angular v10.0.0-next.4+29.sha-e9300c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+30.sha-95a407d');
+var VERSION = new Version('10.0.0-next.4+29.sha-e9300c9');
 
 /**
  * @license
