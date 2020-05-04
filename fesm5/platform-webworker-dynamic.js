a506671c4132f3f27ddaf3855f15760a63882b31
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:08:13 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+var VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
 /**
  * @license
