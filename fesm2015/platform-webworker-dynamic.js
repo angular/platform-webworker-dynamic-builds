1e14fb366cf2b2cb0d61657320b537d11cfd5321
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:00:47 +0000

    d9c4840a9c fix(router): cancel navigation when at least one resolver completes with no "next" emission (#24621)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+29.sha-e9300c9
+ * @license Angular v10.0.0-next.4+32.sha-d9c4840
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+29.sha-e9300c9');
+const VERSION = new Version('10.0.0-next.4+32.sha-d9c4840');
 
 /**
  * @fileoverview added by tsickle
