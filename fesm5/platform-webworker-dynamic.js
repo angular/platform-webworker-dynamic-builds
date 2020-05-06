b2e7e40349ed2c448aeb0190f8e56a8a46c34ecc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:04:00 +0000

    9d13ee0052 fix(ngcc): support ModuleWithProviders functions that delegate (#36948)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+55.sha-0be3792
+ * @license Angular v9.1.4+57.sha-9d13ee0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.4+55.sha-0be3792');
+var VERSION = new Version('9.1.4+57.sha-9d13ee0');
 
 /**
  * @license
