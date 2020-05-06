e21e32cd2fdd41982384723c85237174ad1997b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:12:36 +0000

    da79e0433f test(language-service): [ivy] Add mock service to overwrite files (#36923)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+37.sha-1b8752e
+ * @license Angular v10.0.0-next.5+38.sha-da79e04
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+37.sha-1b8752e');
+const VERSION = new Version('10.0.0-next.5+38.sha-da79e04');
 
 /**
  * @fileoverview added by tsickle
