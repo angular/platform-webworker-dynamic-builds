d2631790b199ea9ce7e5f43d92dd6921bf84f873
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:09:33 +0000

    42d1091d6a fix(compiler-cli): don't try to tag non-ts files as shims (#36987)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
+const VERSION = new Version('10.0.0-next.5+74.sha-42d1091');
 
 /**
  * @fileoverview added by tsickle
