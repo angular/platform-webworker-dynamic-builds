3b8a6d8d98244c97b7f7f7506d9290490e362077
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:54:21 +0000

    dcf1dcb757 test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36968)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+57.sha-9d13ee0
+ * @license Angular v9.1.4+67.sha-dcf1dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.4+57.sha-9d13ee0');
+var VERSION = new Version('9.1.4+67.sha-dcf1dcb');
 
 /**
  * @license
