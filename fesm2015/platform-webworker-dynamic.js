cd2d0551782f8ad59892b772eba20427cd14ce66
Misko Hevery <misko@hevery.com>
2020-05-08 22:01:05 +0000

    c184de175d build: migrate to typescript file for ng-dev config (#37017)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+8.sha-de0c71c
+ * @license Angular v10.0.0-next.6+10.sha-c184de1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
+const VERSION = new Version('10.0.0-next.6+10.sha-c184de1');
 
 /**
  * @fileoverview added by tsickle
