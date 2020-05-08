aff6a71ce6a5c560db18528a9e7435b8e9dcd8aa
Misko Hevery <misko@hevery.com>
2020-05-08 22:04:51 +0000

    1142c378fd feat(language-service): [ivy] wrap ngtsc to handle typecheck files (#36930)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+14.sha-82a3bd5
+ * @license Angular v10.0.0-next.6+15.sha-1142c37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+14.sha-82a3bd5');
+const VERSION = new Version('10.0.0-next.6+15.sha-1142c37');
 
 /**
  * @fileoverview added by tsickle
