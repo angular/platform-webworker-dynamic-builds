061d89197e21593fab15ce22e6ddb5849c6cd154
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:07:06 +0000

    178e750f16 docs: Fix typo in navigation.json (#36955)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+5.sha-14be55c
+ * @license Angular v10.0.0-next.6+6.sha-178e750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+5.sha-14be55c');
+const VERSION = new Version('10.0.0-next.6+6.sha-178e750');
 
 /**
  * @fileoverview added by tsickle
