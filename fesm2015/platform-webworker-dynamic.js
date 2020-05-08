a0f89d3f5a53183536eb0cb2864f58cadca25b59
Misko Hevery <misko@hevery.com>
2020-05-08 22:04:35 +0000

    df71c7c617 build(docs-infra): upgrade cli command docs sources to 15edb0231 (#37005)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+15.sha-1142c37
+ * @license Angular v10.0.0-next.6+11.sha-df71c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+15.sha-1142c37');
+const VERSION = new Version('10.0.0-next.6+11.sha-df71c7c');
 
 /**
  * @fileoverview added by tsickle
