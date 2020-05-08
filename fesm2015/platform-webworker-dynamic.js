11af2aeb7990243c3208dd005543a57775c31924
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 19:00:02 +0000

    de0c71c467 docs: release notes for the v9.1.6 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+6.sha-178e750
+ * @license Angular v10.0.0-next.6+8.sha-de0c71c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+6.sha-178e750');
+const VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
 
 /**
  * @fileoverview added by tsickle
