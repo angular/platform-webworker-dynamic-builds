80f92db9b27e9adb07c8ef08a55b5d5fc7eb43e9
Misko Hevery <misko@hevery.com>
2020-05-11 18:31:15 +0000

    40025f55ad refactor(language-service): move TS utils to separate file (#36984)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+23.sha-286fbf4
+ * @license Angular v10.0.0-next.6+22.sha-40025f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+23.sha-286fbf4');
+const VERSION = new Version('10.0.0-next.6+22.sha-40025f5');
 
 /**
  * @fileoverview added by tsickle
