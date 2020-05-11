5c7a538f5ed379d8f4209dfbea1a09b83648a2a0
Misko Hevery <misko@hevery.com>
2020-05-11 18:17:53 +0000

    28f3c1c96a refactor(core): rename `instructions/container.ts` -> `instructions/template.ts` (#34715)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+18.sha-ed1b4a8
+ * @license Angular v10.0.0-next.6+20.sha-28f3c1c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+18.sha-ed1b4a8');
+const VERSION = new Version('10.0.0-next.6+20.sha-28f3c1c');
 
 /**
  * @fileoverview added by tsickle
