96f86a87a902a99dd1adb426441f6025b75b1819
Misko Hevery <misko@hevery.com>
2020-05-12 22:04:22 +0000

    2cddeabce0 docs: fixes `ngOnit` routing variable (#36905)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+33.sha-4f3ac1d
+ * @license Angular v10.0.0-next.6+31.sha-2cddeab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16,11 +16,10 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  */
 /**
  * \@publicApi
- * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
- *     of Angular
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+33.sha-4f3ac1d');
+const VERSION = new Version('10.0.0-next.6+31.sha-2cddeab');
 
 /**
  * @fileoverview added by tsickle
@@ -29,8 +28,7 @@ const VERSION = new Version('10.0.0-next.6+33.sha-4f3ac1d');
  */
 /**
  * \@publicApi
- * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
- *     of Angular
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
 const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
--- src/platform-webworker-dynamic.d.ts
+++ src/platform-webworker-dynamic.d.ts
@@ -9,7 +9,6 @@ import { PlatformRef, StaticProvider } from '@angular/core';
 export { VERSION } from './version';
 /**
  * @publicApi
- * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
- *     of Angular
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
 export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
