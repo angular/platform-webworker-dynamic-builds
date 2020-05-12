ff814cf56750954bafcac0873a02c97b8d1e6ccd
Misko Hevery <misko@hevery.com>
2020-05-12 22:03:48 +0000

    4f3ac1d983 docs(common): fix CLDR website link in FormStyle doc (#37069)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+30.sha-7addc9f
+ * @license Angular v10.0.0-next.6+33.sha-4f3ac1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19,9 +19,10 @@
      */
     /**
      * @publicApi
-     * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+     * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+     *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.6+30.sha-7addc9f');
+    var VERSION = new core.Version('10.0.0-next.6+33.sha-4f3ac1d');
 
     /**
      * @license
@@ -32,7 +33,8 @@
      */
     /**
      * @publicApi
-     * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+     * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+     *     of Angular
      */
     var platformWorkerAppDynamic = core.createPlatformFactory(platformBrowserDynamic.ɵplatformCoreDynamic, 'workerAppDynamic', [
         {
--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+30.sha-7addc9f
+ * @license Angular v10.0.0-next.6+33.sha-4f3ac1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16,10 +16,11 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  */
 /**
  * \@publicApi
- * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+30.sha-7addc9f');
+const VERSION = new Version('10.0.0-next.6+33.sha-4f3ac1d');
 
 /**
  * @fileoverview added by tsickle
@@ -28,7 +29,8 @@ const VERSION = new Version('10.0.0-next.6+30.sha-7addc9f');
  */
 /**
  * \@publicApi
- * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
  * @type {?}
  */
 const platformWorkerAppDynamic = createPlatformFactory(ɵplatformCoreDynamic, 'workerAppDynamic', [
