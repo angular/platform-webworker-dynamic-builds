238bcc21d36df81677bae64eae653ed444d73ddd
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:33 +0000

    ce39755937 docs: Remove duplicate `and` (#37067)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+14.sha-2cacab9
+ * @license Angular v10.0.0-next.7+10.sha-ce39755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+14.sha-2cacab9');
+const VERSION = new Version('10.0.0-next.7+10.sha-ce39755');
 
 /**
  * @fileoverview added by tsickle
