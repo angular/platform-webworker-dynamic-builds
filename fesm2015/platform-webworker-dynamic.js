153cd377089ff076ef6dfde355e42061ed9d0383
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:49 +0000

    9edea0bb75 feat(platform-server): use absolute URLs from Location for HTTP (#37071)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+10.sha-ce39755
+ * @license Angular v10.0.0-next.7+11.sha-9edea0bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+10.sha-ce39755');
+const VERSION = new Version('10.0.0-next.7+11.sha-9edea0bb');
 
 /**
  * @fileoverview added by tsickle
