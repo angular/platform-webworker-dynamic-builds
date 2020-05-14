b2e6e5673c07925957a3c563d39a8f6095b3a59a
Kara Erickson <karakara@google.com>
2020-05-14 23:11:12 +0000

    aaa89bb715 refactor(core): rename refreshDynamicEmbeddedViews to refreshEmbeddedViews (#37117)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+52.sha-ea971f7
+ * @license Angular v10.0.0-next.7+53.sha-aaa89bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+52.sha-ea971f7');
+const VERSION = new Version('10.0.0-next.7+53.sha-aaa89bb');
 
 /**
  * @fileoverview added by tsickle
