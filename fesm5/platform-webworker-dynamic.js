abe55c669fcd3cdfb5be978a1ecaf4a725b752f5
Kara Erickson <karakara@google.com>
2020-05-14 18:22:39 +0000

    ef9da2f619 refactor(dev-infra): add atscott for fw-core approvers (#37020)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+31.sha-92b97f7
+ * @license Angular v9.1.6+33.sha-ef9da2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.6+31.sha-92b97f7');
+var VERSION = new Version('9.1.6+33.sha-ef9da2f');
 
 /**
  * @license
