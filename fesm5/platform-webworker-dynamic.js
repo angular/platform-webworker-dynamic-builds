c2365f56b19a19050741d5878ba9492d7b133cdb
Kara Erickson <karakara@google.com>
2020-05-18 19:18:58 +0000

    ba9e63a278 fix(dev-infra): incorrect base revision for merge script autosquash (#37184)

--- fesm5/platform-webworker-dynamic.js
+++ fesm5/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+62.sha-6db8e7e
+ * @license Angular v9.1.6+67.sha-ba9e63a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.6+62.sha-6db8e7e');
+var VERSION = new Version('9.1.6+67.sha-ba9e63a');
 
 /**
  * @license
