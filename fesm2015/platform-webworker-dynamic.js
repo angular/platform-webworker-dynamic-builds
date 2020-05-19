f5d15ddbd76cfa3c004c4b989eb410fc311c78c5
Kara Erickson <karakara@google.com>
2020-05-19 22:29:14 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+28.sha-4e96cdc
+ * @license Angular v10.0.0-next.8+27.sha-772c5b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+28.sha-4e96cdc');
+const VERSION = new Version('10.0.0-next.8+27.sha-772c5b8');
 
 /**
  * @fileoverview added by tsickle
