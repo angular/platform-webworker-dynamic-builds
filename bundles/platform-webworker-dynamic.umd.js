257264b4f9d84f53aaa13b666cd2ab88a20ebd91
Kara Erickson <karakara@google.com>
2020-05-19 22:26:06 +0000

    4e96cdc23f build: Add deprecation notice to merge-pr script to nudge to new tooling (#37204)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+25.sha-deac994
+ * @license Angular v10.0.0-next.8+28.sha-4e96cdc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.8+25.sha-deac994');
+    var VERSION = new core.Version('10.0.0-next.8+28.sha-4e96cdc');
 
     /**
      * @license
