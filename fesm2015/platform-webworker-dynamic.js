e807ef83ab2f8cdbabfcf489e150db4874859e71
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 22:00:17 +0000

    540c29cd6b feat(dev-infra): migrate release tool to use new logging system (#37232)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+3.sha-93be1f1
+ * @license Angular v10.0.0-rc.0+14.sha-540c29c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+3.sha-93be1f1');
+const VERSION = new Version('10.0.0-rc.0+14.sha-540c29c');
 
 /**
  * @license
