68d3d4d4025cfb26f1aebede5cd665b70cab5229
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:38:38 +0000

    f001aef2dc build: update requiredBaseCommit for patch branch merges (#37316)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+22.sha-98d8200
+ * @license Angular v10.0.0-rc.0+21.sha-f001aef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+22.sha-98d8200');
+const VERSION = new Version('10.0.0-rc.0+21.sha-f001aef');
 
 /**
  * @license
