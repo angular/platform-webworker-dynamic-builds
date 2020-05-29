15e8b8b5bde7b04d9d170cc357f2bf434cea40ea
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 23:13:58 +0000

    d16a7f3ecc fix(core): reenable decorator downleveling for Angular npm packages (#37317)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+22.sha-82761ec
+ * @license Angular v10.0.0-rc.0+23.sha-d16a7f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+22.sha-82761ec');
+    var VERSION = new core.Version('10.0.0-rc.0+23.sha-d16a7f3');
 
     /**
      * @license
