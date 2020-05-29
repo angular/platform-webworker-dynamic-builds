604d129bc92b2fe72b081956967dce26ef339929
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:53:51 +0000

    82761ec50e docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+11.sha-270da1f
+ * @license Angular v10.0.0-rc.0+22.sha-82761ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+11.sha-270da1f');
+const VERSION = new Version('10.0.0-rc.0+22.sha-82761ec');
 
 /**
  * @license
