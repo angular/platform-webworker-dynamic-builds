f13ff348ef80b47b876da541ccb9ccc4e808c099
atscott <atscott@google.com>
2020-06-03 23:18:05 +0000

    2c1f35e794 fix(language-service): Recover from error in analyzing Ng Modules (#37108)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+22.sha-e35269d
+ * @license Angular v10.0.0-rc.2+24.sha-2c1f35e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+22.sha-e35269d');
+const VERSION = new Version('10.0.0-rc.2+24.sha-2c1f35e');
 
 /**
  * @license
