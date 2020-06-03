16bfbb501fd0af56816f1c292794464f8507a8dd
atscott <atscott@google.com>
2020-06-03 20:57:19 +0000

    60a03b7ef7 refactor(compiler-cli): extract NgCompilerAdapter interface (#37118)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+14.sha-c7c0c1f
+ * @license Angular v10.0.0-rc.2+21.sha-60a03b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+14.sha-c7c0c1f');
+const VERSION = new Version('10.0.0-rc.2+21.sha-60a03b7');
 
 /**
  * @license
