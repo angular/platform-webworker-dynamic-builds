6953c3b655e48a763e59e464b24914a65c019461
atscott <atscott@google.com>
2020-06-04 23:26:13 +0000

    eb6ba9ac80 docs: fix various typos (#37443)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+73.sha-4e7a454
+ * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+73.sha-4e7a454');
+const VERSION = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
 
 /**
  * @license
