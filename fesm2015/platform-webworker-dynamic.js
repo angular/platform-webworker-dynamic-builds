fcebf221ec5aef2a1e76e56a06fc45e32d8998dd
atscott <atscott@google.com>
2020-06-04 16:42:47 +0000

    2021ad12cf ci(docs-infra): skip deploying RC version when lexicographically smaller than stable (#37426)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
+ * @license Angular v10.0.0-rc.0+61.sha-2021ad1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+62.sha-6e7bd93');
+const VERSION = new Version('10.0.0-rc.0+61.sha-2021ad1');
 
 /**
  * @license
