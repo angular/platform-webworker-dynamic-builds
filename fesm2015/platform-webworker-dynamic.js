450bfa66569ea36b58865cc0be66488f082db70e
atscott <atscott@google.com>
2020-06-04 16:55:13 +0000

    5f0be3cb2e feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+24.sha-2c1f35e
+ * @license Angular v10.0.0-rc.2+43.sha-5f0be3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+24.sha-2c1f35e');
+const VERSION = new Version('10.0.0-rc.2+43.sha-5f0be3c');
 
 /**
  * @license
