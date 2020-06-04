947a60939513df7ce8db2dc776f938b30c6e9e97
atscott <atscott@google.com>
2020-06-04 18:05:38 +0000

    9ec25ea036 refactor(dev-infra): change required base commit sha (#37424)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+43.sha-5f0be3c
+ * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+43.sha-5f0be3c');
+const VERSION = new Version('10.0.0-rc.2+45.sha-9ec25ea');
 
 /**
  * @license
