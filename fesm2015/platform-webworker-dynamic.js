d5baa9107f885b3c242ddfa940e5aa881ceb4f00
atscott <atscott@google.com>
2020-06-08 16:51:21 +0000

    5e64c2b1df style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+86.sha-d887ba8
+ * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+86.sha-d887ba8');
+const VERSION = new Version('10.0.0-rc.0+89.sha-5e64c2b');
 
 /**
  * @license
