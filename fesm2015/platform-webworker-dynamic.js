31d78fc00fb9285bf7e142f1408f4481db575993
atscott <atscott@google.com>
2020-06-08 22:26:18 +0000

    a1616ce181 docs: add note on publishing libraries in ivy (#36556)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+70.sha-1c22dff
+ * @license Angular v10.0.0-rc.2+71.sha-a1616ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
+const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
 
 /**
  * @license
