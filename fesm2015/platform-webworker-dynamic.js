6ab701c904eaca325f7dfcb5594aa15ac538230d
atscott <atscott@google.com>
2020-06-08 18:48:00 +0000

    779344012a refactor(core): assert TNode is not a container when setting attribute on element (#37111)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+92.sha-650974e
+ * @license Angular v10.0.0-rc.0+93.sha-7793440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+92.sha-650974e');
+const VERSION = new Version('10.0.0-rc.0+93.sha-7793440');
 
 /**
  * @license
