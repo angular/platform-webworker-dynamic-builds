12025b70215bb3da7de4e4255bea4ae8506a26f3
atscott <atscott@google.com>
2020-06-09 20:55:44 +0000

    d494f7bd5e docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
+ * @license Angular v10.0.0-rc.3+12.sha-d494f7b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.3+11.sha-ec6a7ab');
+const VERSION = new Version('10.0.0-rc.3+12.sha-d494f7b');
 
 /**
  * @license
