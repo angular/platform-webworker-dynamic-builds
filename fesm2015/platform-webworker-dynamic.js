14a9d05016f8f46a15357d516fb171c02d02fd99
atscott <atscott@google.com>
2020-06-09 18:38:20 +0000

    cb1373106b docs: wrong links in lifecycle hooks api documentaion (#36557)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+108.sha-f6ee911
+ * @license Angular v10.0.0-rc.0+109.sha-cb13731
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+108.sha-f6ee911');
+const VERSION = new Version('10.0.0-rc.0+109.sha-cb13731');
 
 /**
  * @license
