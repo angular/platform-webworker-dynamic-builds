8ae54ebd6c9cbbbeb33bd77f4be2b59fdc85296e
atscott <atscott@google.com>
2020-06-09 22:12:05 +0000

    6114cd2bd4 perf(core): avoid pulling in jit-specific code in aot bundles (#37372) (#37514)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+12.sha-d494f7b
+ * @license Angular v10.0.0-rc.3+13.sha-6114cd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.3+12.sha-d494f7b');
+const VERSION = new Version('10.0.0-rc.3+13.sha-6114cd2');
 
 /**
  * @license
