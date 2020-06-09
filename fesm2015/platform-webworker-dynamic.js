3ccb016d2e6e49e8496a2a6718d179d31a5cabbc
atscott <atscott@google.com>
2020-06-09 00:48:51 +0000

    55979fe0ae feat(language-service): TS references from template items (#37437)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+100.sha-6280cf9
+ * @license Angular v10.0.0-rc.0+101.sha-55979fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+100.sha-6280cf9');
+const VERSION = new Version('10.0.0-rc.0+101.sha-55979fe');
 
 /**
  * @license
