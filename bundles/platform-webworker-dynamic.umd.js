1fb0b92a7e9c613961e4c16b2a0475136ab434b1
atscott <atscott01@gmail.com>
2020-06-10 17:51:56 +0000

    742f3d6787 Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+14.sha-9d397eb
+ * @license Angular v10.0.0-rc.3+16.sha-742f3d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.3+14.sha-9d397eb');
+    var VERSION = new core.Version('10.0.0-rc.3+16.sha-742f3d6');
 
     /**
      * @license
