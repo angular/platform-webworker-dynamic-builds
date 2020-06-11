c607c5fceb73e04a4614536196554ff22b640d0e
Misko Hevery <misko@hevery.com>
2020-06-11 18:39:20 +0000

    8c682c52b1 fix(ngcc): use annotateForClosureCompiler option (#36652)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+129.sha-e5b09cc
+ * @license Angular v10.0.0-rc.0+130.sha-8c682c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+129.sha-e5b09cc');
+const VERSION = new Version('10.0.0-rc.0+130.sha-8c682c5');
 
 /**
  * @license
