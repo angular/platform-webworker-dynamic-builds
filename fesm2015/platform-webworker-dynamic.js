f2ed43d151704832e0e440dd5caa2495c7be57ff
Misko Hevery <misko@hevery.com>
2020-06-15 17:01:25 +0000

    d330cd85ac refactor: move `hover` into a proper `@media` hover (#37320)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+172.sha-09d6dc5
+ * @license Angular v10.0.0-rc.0+173.sha-d330cd8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+172.sha-09d6dc5');
+const VERSION = new Version('10.0.0-rc.0+173.sha-d330cd8');
 
 /**
  * @license
