54fa70a6d290b61b7324a1f424ffa60ef207ad41
Misko Hevery <misko@hevery.com>
2020-06-15 17:42:59 +0000

    cc49a91de7 Revert "feat(bazel): expose explicit mapping from closure to devmode files (#36262)"

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+178.sha-300c2fe
+ * @license Angular v10.0.0-rc.0+179.sha-cc49a91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+178.sha-300c2fe');
+const VERSION = new Version('10.0.0-rc.0+179.sha-cc49a91');
 
 /**
  * @license
