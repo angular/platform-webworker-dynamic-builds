fc8d6785d5b56f0960ab5ae747309a86452fd6e1
Misko Hevery <misko@hevery.com>
2020-06-22 18:13:54 +0000

    7e0eccc4ef docs: Refactor-i18n (#36924)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+199.sha-2a970a0
+ * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+199.sha-2a970a0');
+const VERSION = new Version('10.0.0-rc.0+200.sha-7e0eccc');
 
 /**
  * @license
