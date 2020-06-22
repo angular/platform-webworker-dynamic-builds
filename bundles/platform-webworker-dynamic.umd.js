a560503aa71a5d2889066fec96947ac75847f407
Misko Hevery <misko@hevery.com>
2020-06-22 18:22:37 +0000

    fd65958b88 test: update symbol goldens to reflect optimized application (#37623)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
+ * @license Angular v10.0.0-rc.0+208.sha-fd65958
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+200.sha-7e0eccc');
+    var VERSION = new core.Version('10.0.0-rc.0+208.sha-fd65958');
 
     /**
      * @license
