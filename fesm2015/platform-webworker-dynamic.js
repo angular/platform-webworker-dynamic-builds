63eb79eccf3ae2030dc2f38af399bcd236e67bc9
Misko Hevery <misko@hevery.com>
2020-06-23 20:35:38 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+17.sha-82c8b44
+ * @license Angular v10.0.0-rc.6+20.sha-36a00a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.6+17.sha-82c8b44');
+const VERSION = new Version('10.0.0-rc.6+20.sha-36a00a2');
 
 /**
  * @license
