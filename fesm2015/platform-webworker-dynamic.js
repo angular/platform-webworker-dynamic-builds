6c0198a6c26a02158bd024b3cc03942dc5b3f241
Misko Hevery <misko@hevery.com>
2020-06-23 20:08:35 +0000

    e03adaf9b0 docs: add module/target compiler option migration docs (#37429)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+221.sha-38dfbc7
+ * @license Angular v10.0.0-rc.0+222.sha-e03adaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+221.sha-38dfbc7');
+const VERSION = new Version('10.0.0-rc.0+222.sha-e03adaf');
 
 /**
  * @license
