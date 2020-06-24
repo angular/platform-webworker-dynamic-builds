5d450b52323ddaaecff12852cfb3bcb1d1a89647
Misko Hevery <misko@hevery.com>
2020-06-24 16:58:59 +0000

    c49507b289 docs: updating guides and docs related to the v10 release (#37705)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+21.sha-27aa00b
+ * @license Angular v10.0.0-rc.6+23.sha-c49507b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.6+21.sha-27aa00b');
+const VERSION = new Version('10.0.0-rc.6+23.sha-c49507b');
 
 /**
  * @license
