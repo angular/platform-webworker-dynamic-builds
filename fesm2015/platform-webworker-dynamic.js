4ef122591c3b6bab215a7f28a276f6e7dafc34d5
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:38:21 +0000

    a5d5f67be7 fix(http): avoid abort a request when fetch operation is completed (#37367)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+4.sha-0d2cdf6
+ * @license Angular v10.0.0+15.sha-a5d5f67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0+4.sha-0d2cdf6');
+const VERSION = new Version('10.0.0+15.sha-a5d5f67');
 
 /**
  * @license
