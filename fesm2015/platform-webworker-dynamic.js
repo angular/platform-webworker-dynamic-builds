d85add2ee5c95212bb56a754e322b9f84d576611
Andrew Kushnir <akushnir@google.com>
2020-06-25 23:09:01 +0000

    8572e885b4 test(compiler-cli): fix assertion of diagnostic filename on Windows (#37758)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
+ * @license Angular v10.0.0-rc.0+264.sha-8572e88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+263.sha-0e3e9cd');
+const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
 
 /**
  * @license
