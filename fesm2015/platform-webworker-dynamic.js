83f26ee49d390027afd2b7738ecc0e816ee24d5a
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:22:11 +0000

    616543ded0 fix(router): add null support for RouterLink directive (#32616)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+240.sha-2170ea2
+ * @license Angular v10.0.0-rc.0+241.sha-616543d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+240.sha-2170ea2');
+const VERSION = new Version('10.0.0-rc.0+241.sha-616543d');
 
 /**
  * @license
