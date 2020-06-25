d3ea35f5d4997105f543e0358c9d2169c1b7880c
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:00:31 +0000

    8924ec1474 test(docs-infra): add end to end tests for api reference (#37612)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+231.sha-bedc045
+ * @license Angular v10.0.0-rc.0+229.sha-8924ec1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+231.sha-bedc045');
+const VERSION = new Version('10.0.0-rc.0+229.sha-8924ec1');
 
 /**
  * @license
