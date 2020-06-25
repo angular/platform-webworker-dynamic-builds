4cd124fac7daad9ec5126db252c55fcb667c247a
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:03:18 +0000

    f3ec37ebbf release(benchpress): bump version to v0.2.1 and update `@angular/core` peer dependency (#37676)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+232.sha-29490cc
+ * @license Angular v10.0.0-rc.0+234.sha-f3ec37e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+232.sha-29490cc');
+const VERSION = new Version('10.0.0-rc.0+234.sha-f3ec37e');
 
 /**
  * @license
