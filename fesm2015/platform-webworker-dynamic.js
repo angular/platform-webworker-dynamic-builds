1565e6d29052b3694f27c99a36a26ff3c5c35827
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:58:57 +0000

    64f2ffa166 fix(core): cleanup DOM elements when root view is removed (#37600)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+15.sha-a5d5f67
+ * @license Angular v10.0.0+20.sha-64f2ffa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0+15.sha-a5d5f67');
+const VERSION = new Version('10.0.0+20.sha-64f2ffa');
 
 /**
  * @license
