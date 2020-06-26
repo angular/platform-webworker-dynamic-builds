ec9ac82e899426ddbb5832f51c395b1e6d0a82a6
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:29:02 +0000

    c00f4ab2ae fix(core): don't consider inherited NG_ELEMENT_ID during DI (#37574)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+287.sha-2038568
+ * @license Angular v10.0.0-rc.0+288.sha-c00f4ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+287.sha-2038568');
+const VERSION = new Version('10.0.0-rc.0+288.sha-c00f4ab');
 
 /**
  * @license
