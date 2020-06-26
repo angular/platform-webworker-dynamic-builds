e2d22a5bd9da56b22fd608fe96c7504ebf57d3a6
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:22:52 +0000

    dbc2364d16 fix(dev-infra): support running scripts from within a detached head (#37737)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
 /**
  * @license
