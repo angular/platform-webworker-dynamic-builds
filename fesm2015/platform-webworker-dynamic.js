28dc7dead6e69dd0ae925d9eb7a0239c7295b519
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:52:53 +0000

    ad6680f602 fix(language-service): reinstate getExternalFiles() (#37750)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+25.sha-67cfc4c
+ * @license Angular v10.0.0+29.sha-ad6680f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0+25.sha-67cfc4c');
+const VERSION = new Version('10.0.0+29.sha-ad6680f');
 
 /**
  * @license
