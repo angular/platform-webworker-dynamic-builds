d4cb23349875682eb6b7d48a0ec5b9f200ac4ffe
Andrew Kushnir <akushnir@google.com>
2020-06-29 17:50:09 +0000

    78a44768e0 fix(ngcc): ensure lockfile is removed when analyzeFn fails (#37739)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+290.sha-98c047b
+ * @license Angular v10.0.0-rc.0+291.sha-78a4476
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+290.sha-98c047b');
+const VERSION = new Version('10.0.0-rc.0+291.sha-78a4476');
 
 /**
  * @license
