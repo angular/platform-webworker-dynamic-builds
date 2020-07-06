2cc264899ab083792ce12f109c8e5699e93db1af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:16:20 +0000

    1801d0c650 fix(router): properly compare array queryParams for equality (#37709) (#37860)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+314.sha-d7dd295
+ * @license Angular v10.0.0-rc.0+316.sha-1801d0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+314.sha-d7dd295');
+const VERSION = new Version('10.0.0-rc.0+316.sha-1801d0c');
 
 /**
  * @license
