ead957856695765994e1455d167d0d07aa0958e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:19:11 +0000

    596fbb3f06 build(docs-infra): improve applying post-install patches (#37896)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+316.sha-1801d0c
+ * @license Angular v10.0.0-rc.0+319.sha-596fbb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+316.sha-1801d0c');
+const VERSION = new Version('10.0.0-rc.0+319.sha-596fbb3');
 
 /**
  * @license
