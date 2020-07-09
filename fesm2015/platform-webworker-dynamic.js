72ce84df558af394354b67e436a63e7cf9503fb1
atscott <atscott@google.com>
2020-07-09 17:37:38 +0000

    8ee23ba67b docs: add explanation for providedIn any (#35283)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+4.sha-9f2393f
+ * @license Angular v10.0.3+18.sha-8ee23ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+4.sha-9f2393f');
+const VERSION = new Version('10.0.3+18.sha-8ee23ba');
 
 /**
  * @license
