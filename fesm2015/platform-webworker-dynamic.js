1b696844decfdd03660dfdd9a37445d7047754a8
Andrew Scott <atscott01@gmail.com>
2020-07-10 22:27:21 +0000

    88d68d2d21 docs: Move router tutorial (toh) from router.md to new file (#37979)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+24.sha-4783a07
+ * @license Angular v10.1.0-next.0+25.sha-88d68d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+24.sha-4783a07');
+const VERSION = new Version('10.1.0-next.0+25.sha-88d68d2');
 
 /**
  * @license
