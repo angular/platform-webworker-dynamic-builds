3ec755bb2bf1fded587bcb10dd097e1373dfb656
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:52:03 +0000

    66947cf9fa docs: fix typo in router.md (#37227)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+30.sha-d1aa467
+ * @license Angular v10.1.0-next.0+32.sha-66947cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+30.sha-d1aa467');
+const VERSION = new Version('10.1.0-next.0+32.sha-66947cf');
 
 /**
  * @license
