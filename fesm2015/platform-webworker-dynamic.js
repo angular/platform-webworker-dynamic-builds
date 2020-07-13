8ef637f20f4d4ce71bb22c096894ac715f6908ac
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:55:40 +0000

    9cf78d5701 docs(core): Fixed typo in Type JSdoc (#37930)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+45.sha-53e4ff7
+ * @license Angular v10.0.3+52.sha-9cf78d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+45.sha-53e4ff7');
+const VERSION = new Version('10.0.3+52.sha-9cf78d5');
 
 /**
  * @license
