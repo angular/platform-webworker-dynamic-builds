9e56774f672a23dfd15f0accdbef676bb096ef81
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:47:25 +0000

    9185c6e971 fix(router): ensure duplicate popstate/hashchange events are handled correctly (#37674)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+39.sha-69472a1
+ * @license Angular v10.1.0-next.0+40.sha-9185c6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+39.sha-69472a1');
+const VERSION = new Version('10.1.0-next.0+40.sha-9185c6e');
 
 /**
  * @license
