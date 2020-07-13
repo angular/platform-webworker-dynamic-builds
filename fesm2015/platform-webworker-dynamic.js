b7a124478087296bba791f37f8fbd4f698828d53
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:50:32 +0000

    d1aa4678be build(docs-infra): ensure the correct files are in the i18n example (#37947)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+31.sha-a259a84
+ * @license Angular v10.1.0-next.0+30.sha-d1aa467
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+31.sha-a259a84');
+const VERSION = new Version('10.1.0-next.0+30.sha-d1aa467');
 
 /**
  * @license
