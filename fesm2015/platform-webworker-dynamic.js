cd9c86d451b7c768c0425af48c501830ca55c6f2
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:26:59 +0000

    396033da80 refactor(forms): remove unnecessary `!` operators from validators (#36805)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+55.sha-d690eec
+ * @license Angular v10.0.3+58.sha-396033d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+55.sha-d690eec');
+const VERSION = new Version('10.0.3+58.sha-396033d');
 
 /**
  * @license
