20401329f84bfebbb9c7ffde6bca3483597c8fa9
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:27:59 +0000

    421e807f80 docs(forms): Fix typos in template-driven forms tutorial (#37933)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+47.sha-5fd2c8f
+ * @license Angular v10.1.0-next.0+46.sha-421e807
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+47.sha-5fd2c8f');
+    var VERSION = new core.Version('10.1.0-next.0+46.sha-421e807');
 
     /**
      * @license
