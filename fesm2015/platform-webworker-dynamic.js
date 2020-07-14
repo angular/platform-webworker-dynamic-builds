0fd28c2b7a06ea30917a3295598b86ebd46549e1
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:42:13 +0000

    d690eec88f docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+52.sha-9cf78d5
+ * @license Angular v10.0.3+55.sha-d690eec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+52.sha-9cf78d5');
+const VERSION = new Version('10.0.3+55.sha-d690eec');
 
 /**
  * @license
