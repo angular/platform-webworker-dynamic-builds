5f49b26e90ac16374ab7893625036294d1c42b08
Andrew Kushnir <akushnir@google.com>
2020-07-17 21:04:42 +0000

    095cfd961d build: Ignore .history for the xyz.local-history VSCode extension (#38121)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+27.sha-38a7021
+ * @license Angular v10.1.0-next.1+28.sha-095cfd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+27.sha-38a7021');
+    var VERSION = new core.Version('10.1.0-next.1+28.sha-095cfd9');
 
     /**
      * @license
