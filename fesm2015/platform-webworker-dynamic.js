4bb81113b439e2ef6624b7e46ddfe749f54ad699
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:33:16 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+34.sha-3797861
+ * @license Angular v10.0.4+35.sha-4664acc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+34.sha-3797861');
+const VERSION = new Version('10.0.4+35.sha-4664acc');
 
 /**
  * @license
