1e366277647dd7bb5a0ec6643d39859a260a7466
Misko Hevery <misko@hevery.com>
2020-07-23 04:12:19 +0000

    a50a688aaf docs: update api reference for router outlet directive (#38166)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+7.sha-6ec7297
+ * @license Angular v10.0.5+8.sha-a50a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.5+7.sha-6ec7297');
+    var VERSION = new core.Version('10.0.5+8.sha-a50a688');
 
     /**
      * @license
--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+7.sha-6ec7297
+ * @license Angular v10.0.5+8.sha-a50a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.5+7.sha-6ec7297');
+const VERSION = new Version('10.0.5+8.sha-a50a688');
 
 /**
  * @license
