779ebb3742b231edb2e48b85fb0da776698b7600
Misko Hevery <misko@hevery.com>
2020-07-23 18:28:22 +0000

    51aa4aa1c3 docs: update dynamic-component loading guide (#36959)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+13.sha-8bb8f98
+ * @license Angular v10.1.0-next.2+14.sha-51aa4aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+13.sha-8bb8f98');
+    var VERSION = new core.Version('10.1.0-next.2+14.sha-51aa4aa');
 
     /**
      * @license
