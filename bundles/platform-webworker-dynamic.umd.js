33c1ec52d3b1b2a5e3dd24eb4b12b574d08b8820
Misko Hevery <misko@hevery.com>
2020-07-24 22:37:34 +0000

    253337dc0a feat(zone.js): move MutationObserver/FileReader to different module (#31657)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+23.sha-6e2db22
+ * @license Angular v10.1.0-next.2+24.sha-253337d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+23.sha-6e2db22');
+    var VERSION = new core.Version('10.1.0-next.2+24.sha-253337d');
 
     /**
      * @license
