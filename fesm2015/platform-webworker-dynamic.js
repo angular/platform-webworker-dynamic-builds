dce7a636fd7e4e1a52c0020bf2a8f85700a1b33f
Misko Hevery <misko@hevery.com>
2020-07-24 22:42:23 +0000

    7467fd36b9 fix(zone.js): clearTimeout/clearInterval should call on object global (#37858)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+18.sha-aca0198
+ * @license Angular v10.0.5+19.sha-7467fd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.5+18.sha-aca0198');
+const VERSION = new Version('10.0.5+19.sha-7467fd3');
 
 /**
  * @license
