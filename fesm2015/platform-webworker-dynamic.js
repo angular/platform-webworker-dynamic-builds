386235eb168de3e9b3338827351322caad1bca5e
Misko Hevery <misko@hevery.com>
2020-07-25 01:23:36 +0000

    67e3ecc7e3 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+27.sha-6a3e68b
+ * @license Angular v10.1.0-next.2+30.sha-67e3ecc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+27.sha-6a3e68b');
+const VERSION = new Version('10.1.0-next.2+30.sha-67e3ecc');
 
 /**
  * @license
