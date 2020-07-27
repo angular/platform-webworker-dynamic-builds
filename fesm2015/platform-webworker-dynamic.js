01a386a407baa5a79dce18eaac792baf1ad50a8f
Misko Hevery <misko@hevery.com>
2020-07-27 19:29:33 +0000

    2b6ab57d78 docs: add template ref var to glossary (#36743)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+37.sha-b65b616
+ * @license Angular v10.1.0-next.2+38.sha-2b6ab57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+37.sha-b65b616');
+const VERSION = new Version('10.1.0-next.2+38.sha-2b6ab57');
 
 /**
  * @license
