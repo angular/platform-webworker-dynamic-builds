de830387ccf03160ba4194d4cf7089248876e461
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:12:29 +0000

    1eebb7f189 test(compiler-cli): disable one TypeChecker test on Windows due to path sensitivity issue (#38294)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+16.sha-7f8c222
+ * @license Angular v10.1.0-next.3+18.sha-1eebb7f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+16.sha-7f8c222');
+const VERSION = new Version('10.1.0-next.3+18.sha-1eebb7f');
 
 /**
  * @license
