ebfe9eec44cea4da25375c9e3ea8012213e321af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:42:55 +0000

    fd51e01335 fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+19.sha-3a525d1
+ * @license Angular v10.1.0-next.3+20.sha-fd51e01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+19.sha-3a525d1');
+const VERSION = new Version('10.1.0-next.3+20.sha-fd51e01');
 
 /**
  * @license
