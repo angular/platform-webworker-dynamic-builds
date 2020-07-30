92b18b8ff8a5dda129cf9ec19de3483754253420
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:27:05 +0000

    4a6abbdfc6 fix(compiler-cli): correct type of makeDiagnostic()

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+1.sha-0469d92
+ * @license Angular v10.0.6+19.sha-4a6abbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.6+1.sha-0469d92');
+const VERSION = new Version('10.0.6+19.sha-4a6abbd');
 
 /**
  * @license
