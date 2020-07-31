6ad668bc36d6c20f4aeaef45e56917fd783a8c95
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:19:40 +0000

    e8896b9de3 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+22.sha-6f6102d
+ * @license Angular v10.1.0-next.3+23.sha-e8896b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+22.sha-6f6102d');
+const VERSION = new Version('10.1.0-next.3+23.sha-e8896b9');
 
 /**
  * @license
