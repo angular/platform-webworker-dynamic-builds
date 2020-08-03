a4dcaa9622f74b4ef32d66204918bd2cf085c3e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:18:38 +0000

    e49b053dac build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+39.sha-ff9f4de
+ * @license Angular v10.1.0-next.3+41.sha-e49b053
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
+const VERSION = new Version('10.1.0-next.3+41.sha-e49b053');
 
 /**
  * @license
