6ee847d3a4331b236db9e79487428646284278d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:22:05 +0000

    8fbf40bf40 feat(core): update reference and doc to change `async` to `waitAsync`. (#37583)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+40.sha-87baa06
+ * @license Angular v10.1.0-next.3+43.sha-8fbf40b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+40.sha-87baa06');
+const VERSION = new Version('10.1.0-next.3+43.sha-8fbf40b');
 
 /**
  * @license
