485dcfa2eb11132f049ce877bd3c9244aff8cdff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:30:29 +0000

    2ab8e88924 release: cut the v10.0.8 release

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+20.sha-a91dd2e
+ * @license Angular v10.0.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.7+20.sha-a91dd2e');
+    var VERSION = new core.Version('10.0.8');
 
     /**
      * @license
