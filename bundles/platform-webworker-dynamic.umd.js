558c6056d43394ceacc42e0a315bf29de3965b7b
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:20:54 +0000

    415131413d fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8
+ * @license Angular v10.0.8+6.sha-4151314
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.8');
+    var VERSION = new core.Version('10.0.8+6.sha-4151314');
 
     /**
      * @license
