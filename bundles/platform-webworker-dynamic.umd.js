eb5224ea8df207875e24eed11e259a491950d453
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:28:29 +0000

    016a41b14d fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.8+10.sha-58f4b3a');
+    var VERSION = new core.Version('10.0.8+14.sha-016a41b');
 
     /**
      * @license
--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+10.sha-58f4b3a');
+const VERSION = new Version('10.0.8+14.sha-016a41b');
 
 /**
  * @license
