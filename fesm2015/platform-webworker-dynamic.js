ff5ddb4ee2ef67ee76441208dcef53529d81c409
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:37:58 +0000

    db897f4f7a docs: add a page with the Angular roadmap (#38356)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+6.sha-4151314
+ * @license Angular v10.0.8+8.sha-db897f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+6.sha-4151314');
+const VERSION = new Version('10.0.8+8.sha-db897f4');
 
 /**
  * @license
