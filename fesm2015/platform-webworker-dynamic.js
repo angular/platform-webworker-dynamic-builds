31cb2c3a7ce14a3e662904bc46d6d678e1bf1fdf
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:30:14 +0000

    42a649d80f docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+23.sha-47fbfb3
+ * @license Angular v10.0.8+24.sha-42a649d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+23.sha-47fbfb3');
+const VERSION = new Version('10.0.8+24.sha-42a649d');
 
 /**
  * @license
