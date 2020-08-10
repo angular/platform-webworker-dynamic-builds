ad9d1073c1a8e2849110aec9ac05078f74318a7d
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:45:35 +0000

    8f708b561c fix(router): defer loading of wildcard module until needed (#38348)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+30.sha-e34c33c
+ * @license Angular v10.1.0-next.4+31.sha-8f708b5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
+const VERSION = new Version('10.1.0-next.4+31.sha-8f708b5');
 
 /**
  * @license
