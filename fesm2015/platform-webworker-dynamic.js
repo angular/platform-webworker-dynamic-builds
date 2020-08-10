8f91254021bc5114ed69568399789cd8c79a796e
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:04:47 +0000

    ee5123f5dc fix(core): Store the currently selected ICU in `LView` (#38345)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+26.sha-3dfaf56
+ * @license Angular v10.0.8+27.sha-ee5123f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+26.sha-3dfaf56');
+const VERSION = new Version('10.0.8+27.sha-ee5123f');
 
 /**
  * @license
