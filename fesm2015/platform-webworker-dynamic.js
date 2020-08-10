09141648e0d62c0bf02d2620ac043836cc04d9a5
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:26:26 +0000

    0de93fd402 docs: update team contributors page (#38384)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+28.sha-6ff28ac
+ * @license Angular v10.1.0-next.4+27.sha-0de93fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+28.sha-6ff28ac');
+const VERSION = new Version('10.1.0-next.4+27.sha-0de93fd');
 
 /**
  * @license
