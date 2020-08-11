b91bc1f9a047afcdd9e3b7a22429f1c314362d37
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:52:46 +0000

    02aca135bd fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+31.sha-32109dc
+ * @license Angular v10.0.8+32.sha-02aca13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+31.sha-32109dc');
+const VERSION = new Version('10.0.8+32.sha-02aca13');
 
 /**
  * @license
