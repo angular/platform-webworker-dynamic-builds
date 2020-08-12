15356ac43e8f7b575dac1635f4b96c07bc46f8fd
Andrew Kushnir <akushnir@google.com>
2020-08-12 15:46:47 +0000

    4d17418569 docs: delete one superfluous sentence (#38339)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+44.sha-a6292fa
+ * @license Angular v10.1.0-next.4+47.sha-4d17418
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+44.sha-a6292fa');
+const VERSION = new Version('10.1.0-next.4+47.sha-4d17418');
 
 /**
  * @license
