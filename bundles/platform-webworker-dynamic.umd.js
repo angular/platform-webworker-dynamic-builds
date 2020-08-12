9675deb15643f1385998f087e8fcda0f61029fe7
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:53:31 +0000

    2f53bbba20 docs: remove unused Input decorator (#38306)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9
+ * @license Angular v10.0.9+3.sha-2f53bbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.9');
+    var VERSION = new core.Version('10.0.9+3.sha-2f53bbb');
 
     /**
      * @license
