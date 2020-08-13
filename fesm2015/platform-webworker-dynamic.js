6439c1242e6c318e7380412f466b796ad6fc2c03
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:55:26 +0000

    1388c1761f perf(compiler-cli): don't emit template guards when child scope is empty (#38418)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+13.sha-175c79d
+ * @license Angular v10.1.0-next.5+16.sha-1388c17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+13.sha-175c79d');
+const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
 
 /**
  * @license
