b0857576435b689c7797829ebf78e123fcd53477
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:02:32 +0000

    d5f819ebc1 style(compiler-cli): remove unused constant (#38441)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+16.sha-1388c17
+ * @license Angular v10.1.0-next.5+17.sha-d5f819e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
+const VERSION = new Version('10.1.0-next.5+17.sha-d5f819e');
 
 /**
  * @license
