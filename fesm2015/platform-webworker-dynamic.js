51b1340d969f9405e0b01f794de3047c7cead4de
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:12:36 +0000

    945751e2e8 ci: disable closure size tracking test (#38449)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+11.sha-b769771
+ * @license Angular v10.1.0-next.5+12.sha-945751e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+11.sha-b769771');
+const VERSION = new Version('10.1.0-next.5+12.sha-945751e');
 
 /**
  * @license
