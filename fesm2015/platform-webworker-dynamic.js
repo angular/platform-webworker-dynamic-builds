63800eff43df56ab8deb0bad3b9a41f60b47a8e5
Andrew Scott <atscott@google.com>
2020-08-17 20:15:37 +0000

    f3dd6c224c fix(ngcc): detect synthesized delegate constructors for downleveled ES2015 classes (#38500)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+18.sha-84d1ba7
+ * @license Angular v10.0.9+21.sha-f3dd6c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.9+18.sha-84d1ba7');
+const VERSION = new Version('10.0.9+21.sha-f3dd6c2');
 
 /**
  * @license
