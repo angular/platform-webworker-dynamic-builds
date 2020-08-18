49bba9c52a26e2c3141344aa754d5d02249c4581
Andrew Scott <atscott@google.com>
2020-08-18 17:33:05 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
+const VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
 
 /**
  * @license
