a602b3808acc7c0813dffa7562061991ef135e48
Andrew Scott <atscott@google.com>
2020-08-18 17:45:03 +0000

    dbfb50e9f4 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
+const VERSION = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
 /**
  * @license
