3af28f29f45a41b90d6b39f44b7efcb17114376f
Misko Hevery <misko@hevery.com>
2020-08-19 21:45:23 +0000

    ac461e1efd fix(localize): extract the correct message ids (#38498)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+7.sha-f245c6b
+ * @license Angular v10.1.0-next.7+8.sha-ac461e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+7.sha-f245c6b');
+const VERSION = new Version('10.1.0-next.7+8.sha-ac461e1');
 
 /**
  * @license
