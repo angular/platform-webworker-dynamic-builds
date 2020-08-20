363dc64bb0fe5cf279ce8e1a0b09312b9b48d310
Misko Hevery <misko@hevery.com>
2020-08-20 16:22:11 +0000

    c90262e619 Revert "Revert "fix(core): remove closing body tag from inert DOM builder (#38454)""

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+6.sha-87bbf69
+ * @license Angular v10.0.11+7.sha-c90262e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.11+6.sha-87bbf69');
+const VERSION = new Version('10.0.11+7.sha-c90262e');
 
 /**
  * @license
