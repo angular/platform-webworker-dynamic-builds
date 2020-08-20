d85215bc57ce4650d89f960c85f6f4a60f1c01de
Misko Hevery <misko@hevery.com>
2020-08-20 17:04:33 +0000

    1775f351d6 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+7.sha-c90262e
+ * @license Angular v10.0.11+10.sha-1775f35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.11+7.sha-c90262e');
+const VERSION = new Version('10.0.11+10.sha-1775f35');
 
 /**
  * @license
