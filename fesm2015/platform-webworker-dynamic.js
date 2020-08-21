9d7b72a40edfb6016be8d274e7c069e57f0d20ad
Misko Hevery <misko@hevery.com>
2020-08-21 19:40:56 +0000

    2a643e1ab6 docs: change function name from async -> waitForAsync (#38548)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+14.sha-956b25a
+ * @license Angular v10.1.0-next.7+16.sha-2a643e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+14.sha-956b25a');
+const VERSION = new Version('10.1.0-next.7+16.sha-2a643e1');
 
 /**
  * @license
