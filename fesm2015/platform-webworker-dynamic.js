c20f679904f17743fbdb8378dfea64c9db8b5f42
Misko Hevery <misko@hevery.com>
2020-08-25 22:45:35 +0000

    91689193bf docs: Minor fixes in NgModules section (#36177)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12+3.sha-d066368
+ * @license Angular v10.0.12+4.sha-9168919
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.12+3.sha-d066368');
+const VERSION = new Version('10.0.12+4.sha-9168919');
 
 /**
  * @license
