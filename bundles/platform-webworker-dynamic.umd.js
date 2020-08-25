ba7ee0b096fab2658d944e3180891a756f1bae35
Misko Hevery <misko@hevery.com>
2020-08-25 17:17:58 +0000

    d06636848e docs: add correct path for karma.conf.js file (#38571)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12
+ * @license Angular v10.0.12+3.sha-d066368
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.12');
+    var VERSION = new core.Version('10.0.12+3.sha-d066368');
 
     /**
      * @license
