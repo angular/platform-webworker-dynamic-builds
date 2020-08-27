6def429bf0c179c429311661e9d219c85774ebc1
Joey Perrott <josephperrott@gmail.com>
2020-08-27 18:24:19 +0000

    90cec40cce build: bump to lastest sha of angular/dev-infra's lock-closed action (#38615)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+6.sha-4036281
+ * @license Angular v10.1.0-rc.0+7.sha-90cec40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+6.sha-4036281');
+const VERSION = new Version('10.1.0-rc.0+7.sha-90cec40');
 
 /**
  * @license
