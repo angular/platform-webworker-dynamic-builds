8e1dca138828fa1ddfe59fce25284f23fa135ad6
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:01:10 +0000

    ebc0e46501 refactor(dev-infra): improve error message for unexpected version branches (#38622)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+23.sha-d5fabc3
+ * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+23.sha-d5fabc3');
+const VERSION = new Version('10.1.0-rc.0+22.sha-ebc0e46');
 
 /**
  * @license
