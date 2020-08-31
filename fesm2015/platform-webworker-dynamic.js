dafaa4713edce9b2dff36fdf1eea8118c6179883
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:08:46 +0000

    0fc2bef0cd docs(service-worker): add links to service worker communication guide (#36847)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
+ * @license Angular v10.1.0-rc.0+29.sha-0fc2bef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵplatformCoreDynamic, ɵResourceLoaderImpl } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+24.sha-0a7a5e3');
+const VERSION = new Version('10.1.0-rc.0+29.sha-0fc2bef');
 
 /**
  * @license
