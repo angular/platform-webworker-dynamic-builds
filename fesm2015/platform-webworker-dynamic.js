17ef862dc2c714c3347850a75d693527b4bce5fb
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:58:57 +0000

    9a649809ca refactor(forms): remove extra space in error message (#38637)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+19.sha-4e8592e
+ * @license Angular v10.0.14+21.sha-9a64980
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.14+19.sha-4e8592e');
+const VERSION = new Version('10.0.14+21.sha-9a64980');
 
 /**
  * @license
