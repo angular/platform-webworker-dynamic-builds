9f672ae6639caa4285d4e467a668089e426be217
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:09:27 +0000

    4e8592e610 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+13.sha-cb6bf40
+ * @license Angular v10.0.14+19.sha-4e8592e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.14+13.sha-cb6bf40');
+const VERSION = new Version('10.0.14+19.sha-4e8592e');
 
 /**
  * @license
