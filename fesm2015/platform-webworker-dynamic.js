6cb4fa1d3081548b21e9af61264f200e895aa671
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:21:50 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+19.sha-5588324');
+const VERSION = new Version('10.1.0');
 
 /**
  * @license
