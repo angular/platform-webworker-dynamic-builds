41feb85b9e5581d27eebb5d194242d8594856245
Andrew Scott <atscott@google.com>
2020-09-03 21:30:49 +0000

    9bf32c4dcb refactor(compiler-cli): make template parsing errors into diagnostics (#38576)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+7.sha-25afbcc
+ * @license Angular v10.1.0+9.sha-9bf32c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0+7.sha-25afbcc');
+const VERSION = new Version('10.1.0+9.sha-9bf32c4');
 
 /**
  * @license
