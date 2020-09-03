1fe8164e0fdeb6ea163c21a811a51d163f366a43
atscott <atscott01@gmail.com>
2020-09-03 00:11:16 +0000

    fdea1804d6 fix(core): remove CollectionChangeRecord symbol (#38668)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+2.sha-5da1934
+ * @license Angular v11.0.0-next.0+6.sha-fdea180
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+2.sha-5da1934');
+const VERSION = new Version('11.0.0-next.0+6.sha-fdea180');
 
 /**
  * @license
