824b6025d3fc677a8b8a87e5c83d4dcff028fec4
atscott <atscott01@gmail.com>
2020-09-08 21:50:13 +0000

    190dca0fdc fix(localize): enable whitespace preservation marker in XLIFF files (#38737)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+26.sha-a1c34c6
+ * @license Angular v10.1.0+34.sha-190dca0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0+26.sha-a1c34c6');
+const VERSION = new Version('10.1.0+34.sha-190dca0');
 
 /**
  * @license
