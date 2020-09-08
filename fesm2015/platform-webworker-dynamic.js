0199854efcad3fb12fbfbac613e1e6efe4634992
atscott <atscott01@gmail.com>
2020-09-08 17:30:33 +0000

    5863537575 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+10.sha-0a277c6
+ * @license Angular v10.1.0+20.sha-5863537
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0+10.sha-0a277c6');
+const VERSION = new Version('10.1.0+20.sha-5863537');
 
 /**
  * @license
