8be8d848a12f2a35f82931f40e31c1b65db44928
atscott <atscott01@gmail.com>
2020-09-09 23:53:38 +0000

    1373a98e25 feat(dev-infra): Allow local user ng-dev configuration to disable commit message wizard (#38701)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+3.sha-3c4b8b9
+ * @license Angular v11.0.0-next.1+5.sha-1373a98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+3.sha-3c4b8b9');
+const VERSION = new Version('11.0.0-next.1+5.sha-1373a98');
 
 /**
  * @license
