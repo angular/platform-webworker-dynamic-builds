9b5f773b5baa436c9f4eb3b43c43c13ee9a24e4d
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:54:30 +0000

    03447ba52f docs: Move Displaying data topic and ToH tutorial (#38774)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+19.sha-19598b4
+ * @license Angular v11.0.0-next.1+21.sha-03447ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+19.sha-19598b4');
+const VERSION = new Version('11.0.0-next.1+21.sha-03447ba');
 
 /**
  * @license
