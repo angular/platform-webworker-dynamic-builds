bbdeb420ab6c42b8de45dbfc4148f3a78320f20d
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:45:35 +0000

    b05d79d14a build(router): update symbols for routing app (#38817)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+15.sha-8096c63
+ * @license Angular v10.1.1+18.sha-b05d79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+15.sha-8096c63');
+const VERSION = new Version('10.1.1+18.sha-b05d79d');
 
 /**
  * @license
