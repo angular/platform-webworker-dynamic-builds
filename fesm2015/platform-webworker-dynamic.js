e4f0bcdac55006220bb44fa51b8f2a9849ef5e92
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:06:43 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+13.sha-31d0ee4');
+const VERSION = new Version('10.1.1+15.sha-8096c63');
 
 /**
  * @license
