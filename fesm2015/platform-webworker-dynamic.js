117f29ea6c72d8c79bcd8ef88f33c8a97717f60c
Andrew Kushnir <akushnir@google.com>
2020-09-14 20:30:43 +0000

    ebede67433 perf(compiler-cli): optimize computation of type-check scope information (#38843)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+20.sha-c62d1cb
+ * @license Angular v10.1.1+22.sha-ebede67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+20.sha-c62d1cb');
+const VERSION = new Version('10.1.1+22.sha-ebede67');
 
 /**
  * @license
