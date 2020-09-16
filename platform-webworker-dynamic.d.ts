b5d5e7cf347bc782d10ee74b5cd7d9d396cdc18f
GitHub <noreply@github.com>
2020-09-16 19:46:22 +0000

    956e4f2bab docs: add upcoming angular conferences

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const VERSION: Version;
+
+export { }
