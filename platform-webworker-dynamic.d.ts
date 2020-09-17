b4d7ce6698389c52d5d4bc2f6d48fc9028422901
Joey Perrott <josephperrott@gmail.com>
2020-09-17 18:28:44 +0000

    537934abe2 build: wrap bazel command in custom wrapping script to assess diagnostic info

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.0.0-next.0+46.sha-537934a
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const VERSION: Version;
+
+export { }
