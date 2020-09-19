c2e4caf9d8d5bdd1e051c5bed2ecca54282145ec
Misko Hevery <misko@hevery.com>
2020-09-19 00:18:48 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+21.sha-3902ec0
+ * @license Angular v10.1.2+24.sha-7d5b515
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.2+21.sha-3902ec0');
+const VERSION = new Version('10.1.2+24.sha-7d5b515');
 
 /**
  * @license
