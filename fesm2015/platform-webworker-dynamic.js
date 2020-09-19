e8cb8aeed28938cfc0617096f2f0bec1a1f62761
Misko Hevery <misko@hevery.com>
2020-09-19 00:26:32 +0000

    a91f0f6b82 refactor(compiler): refactor ast spans test to be more human readable (#38902)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+28.sha-b68fab5
+ * @license Angular v11.0.0-next.2+30.sha-a91f0f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+28.sha-b68fab5');
+const VERSION = new Version('11.0.0-next.2+30.sha-a91f0f6');
 
 /**
  * @license
