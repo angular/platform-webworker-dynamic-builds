6b0b9ad2569f324cad2e08317e63021e88be3195
Misko Hevery <misko@hevery.com>
2020-09-22 22:33:55 +0000

    32e32e5bdc refactor(dev-infra): simplify runBenchmark (#38941)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+24.sha-7d5b515
+ * @license Angular v10.1.2+33.sha-32e32e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.2+24.sha-7d5b515');
+const VERSION = new Version('10.1.2+33.sha-32e32e5');
 
 /**
  * @license
