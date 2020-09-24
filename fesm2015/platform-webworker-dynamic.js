fc616e7911fed0c4925c9ad29f05fb0cfdb27fe9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:03:49 +0000

    8422633b8a test(docs-infra): add missing unit tests for backoff example (#38896)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+5.sha-19d543f
+ * @license Angular v11.0.0-next.3+7.sha-8422633
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+5.sha-19d543f');
+const VERSION = new Version('11.0.0-next.3+7.sha-8422633');
 
 /**
  * @license
