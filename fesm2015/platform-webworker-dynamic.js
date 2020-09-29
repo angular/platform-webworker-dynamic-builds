af710909531476e5a5e6929bcc3bfde79106f1a3
Misko Hevery <misko@hevery.com>
2020-09-29 15:05:34 +0000

    69302adc02 refactor(core): Create `NodeInjectorOffset` type which better describes NodeInjector (#38707)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+49.sha-875fcfe
+ * @license Angular v10.1.3+60.sha-69302ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.3+49.sha-875fcfe');
+const VERSION = new Version('10.1.3+60.sha-69302ad');
 
 /**
  * @license
