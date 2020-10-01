18976da701a7bae0503e1f02892afbb138e2ca61
Joey Perrott <josephperrott@gmail.com>
2020-10-01 23:16:46 +0000

    42f9679376 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#37727)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+15.sha-bb19e61
+ * @license Angular v10.1.4+18.sha-42f9679
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.4+15.sha-bb19e61');
+const VERSION = new Version('10.1.4+18.sha-42f9679');
 
 /**
  * @license
