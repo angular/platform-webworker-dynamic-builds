d4457d4925009558ab3482d39c1a670305cb445c
Joey Perrott <josephperrott@gmail.com>
2020-10-01 19:54:38 +0000

    bb19e61848 feat(dev-infra): add a command to verify NgBot YAML config syntax (#39071)

--- bundles/platform-webworker-dynamic.umd.js
+++ bundles/platform-webworker-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+14.sha-50e83e2
+ * @license Angular v10.1.4+15.sha-bb19e61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.4+14.sha-50e83e2');
+    var VERSION = new core.Version('10.1.4+15.sha-bb19e61');
 
     /**
      * @license
