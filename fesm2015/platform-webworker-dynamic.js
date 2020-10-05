344f2dc1971cc8f88d9094ae77021b57c559a18f
Joey Perrott <josephperrott@gmail.com>
2020-10-05 17:46:32 +0000

    878e2f0deb feat(dev-infra): show CI status of all active release trains (#39067)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+23.sha-4c30f51
+ * @license Angular v10.1.4+24.sha-878e2f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.4+23.sha-4c30f51');
+const VERSION = new Version('10.1.4+24.sha-878e2f0');
 
 /**
  * @license
