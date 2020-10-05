e9d6b9e0f2c5da56753255b871d5f727219d336d
sajeetharan <sajeefx@gmail.com>
2020-10-05 19:32:35 +0000

    4bd836c87e updated sajee's profile

---
+++ platform-webworker-dynamic.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.0.0-next.3+23.sha-4bd836c
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+import { Version } from '@angular/core';
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const platformWorkerAppDynamic: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * @publicApi
+ * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
+ *     of Angular
+ */
+export declare const VERSION: Version;
+
+export { }
