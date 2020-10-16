4fbc1be65f4a56fc9e1f5fdc307ead5e6e2373c2
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:46:17 +0000

    4a5f17cd8e build(docs-infra): add a tool to create new examples (#39283)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+16.sha-94d7ef3
+ * @license Angular v10.1.6+18.sha-4a5f17c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+16.sha-94d7ef3');
+const VERSION = new Version('10.1.6+18.sha-4a5f17c');
 
 /**
  * @license
