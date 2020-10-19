a32059ab476d726ad11583304a2420cbdb52180e
Andrew Kushnir <akushnir@google.com>
2020-10-19 23:49:33 +0000

    cd8ec3d902 docs: add Jessica Janiuk to contributors.json (#39253)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+27.sha-02b8750
+ * @license Angular v10.1.6+28.sha-cd8ec3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+27.sha-02b8750');
+const VERSION = new Version('10.1.6+28.sha-cd8ec3d');
 
 /**
  * @license
