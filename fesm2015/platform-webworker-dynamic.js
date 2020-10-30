8a84d2434ac9929ef45ee248b64fa1a4926c7007
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:45:54 +0000

    85d5242630 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+12.sha-90acb91
+ * @license Angular v10.2.1+14.sha-85d5242
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+12.sha-90acb91');
+const VERSION = new Version('10.2.1+14.sha-85d5242');
 
 /**
  * @license
