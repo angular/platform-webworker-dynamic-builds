1fa2cced651d0fcaeb5fd73992c946907f69aea1
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:12:49 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+15.sha-71d0063
+ * @license Angular v10.2.1+29.sha-aa3bd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+15.sha-71d0063');
+const VERSION = new Version('10.2.1+29.sha-aa3bd27');
 
 /**
  * @license
