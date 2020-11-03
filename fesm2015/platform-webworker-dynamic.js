d3b441c70a9e8ffb41e14d0d1bb6691024e2cede
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:16:19 +0000

    250858a48e feat(dev-infra): provide tooling to check what branches a pr targets (#39504)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+29.sha-aa3bd27
+ * @license Angular v10.2.1+30.sha-250858a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+29.sha-aa3bd27');
+const VERSION = new Version('10.2.1+30.sha-250858a');
 
 /**
  * @license
