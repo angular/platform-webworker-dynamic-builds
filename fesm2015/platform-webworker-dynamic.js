f9b43dc6b70e12f6cb4c9e77034521cb1cc8a719
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:15:08 +0000

    08e077c0e0 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+33.sha-8929355
+ * @license Angular v10.2.2+1.sha-08e077c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+33.sha-8929355');
+const VERSION = new Version('10.2.2+1.sha-08e077c');
 
 /**
  * @license
