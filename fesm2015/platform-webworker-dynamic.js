676967cb9bb8f963bf6da36f4e7473db82349935
Misko Hevery <misko@hevery.com>
2020-11-09 16:03:33 +0000

    ae71b76089 ci: log commands output when deploying angular.io to Firebase (#39596)

--- fesm2015/platform-webworker-dynamic.js
+++ fesm2015/platform-webworker-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+8.sha-861e4fa
+ * @license Angular v10.2.2+10.sha-ae71b76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { ɵResourceLoaderImpl, ɵplatformCoreDynamic } from '@angular/platform-b
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.2+8.sha-861e4fa');
+const VERSION = new Version('10.2.2+10.sha-ae71b76');
 
 /**
  * @license
